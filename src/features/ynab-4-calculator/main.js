89c7a257e399a24e9a506a1fed5239bfe03abc19
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-05 21:50:45 +1100

    This may fix #7. Could you test and let me know?

--- src/features/ynab-4-calculator/main.js
+++ src/features/ynab-4-calculator/main.js
@@ -1,17 +1,17 @@
 function injectInitializer() {
   if (typeof Em !== 'undefined' && typeof Ember !== 'undefined') {
 
-    $(document).on('keydown', 'li.budget-table-cell-budgeted div.currency-input', function(e) {
+    $(document).on('keypress', 'li.budget-table-cell-budgeted div.currency-input', function(e) {
 
-      if ((e.which == 187 && e.shiftKey) || // + on main keyboard
-           e.which == 189                || // - on main keyboard
-           e.which == 191                || // / on main keyboard
-          (e.which == 56  && e.shiftKey) || // * on main keyboard
-           e.which == 107                || // + on number pad
-           e.which == 109                || // - on number pad
-           e.which == 111                || // / on number pad
-           e.which == 106                   // * on number pad
-      ) {
+      e = e || window.event;
+      var charCode = e.which || e.keyCode;
+      var charTyped = String.fromCharCode(charCode);
+
+      if (charTyped == '+' ||
+          charTyped == '-' ||
+          charTyped == '*' ||
+          charTyped == '/')
+      {
         var input = $(this).find('input');
         var length = input.val().length;
 
