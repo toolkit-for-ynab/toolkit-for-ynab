562441751971c869dfc2bed9f4478d34a18ff748
Nathanael Ritz <nathanritz@gmail.com>
2016-01-05 09:13:31 -0700

    Added options: Credit payment category warnings, Highlight all negative category balances red

---
+++ src/features/highlight-negatives-negative/main.js
@@ -0,0 +1,26 @@
+(function highlightNegativesRed_enhancedYNAB() { // give function unique name unlikely to be used by YNAB devs
+   
+	if ( typeof Em !== 'undefined' && typeof Ember !== 'undefined' && typeof $ !== 'undefined' && $('.budget-table-cell-available-div.user-data').length ) {
+		
+		var availableBalances = $('.budget-table-cell-available-div.user-data');
+		var categoryBalance = {};
+
+		$(availableBalances).each(function () {
+			categoryBalance = $(this).find('.user-data.currency').html(); // get the value
+		  	categoryBalance = Number( categoryBalance.replace(/[^\d.-]/g, '') ); // force data type as number
+		  	
+		
+			if ( categoryBalance < 0 ) {
+				
+			    if ( $(this).find('.user-data.currency').hasClass('cautious') ) {
+			    	$(this).find('.user-data.currency').removeClass('cautious').addClass('negative') 
+			    };
+			};
+
+		});
+
+	}
+	setTimeout(highlightNegativesRed_enhancedYNAB, 300);
+
+})();
+
