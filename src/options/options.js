81fc2a0d432624f7235b31426cb6b3340b4d2b1b
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-07 22:52:24 +1100

    Renamed previously merged feature to Category Activity Popup Width, as there's already a category popup for editing, etc.
    This has nothing to do with your pull request but I went ahead and did it when I noticed it.

--- src/options/options.js
+++ src/options/options.js
@@ -7,8 +7,8 @@ function save_options() {
   var removeZeroCategories = document.getElementById('removeZeroCategories').checked;
   var budgetRowsHeightSelect = document.getElementById('budgetRowsHeight');
   budgetRowsHeight = budgetRowsHeightSelect.options[budgetRowsHeightSelect.selectedIndex].value;
-  var categoryPopupWidthSelect = document.getElementById('categoryPopupWidth');
-  categoryPopupWidth = categoryPopupWidthSelect.options[categoryPopupWidthSelect.selectedIndex].value;
+  var categoryActivityPopupWidthSelect = document.getElementById('categoryActivityPopupWidth');
+  categoryActivityPopupWidth = categoryActivityPopupWidthSelect.options[categoryActivityPopupWidthSelect.selectedIndex].value;
   var moveMoneyDialog = document.getElementById('moveMoneyDialog').checked;
 
   chrome.storage.sync.set({
@@ -19,7 +19,7 @@ function save_options() {
     enableRetroCalculator: enableRetroCalculator,
     removeZeroCategories: removeZeroCategories,
     budgetRowsHeight: budgetRowsHeight,
-    categoryPopupWidth: categoryPopupWidth,
+    categoryActivityPopupWidth: categoryActivityPopupWidth,
     moveMoneyDialog: moveMoneyDialog
   }, function() {
     // Update status to let user know options were saved.
@@ -43,10 +43,10 @@ function restore_options() {
     highlightNegativesNegative: false,
     enableRetroCalculator: true,
     removeZeroCategories: true,
+    moveMoneyDialog: false,
     budgetRowsHeight: 0,
-    categoryPopupWidth: 0
-    budgetRowsHeight: 0,
-    moveMoneyDialog: false
+    categoryActivityPopupWidth: 0,
+    budgetRowsHeight: 0
   }, function(items) {
     document.getElementById('colourBlindMode').checked = items.colourBlindMode;
     document.getElementById('hideAOM').checked = items.hideAOM;
@@ -56,8 +56,8 @@ function restore_options() {
     document.getElementById('removeZeroCategories').checked = items.removeZeroCategories;
     var budgetRowsHeightSelect = document.getElementById('budgetRowsHeight');
     budgetRowsHeightSelect.value = items.budgetRowsHeight;
-    var categoryPopupWidthSelect = document.getElementById('categoryPopupWidth');
-    categoryPopupWidthSelect.value = items.categoryPopupWidth;
+    var categoryActivityPopupWidthSelect = document.getElementById('categoryActivityPopupWidth');
+    categoryActivityPopupWidthSelect.value = items.categoryActivityPopupWidth;
     document.getElementById('moveMoneyDialog').checked = items.moveMoneyDialog;
   });
 }
