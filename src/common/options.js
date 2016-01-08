40b6cae58bbbc5733066eed258c179701174e6a2
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-09 10:28:02 +1100

    Saving of options works. I like to think it'll be easier to turn into a proper dependency injection setup later this weekend.

--- src/common/options.js
+++ src/common/options.js
@@ -6,95 +6,98 @@
 //   }}
 // };
 
+function ensureDefaultsAreSet() {
+  var storedKeys = kango.storage.getKeys();
+
+  if (storedKeys.indexOf('collapseExpandBudgetGroups') < 0) {
+    kango.storage.setItem('collapseExpandBudgetGroups', true);
+  }
+
+  if (storedKeys.indexOf('enableRetroCalculator') < 0) {
+    kango.storage.setItem('enableRetroCalculator', true);
+  }
+
+  if (storedKeys.indexOf('removeZeroCategories') < 0) {
+    kango.storage.setItem('removeZeroCategories', true);
+  }
+}
+
+function saveCheckboxOption(elementId) {
+  var checked = document.getElementById(elementId).checked;
+  kango.storage.setItem(elementId, checked);
+}
+
+function saveSelectOption(elementId) {
+  var select = document.getElementById(elementId);
+  kango.storage.setItem(elementId, select.options[select.selectedIndex].value);
+}
+
+function restoreCheckboxOption(elementId) {
+  var checked = kango.storage.getItem(elementId);
+  document.getElementById(elementId).checked = checked;
+}
+
+function restoreSelectOption(elementId) {
+  var data = kango.storage.getItem(elementId) || 0;
+  var select = document.getElementById(elementId);
+
+  select.value = data;
+}
+
 function save_options() {
-  var collapseExpandBudgetGroups = document.getElementById('collapseExpandBudgetGroups').checked;
-  var collapseSideMenu = document.getElementById('collapseSideMenu').checked;
-  var colourBlindMode = document.getElementById('colourBlindMode').checked;
-  var hideAOM = document.getElementById('hideAOM').checked;
-  var checkCreditBalances = document.getElementById('checkCreditBalances').checked;
-  var highlightNegativesNegative = document.getElementById('highlightNegativesNegative').checked;
-  var enableRetroCalculator = document.getElementById('enableRetroCalculator').checked;
-  var removeZeroCategories = document.getElementById('removeZeroCategories').checked;
-  var budgetRowsHeightSelect = document.getElementById('budgetRowsHeight');
-  budgetRowsHeight = budgetRowsHeightSelect.options[budgetRowsHeightSelect.selectedIndex].value;
-  var reconciledTextColorSelect = document.getElementById('reconciledTextColor');
-  reconciledTextColor = reconciledTextColorSelect.options[reconciledTextColorSelect.selectedIndex].value;
-  var categoryActivityPopupWidthSelect = document.getElementById('categoryActivityPopupWidth');
-  categoryActivityPopupWidth = categoryActivityPopupWidthSelect.options[categoryActivityPopupWidthSelect.selectedIndex].value;
-  var moveMoneyDialog = document.getElementById('moveMoneyDialog').checked;
-  var moveMoneyAutocomplete = document.getElementById('moveMoneyAutocomplete').checked;
-  var toggleSplits = document.getElementById('toggleSplits').checked;
-  var accountsSelectedTotal = document.getElementById('accountsSelectedTotal').checked;
 
-  chrome.storage.sync.set({
-    collapseExpandBudgetGroups: collapseExpandBudgetGroups,
-    collapseSideMenu: collapseSideMenu,
-    colourBlindMode: colourBlindMode,
-    hideAOM: hideAOM,
-    checkCreditBalances: checkCreditBalances,
-    highlightNegativesNegative: highlightNegativesNegative,
-    enableRetroCalculator: enableRetroCalculator,
-    removeZeroCategories: removeZeroCategories,
-    budgetRowsHeight: budgetRowsHeight,
-    reconciledTextColor: reconciledTextColor,
-    categoryActivityPopupWidth: categoryActivityPopupWidth,
-    moveMoneyDialog: moveMoneyDialog,
-    moveMoneyAutocomplete: moveMoneyAutocomplete,
-    toggleSplits: toggleSplits,
-    accountsSelectedTotal: accountsSelectedTotal
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+  saveCheckboxOption('collapseExpandBudgetGroups');
+  saveCheckboxOption('collapseSideMenu');
+  saveCheckboxOption('colourBlindMode');
+  saveCheckboxOption('hideAOM');
+  saveCheckboxOption('checkCreditBalances');
+  saveCheckboxOption('highlightNegativesNegative');
+  saveCheckboxOption('enableRetroCalculator');
+  saveCheckboxOption('removeZeroCategories');
+  saveCheckboxOption('moveMoneyDialog');
+  saveCheckboxOption('moveMoneyAutocomplete');
+  saveCheckboxOption('toggleSplits');
+  saveCheckboxOption('accountsSelectedTotal');
 
-    setTimeout(function() {
-      status.textContent = '';
-    }, 1000);
-  });
+  saveSelectOption('budgetRowsHeight');
+  saveSelectOption('reconciledTextColor');
+  saveSelectOption('categoryActivityPopupWidth');
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById('status');
+  status.textContent = 'Options saved.';
+
+  setTimeout(function() {
+    status.textContent = '';
+  }, 1000);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
 
-  chrome.storage.sync.get({
-    collapseExpandBudgetGroups: true,
-    collapseSideMenu: false,
-    colourBlindMode: false,
-    hideAOM: false,
-    checkCreditBalances: false,
-    highlightNegativesNegative: false,
-    enableRetroCalculator: true,
-    removeZeroCategories: true,
-    moveMoneyDialog: false,
-    budgetRowsHeight: 0,
-    categoryActivityPopupWidth: 0,
-    budgetRowsHeight: 0,
-    moveMoneyAutocomplete: false,
-    toggleSplits: false,
-    accountsSelectedTotal: false,
-    reconciledTextColor: 0
-  }, function(items) {
-    document.getElementById('collapseExpandBudgetGroups').checked = items.collapseExpandBudgetGroups;
-    document.getElementById('collapseSideMenu').checked = items.collapseSideMenu;
-    document.getElementById('colourBlindMode').checked = items.colourBlindMode;
-    document.getElementById('hideAOM').checked = items.hideAOM;
-    document.getElementById('checkCreditBalances').checked = items.checkCreditBalances;
-    document.getElementById('highlightNegativesNegative').checked = items.highlightNegativesNegative;
-    document.getElementById('enableRetroCalculator').checked = items.enableRetroCalculator;
-    document.getElementById('removeZeroCategories').checked = items.removeZeroCategories;
-    var budgetRowsHeightSelect = document.getElementById('budgetRowsHeight');
-    budgetRowsHeightSelect.value = items.budgetRowsHeight;
-    var reconciledTextColorSelect = document.getElementById('reconciledTextColor');
-    reconciledTextColorSelect.value = items.reconciledTextColor;
-    var categoryActivityPopupWidthSelect = document.getElementById('categoryActivityPopupWidth');
-    categoryActivityPopupWidthSelect.value = items.categoryActivityPopupWidth;
-    document.getElementById('moveMoneyDialog').checked = items.moveMoneyDialog;
-    document.getElementById('moveMoneyAutocomplete').checked = items.moveMoneyAutocomplete;
-    document.getElementById('toggleSplits').checked = items.toggleSplits;
-    document.getElementById('accountsSelectedTotal').checked = items.accountsSelectedTotal;
-  });
+  ensureDefaultsAreSet();
+
+  restoreCheckboxOption('collapseExpandBudgetGroups');
+  restoreCheckboxOption('collapseSideMenu');
+  restoreCheckboxOption('colourBlindMode');
+  restoreCheckboxOption('hideAOM');
+  restoreCheckboxOption('checkCreditBalances');
+  restoreCheckboxOption('highlightNegativesNegative');
+  restoreCheckboxOption('enableRetroCalculator');
+  restoreCheckboxOption('removeZeroCategories');
+  restoreCheckboxOption('moveMoneyDialog');
+  restoreCheckboxOption('moveMoneyAutocomplete');
+  restoreCheckboxOption('toggleSplits');
+  restoreCheckboxOption('accountsSelectedTotal');
+
+  restoreSelectOption('budgetRowsHeight');
+  restoreSelectOption('reconciledTextColor');
+  restoreSelectOption('categoryActivityPopupWidth');
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
+KangoAPI.onReady(function() {
+  restore_options();
+});
+
 document.getElementById('save').addEventListener('click', save_options);
