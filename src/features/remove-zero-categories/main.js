75ced61ad009abb48257e9f6c97bd1355a91aca9
Nathanael Ritz <nathanritz@gmail.com>
2016-01-07 23:23:14 -0700

    Added a more reliable css selector (unix line endings)
    Replaced 'fieldset' with '.modal-budget-overspending' which has a higher
    chance of remaining consistent in production code.

--- src/features/remove-zero-categories/main.js
+++ src/features/remove-zero-categories/main.js
@@ -1,7 +1,7 @@
 (function removeZeroCategoriesFromCoverOverbudgeting() {
 
   if (typeof Em !== 'undefined' && typeof Ember !== 'undefined' && typeof $ !== 'undefined') {
-    var coverOverbudgetingCategories = $( "fieldset>.options-shown>.ynab-select-options" ).children('li');
+    var coverOverbudgetingCategories = $( ".modal-budget-overspending .options-shown .ynab-select-options" ).children('li');
     coverOverbudgetingCategories.each(function(i) {
       var t = $(this).text(); // Category balance text.
       var categoryBalance = parseInt(t.substr(t.length - t.indexOf(":") + 2).replace( /\D/g, ''))
