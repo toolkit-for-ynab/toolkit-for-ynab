6f5e4c1617c6cb24ff00c43ebf8efe8cd7e82539
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-10 23:31:13 +1100

    Fixes two bugs: overhang on options page that was enabling horizontal scroll, and Firefox bug where panel didn't close after clicking a button.

--- src/common/popup/popup.js
+++ src/common/popup/popup.js
@@ -2,6 +2,7 @@ KangoAPI.onReady(function() {
 
   $('#openSettings').click(function() {
     kango.ui.optionsPage.open();
+    KangoAPI.closeWindow();
   });
 
   $('#versionNumber').text(kango.getExtensionInfo().version);
