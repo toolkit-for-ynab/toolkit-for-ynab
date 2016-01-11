ee6941c97c9beb41efb050018383f9f9cdebe3df
Stefan Mai <stefan.mai@iamnafets.com>
2016-01-11 09:35:06 -0800

    Fixing internationalization bug, fixing quote styling.

--- src/common/res/features/pacing/main.js
+++ src/common/res/features/pacing/main.js
@@ -4,9 +4,9 @@ function ynabEnhancedFormatCurrency(e, html) {
   n = ynab.YNABSharedLib.currencyFormatter.getCurrency();
   a = Ember.Handlebars.Utils.escapeExpression(n.currency_symbol);
   if (html) {
-      a = "<bdi>" + a + "</bdi>";
+      a = '<bdi>' + a + '</bdi>';
   }
-  n.symbol_first ? (r = "-" === e.charAt(0), e = r ? "-" + a + e.slice(1) : a + e) : e += a;
+  n.symbol_first ? (r = '-' === e.charAt(0), e = r ? '-' + a + e.slice(1) : a + e) : e += a;
   return new Ember.Handlebars.SafeString(e);
 }
 
@@ -32,32 +32,32 @@ function inCurrentMonth() {
     if(inCurrentMonth()) {
       // Make room for the column
       $('#ynab-toolkit-pacing-style').remove();
-      $("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-available { width: 10% !important; } </style>").appendTo("head");
+      $('<style type="text/css" id="ynab-toolkit-pacing-style"> .budget-table-cell-available { width: 10% !important; } </style>').appendTo('head');
     } else {
       $('#ynab-toolkit-pacing-style').remove();
-      $("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-pacing { display: none; } </style>").appendTo("head");
+      $('<style type="text/css" id="ynab-toolkit-pacing-style"> .budget-table-cell-pacing { display: none; } </style>').appendTo('head');
     }
 
 
     $('.budget-table-cell-pacing').remove()
     
-    $('.budget-table-header .budget-table-cell-available').after($("<li class='budget-table-cell-pacing'><strong>PACING</strong></li>"));
+    $(".budget-table-header .budget-table-cell-available").after($('<li class="budget-table-cell-pacing"><strong>PACING</strong></li>'));
     
     $('.budget-table-row').each(function(){ 
-      var available = parseFloat($(this).find('.budget-table-cell-available').text().replace("$","").replace(",","")); 
-      var activity = -parseFloat($(this).find('.budget-table-cell-activity').text().replace("$","").replace(",","")); 
+      var available = ynab.YNABSharedLib.defaultInstance.currencyFormatter.unformat($(this).find('.budget-table-cell-available').text());
+      var activity = -ynab.YNABSharedLib.defaultInstance.currencyFormatter.unformat($(this).find('.budget-table-cell-activity').text());
       var budgeted = available+activity;
       var burned = activity/budgeted;
       var pace = burned/timeSpent();
     
-      var displayType = "dollars";
-      if(displayType == "percentage") {
+      var displayType = 'dollars';
+      if(displayType == 'percentage') {
         if(pace > 1.25) {
-          var temperature = "hot";
+          var temperature = 'hot';
         } else if(pace > .75) {
-          var temperature = "warm";
+          var temperature = 'warm';
         } else {
-          var temperature = "cool";
+          var temperature = 'cool';
         }
         if(!isFinite(pace)) {
           var display = 999;
@@ -65,25 +65,25 @@ function inCurrentMonth() {
           var display = Math.max(0,Math.round(pace*100));
         }
         if(pace > 0) {
-          $(this).append("<li class='budget-table-cell-pacing'><span class='budget-table-cell-pacing-display "+temperature+"'>"+display+"%</span></li>");
+          $(this).append('<li class="budget-table-cell-pacing"><span class="budget-table-cell-pacing-display '+temperature+'">'+display+'%</span></li>');
         }
-      } else if (displayType == "dollars") {
+      } else if (displayType == 'dollars') {
         display = Math.round((budgeted*timeSpent()-activity)*1000);
         if(pace > 1.25) {
-          var temperature = "hot";
+          var temperature = 'hot';
         } else if(pace > 1) {
-          var temperature = "warm";
+          var temperature = 'warm';
         } else if(activity > 0) {
-          var temperature = "cool";
+          var temperature = 'cool';
         } else {
-          var temperature = "neutral";
+          var temperature = 'neutral';
         }
         if(display >= 0) {
-          var tooltip = "You have spent "+ynabEnhancedFormatCurrency(display, true)+" less than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
+          var tooltip = 'You have spent '+ynabEnhancedFormatCurrency(display, true)+' less than your available budget for this category '+Math.round(timeSpent()*100)+'% of the way through the month.';
         } else if(display < 0) {
-          var tooltip = "You have spent "+(ynabEnhancedFormatCurrency(-display), false)+" more than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
+          var tooltip = 'You have spent '+ynabEnhancedFormatCurrency(-display, false)+' more than your available budget for this category '+Math.round(timeSpent()*100)+'% of the way through the month.';
         }
-        $(this).append("<li class='budget-table-cell-pacing'><span title='"+tooltip+"' class='budget-table-cell-pacing-display "+temperature+"'>"+ynabEnhancedFormatCurrency(display, true)+"</span></li>");
+        $(this).append('<li class="budget-table-cell-pacing"><span title="'+tooltip+'" class="budget-table-cell-pacing-display '+temperature+'">'+ynabEnhancedFormatCurrency(display, true)+'</span></li>');
       }
     });
 
