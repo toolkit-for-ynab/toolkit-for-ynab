0a119da96c943fd50c616c9e8089c5639e579867
Stefan Mai <stefan.mai@iamnafets.com>
2016-01-11 00:38:58 -0800

    Make currency format work with the user's settings.

--- src/common/res/features/pacing/main.js
+++ src/common/res/features/pacing/main.js
@@ -1,30 +1,42 @@
+function ynabEnhancedFormatCurrency(e, html) {
+  var n, r, a;
+  e = ynab.formatCurrency(e);
+  n = ynab.YNABSharedLib.currencyFormatter.getCurrency();
+  a = Ember.Handlebars.Utils.escapeExpression(n.currency_symbol);
+  if (html) {
+      a = "<bdi>" + a + "</bdi>";
+  }
+  n.symbol_first ? (r = "-" === e.charAt(0), e = r ? "-" + a + e.slice(1) : a + e) : e += a;
+  return new Ember.Handlebars.SafeString(e);
+}
+
 // Calculate the proportion of the month that has been spent -- only works for the current month
 function timeSpent() {
-	var today = new Date();
-	var daysInMonth = new Date(today.getYear(), today.getMonth(), 0).getDate();
-	var day = today.getDate();
+  var today = new Date();
+  var daysInMonth = new Date(today.getYear(), today.getMonth(), 0).getDate();
+  var day = today.getDate();
 
-	return day/daysInMonth;
+  return day/daysInMonth;
 }
 
 // Determine whether the selected month is the current month
 function inCurrentMonth() {
-	var today = new Date();
-	var selectedMonth = new Date($('.budget-header-calendar-date-button').text());
-	return selectedMonth.getMonth() == today.getMonth() && selectedMonth.getYear() == today.getYear();
+  var today = new Date();
+  var selectedMonth = new Date($('.budget-header-calendar-date-button').text());
+  return selectedMonth.getMonth() == today.getMonth() && selectedMonth.getYear() == today.getYear();
 }
 
 (
  function addPacingColumnToBudget() {
   if (typeof Em !== 'undefined' && typeof Ember !== 'undefined' && typeof $ !== 'undefined') {
     if(inCurrentMonth()) {
-			// Make room for the column
-			$('#ynab-toolkit-pacing-style').remove();
-			$("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-available { width: 10% !important; } </style>").appendTo("head");
-		} else {
-			$('#ynab-toolkit-pacing-style').remove();
-			$("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-pacing { display: none; } </style>").appendTo("head");
-		}
+      // Make room for the column
+      $('#ynab-toolkit-pacing-style').remove();
+      $("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-available { width: 10% !important; } </style>").appendTo("head");
+    } else {
+      $('#ynab-toolkit-pacing-style').remove();
+      $("<style type='text/css' id='ynab-toolkit-pacing-style'> .budget-table-cell-pacing { display: none; } </style>").appendTo("head");
+    }
 
 
     $('.budget-table-cell-pacing').remove()
@@ -67,11 +79,11 @@ function inCurrentMonth() {
           var temperature = "neutral";
         }
         if(display >= 0) {
-          var tooltip = "You have spent $"+ynab.formatCurrency(display)+" less than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
+          var tooltip = "You have spent "+ynabEnhancedFormatCurrency(display, true)+" less than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
         } else if(display < 0) {
-          var tooltip = "You have spent $"+(ynab.formatCurrency(-display))+" more than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
+          var tooltip = "You have spent "+(ynabEnhancedFormatCurrency(-display), false)+" more than your available budget for this category "+Math.round(timeSpent()*100)+"% of the way through the month.";
         }
-        $(this).append("<li class='budget-table-cell-pacing'><span title='"+tooltip+"' class='budget-table-cell-pacing-display "+temperature+"'>$"+ynab.formatCurrency(display)+"</span></li>");
+        $(this).append("<li class='budget-table-cell-pacing'><span title='"+tooltip+"' class='budget-table-cell-pacing-display "+temperature+"'>"+ynabEnhancedFormatCurrency(display, true)+"</span></li>");
       }
     });
 
