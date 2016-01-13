6c5a9880fe5043a6692cae73436157b7c1c20c19
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-14 00:16:47 +1100

    Added browser specific workaround for needing the "tabs" permission just to open up the settings page. Also bumped the version number.

--- src/common/popup/popup.js
+++ src/common/popup/popup.js
@@ -1,7 +1,17 @@
 KangoAPI.onReady(function() {
 
   $('#openSettings').click(function() {
-    kango.ui.optionsPage.open();
+
+    // Chrome requires the "tabs" permission to open the options page.
+    // We don't want to ask for this permission because it has an ominous
+    // message about being able to read your entire browsing history.
+    // For some reason, kango isn't using chrome.runtime.openOptionsPage().
+    if (kango.browser.getName() == 'chrome') {
+      chrome.runtime.openOptionsPage();
+    } else {
+      kango.ui.optionsPage.open();
+    }
+    
     KangoAPI.closeWindow();
   });
 
