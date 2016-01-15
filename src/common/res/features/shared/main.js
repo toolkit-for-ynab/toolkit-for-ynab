a0a6178b20d714c7e309d302b5f3789b0b87904a
Johannes DeMattia <johannes@demattia.se>
2016-01-15 21:43:30 +0100

    Finished rewrite of ynabToolKit.getVisibleTransactions()

--- src/common/res/features/shared/main.js
+++ src/common/res/features/shared/main.js
@@ -10,77 +10,77 @@ window.ynabToolKit = new function() {
     // If accountId === 'null' then all transactions for all accounts are returned with the visibility
     // settings for All accounts applied.
     this.getVisibleTransactions = function (accountId)  {
-        var transactions, originalTransactions, n, r, a, endDate, o, s, startMonth, startYear, d, u, scheduledTransactions, p, m, g, f, y, v, b, C, _, showReconciled, T, accountSettings, A, E, x, singleOccurringTransactions, F, I, S, startDateUTC, D, subTransactions, endMonth, endYear, R, O, Y, visibleTransactions, showNotifications;
+        var transactions, originalTransactions, endDate, endDateUTC, sortBySortableIndex, accountStartMonth, accountStartYear, subTransactionsAdded, scheduledTransactions, addSubTransactionToVisibleTransactions, transactionPosition, accountShowReconciled, accountSettings, subTransaction, singleOccurranceTransactions, accountShowScheduled, startDateUTC, sortedSubTransactions, subTransactions, accountEndMonth, accountEndYear, visibleTransactions, accountShowWithNotifications, b, f;
         if (accountId === 'null') {
             transactions = ynab.YNABSharedLib.getBudgetViewModel_AllAccountTransactionsViewModel()._result.visibleTransactionDisplayItems;
         } else {
             transactions = ynab.YNABSharedLib.getBudgetViewModel_AllAccountTransactionsViewModel()._result.transactionDisplayItemsCollection.findItemsByAccountId(accountId);
         }
         originalTransactions = transactions;
-        accountSettings = jQuery.parseJSON(localStorage.getItem("." + accountId + "_account_filter")),
-        startMonth = accountSettings.fromMonth,
-        startYear = accountSettings.fromYear,
-        endMonth = accountSettings.toMonth,
-        endYear = accountSettings.toYear,
-        showReconciled = accountSettings.reconciled,
-        showScheduled = accountSettings.scheduled,
-        showNotifications = accountSettings.withNotification,
-        null !== startMonth && null !== startYear && (startDateUTC = new ynab.utilities.DateWithoutTime(startYear, startMonth).getUTCTime()),
-        null !== endMonth && null !== endYear && (endDate = new ynab.utilities.DateWithoutTime(endYear, endMonth),
-        endDate.addMonths(1),
-        endDateUTC = endDate.getUTCTime()),
-        scheduledTransactions = Object.create(null),
-        subTransactions = Object.create(null),
-        singleOccurringTransactions = [];
+        accountSettings = jQuery.parseJSON(localStorage.getItem("." + accountId + "_account_filter"));
+        accountStartMonth = accountSettings.fromMonth;
+        accountStartYear = accountSettings.fromYear;
+        accountEndMonth = accountSettings.toMonth;
+        accountEndYear = accountSettings.toYear;
+        accountShowReconciled = accountSettings.reconciled;
+        accountShowScheduled = accountSettings.scheduled;
+        accountShowWithNotifications = accountSettings.withNotification;
+        if (null !== accountStartMonth && null !== accountStartYear) {
+            startDateUTC = new ynab.utilities.DateWithoutTime(accountStartYear, accountStartMonth).getUTCTime();
+        }
+        if (null !== accountEndMonth && null !== accountEndYear) {
+            endDate = new ynab.utilities.DateWithoutTime(accountEndYear, accountEndMonth);
+            endDate.addMonths(1);
+            endDateUTC = endDate.getUTCTime();
+        }
+        scheduledTransactions = Object.create(null);
+        subTransactions = Object.create(null);
+        singleOccurranceTransactions = [];
         visibleTransactions = transactions.filter(function(transaction) {
             var transactionCleared, transactionDate, transactionDateUTC, transactionDisplayItemType, transactionEntityId, transactionIsSplit, transactionIsTombstone, transactionNeedsApproval, transactionNeedsCategory, parentEntityId, transactionProperties;
-            return transactionProperties = transaction.getProperties("entityId", "isTombstone", "displayItemType", "date", "cleared", "needsApproval", "needsCategory", "isSplit"),
-            transactionEntityId = transactionProperties.entityId, //s
-            transactionIsTombstone = transactionProperties.isTombstone, //l
-            transactionDisplayItemType = transactionProperties.displayItemType, //o
-            transactionDate = transactionProperties.date, //r
-            transactionCleared = transactionProperties.cleared, //t
-            transactionNeedsApproval = transactionProperties.needsApproval, //d
-            transactionNeedsCategory = transactionProperties.needsCategory, //u
-            transactionIsSplit = transactionProperties.isSplit, //c
-            transactionIsTombstone ? (!1) : (transactionDateUTC = transactionDate.getUTCTime(),
-            startDateUTC && startDateUTC > transactionDateUTC || endDateUTC && transactionDateUTC >= endDateUTC ? !1 : showReconciled === !1 && transactionCleared === ynab.constants.TransactionState.Reconciled ? !1 : "needsApproval" === showNotifications && !transactionNeedsApproval || "needsCategory" === showNotifications && (!transactionNeedsCategory || transactionNeedsApproval) ? !1 : transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.ScheduledTransaction ? (showScheduled && (singleOccurringTransactions.push(transaction),
-            scheduledTransactions[transactionEntityId] = transaction),
-            !1) : transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.SubTransaction || transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.ScheduledSubTransaction ? (parentEntityId = transaction.get("parentEntityId"),
-            Array.isArray(subTransactions[parentEntityId]) || (subTransactions[parentEntityId] = []),
-            subTransactions[parentEntityId].push(transaction),
-            !1) : (transactionIsSplit && (scheduledTransactions[transactionEntityId] = transaction),
-            !0))
-        }),
-        visibleTransactions.push.apply(visibleTransactions, singleOccurringTransactions),
-        p = function(transactions, originalTransactions) {
-            var n;
-            return n = visibleTransactions.length,
-            transactions >= n ? visibleTransactions.push(originalTransactions) : visibleTransactions.splice(transactions, 0, originalTransactions)
-        },
-        s = function(transactions) {
-            return transactions.sortBy("sortableIndex")
+            transactionProperties = transaction.getProperties("entityId", "isTombstone", "displayItemType", "date", "cleared", "needsApproval", "needsCategory", "isSplit");
+            transactionEntityId = transactionProperties.entityId;
+            transactionIsTombstone = transactionProperties.isTombstone;
+            transactionDisplayItemType = transactionProperties.displayItemType;
+            transactionDate = transactionProperties.date;
+            transactionCleared = transactionProperties.cleared;
+            transactionNeedsApproval = transactionProperties.needsApproval;
+            transactionNeedsCategory = transactionProperties.needsCategory;
+            transactionIsSplit = transactionProperties.isSplit;
+            transactionDateUTC = transactionDate.getUTCTime();
+            return transactionIsTombstone ? false :
+            (startDateUTC && startDateUTC > transactionDateUTC || endDateUTC && transactionDateUTC >= endDateUTC ? false :
+            accountShowReconciled === false && transactionCleared === ynab.constants.TransactionState.Reconciled ? false :
+            "needsApproval" === accountShowWithNotifications && !transactionNeedsApproval || "needsCategory" === accountShowWithNotifications && (!transactionNeedsCategory || transactionNeedsApproval) ? false :
+            transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.ScheduledTransaction ? (accountShowScheduled && (singleOccurranceTransactions.push(transaction),
+            scheduledTransactions[transactionEntityId] = transaction), false) :
+            transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.SubTransaction || transactionDisplayItemType === ynab.constants.TransactionDisplayItemType.ScheduledSubTransaction ? (parentEntityId = transaction.get("parentEntityId"),
+            Array.isArray(subTransactions[parentEntityId]) || (subTransactions[parentEntityId] = []), subTransactions[parentEntityId].push(transaction), false) :
+            (transactionIsSplit && (scheduledTransactions[transactionEntityId] = transaction), true));
+        });
+        visibleTransactions.push.apply(visibleTransactions, singleOccurranceTransactions);
+        addSubTransactionToVisibleTransactions = function(transactionPosition, subTransaction) {
+            var n = visibleTransactions.length;
+            return transactionPosition >= n ? visibleTransactions.push(subTransaction) : visibleTransactions.splice(transactionPosition, 0, subTransaction);
         };
-        for (scheduledTransaction in scheduledTransactions)
-            if (_ = visibleTransactions.indexOf(scheduledTransactions[scheduledTransaction]),
-            -1 !== _)
-                if (n === scheduledTransaction && r.length > 0) {
-                    for (A = s(r),
-                    d = g = 0,
-                    v = A.length; v > g; d = ++g)
-                        D = A[d],
-                        p(d + _ + 1, D);
-                    p(r.length + _ + 1, {
-                        type: "split"
-                    })
-                } else if (subTransactions[scheduledTransaction])
-                    for (d = 0,
-                    E = s(subTransactions[scheduledTransaction]),
-                    f = 0,
-                    b = E.length; b > f; f++)
-                        D = E[f],
-                        D.get("isTombstone") || (d++,
-                        p(d + _, D));
+        sortBySortableIndex = function(e) {
+            return e.sortBy("sortableIndex");
+        };
+        for (scheduledTransaction in scheduledTransactions) {
+            transactionPosition = visibleTransactions.indexOf(scheduledTransactions[scheduledTransaction]);
+            if (transactionPosition !== false) {
+                if (subTransactions[scheduledTransaction]) {
+                    subTransactionsAdded = 0;
+                    sortedSubTransactions = sortBySortableIndex(subTransactions[scheduledTransaction]);
+                    f = 0;
+                    for (b = sortedSubTransactions.length; b > f; f++) {
+                        subTransaction = sortedSubTransactions[f];
+                        subTransaction.get("isTombstone") || (subTransactionsAdded++,
+                        addSubTransactionToVisibleTransactions(subTransactionsAdded + transactionPosition, subTransaction));
+                    }
+                }
+            }
+        }
         return visibleTransactions;
     };
 
