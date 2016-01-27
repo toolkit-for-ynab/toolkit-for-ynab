90df181892ff8bfc20fc43a11f7b9d251aedc6fd
Kevin Brown <kevinbrowntech@gmail.com>
2016-01-27 20:48:15 +1100

    Added the @2x icon for Safari.

--- build.bat
+++ build.bat
@@ -3,8 +3,9 @@
 python populateFeaturesFiles.py
 python lib/kango-framework-latest/kango.py build .
 
-echo "[   INFO] Moving Safari extension icon into place..."
+echo "[   INFO] Moving Safari extension icons into place..."
 pushd output\safari\*.safariextension\icons
   del button.png
   move button-safari.png button.png
+  move button-safari@2x.png button@2x.png
 popd
