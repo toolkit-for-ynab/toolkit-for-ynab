ab73e0177f54d3570245cb2f6b04adcd11957121
Nathanael Ritz <nathanritz@gmail.com>
2016-01-30 19:59:22 -0700

    Refactor reconciled text color to use MutationObserver
    Re-commit (original 15928554d8b5191c7ad947d2de09b34334722575).
    
    Refactored the reconciled text color
    feature to use the MutationObserver and confirmed that the autobuild of
    feedChanges.js works as expected.

--- src/common/res/features/reconciled-text-color/main.js
+++ src/common/res/features/reconciled-text-color/main.js
@@ -1,22 +1,40 @@
-(function addIsReconciledClassForRows() {
-   
-  if ( typeof Em !== 'undefined' && typeof Ember !== 'undefined' && typeof $ !== 'undefined' ) {
-    
-    var transactionRows = $('.ynab-grid-body-row');
-    var previousReconciled = false;
-    $(transactionRows).each(function(i) {
-      clearedField = $(this).find(".ynab-grid-cell-cleared>i").first();
-      isReconciled = clearedField.hasClass("is-reconciled");
-      if (isReconciled) {
-        $(this).addClass("is-reconciled-row");
-      }
-      if ($(this).hasClass("ynab-grid-body-sub") && previousReconciled) {
-        $(this).addClass("is-reconciled-row");
-        isReconciled = true;
-      }
-      previousReconciled = isReconciled;
-    })
-  }
-  setTimeout(addIsReconciledClassForRows, 300);
+(function poll() {
+  // Waits until an external function gives us the all clear that we can run (at /shared/main.js)
+  if ( typeof ynabToolKit !== "undefined"  && ynabToolKit.pageReady === true ) {
 
+    ynabToolKit.reconciledTextColor = new function ()  { // Keep feature functions contained within this
+
+      this.invoke = function() {
+
+        var transactionRows = $('.ynab-grid-body-row');
+        var previousReconciled = false;
+        $(transactionRows).each(function(i) {
+          clearedField = $(this).find(".ynab-grid-cell-cleared>i").first();
+          isReconciled = clearedField.hasClass("is-reconciled");
+          if (isReconciled) {
+            $(this).addClass("is-reconciled-row");
+          }
+          if ($(this).hasClass("ynab-grid-body-sub") && previousReconciled) {
+            $(this).addClass("is-reconciled-row");
+            isReconciled = true;
+          }
+          previousReconciled = isReconciled;
+        })
+      },
+
+      this.observe = function(changedNodes) {
+
+        if (changedNodes.has('ynab-grid-body')) {
+          // We found Account transactions rows
+          ynabToolKit.reconciledTextColor.invoke();
+        }
+
+      };
+
+    }; // Keep feature functions contained within this
+    ynabToolKit.reconciledTextColor.invoke(); // run once on page load
+
+  } else {
+    setTimeout(poll, 250);
+  }
 })();
