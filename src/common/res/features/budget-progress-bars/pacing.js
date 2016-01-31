be3aa2c8a8ed41be3b8bbed6cface487ee486c81
Nathanael Ritz <nathanritz@gmail.com>
2016-01-31 13:57:56 -0700

    Address #115 where progress bars did not display correctly
    Based on the suggestion from @dbaldon (
    https://github.com/blargity/toolkit-for-ynab/issues/115#issuecomment-177597447
    ), this should keep the progress bars and the goal indicator present
    between multiple months.

--- src/common/res/features/budget-progress-bars/pacing.js
+++ src/common/res/features/budget-progress-bars/pacing.js
@@ -73,8 +73,7 @@
 
       this.observe = function(changedNodes) {
 
-      if (changedNodes.has('navlink-budget') && changedNodes.has('active')) {
-          // The user has returned back to the budget screen
+        if ( (changedNodes.has('navlink-budget') && changedNodes.has('active') ) || changedNodes.has('budget-inspector') ) {
           ynabToolKit.budgetProgressBars.invoke();
         }
       };
