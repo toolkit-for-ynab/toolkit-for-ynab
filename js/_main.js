1518be63db16a10e358b21610c668bc0f1d9fe8c
John Pethers <john.pethers@make.com.au>
2016-02-04 10:55:32 +1100

    Added skrollr for parallax

---
+++ js/_main.js
@@ -0,0 +1,9 @@
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
