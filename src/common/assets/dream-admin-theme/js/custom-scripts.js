c43e4b04a4fde43eb1ae706da8ab9580fa03c164
Kevin Brown <kevinbrowntech@gmail.com>
2016-02-11 23:40:08 +1100

    Replacing minified sources with unminified versions as per request from AMO editors (Firefox Review).

--- src/common/assets/dream-admin-theme/js/custom-scripts.js
+++ src/common/assets/dream-admin-theme/js/custom-scripts.js
@@ -4,34 +4,28 @@
     http://creativecommons.org/licenses/by/3.0/
 ---------------------------------------------------------  */
 
-(function ($) {
-    "use strict";
-    var mainApp = {
+(function($) {
+  "use strict";
+  var mainApp = {
 
-        initFunction: function () {
-            /*MENU
-            ------------------------------------*/
-            $('#main-menu').metisMenu();
+      initFunction: function() {
+        $(window).bind("load resize", function() {
+          if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+          } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+          }
+        });
+      },
 
-            $(window).bind("load resize", function () {
-                if ($(this).width() < 768) {
-                    $('div.sidebar-collapse').addClass('collapse')
-                } else {
-                    $('div.sidebar-collapse').removeClass('collapse')
-                }
-            });
-        },
-
-        initialization: function () {
-            mainApp.initFunction();
-
-        }
-
+      initialization: function() {
+        mainApp.initFunction();
+      }
     }
     // Initializing ///
 
-    $(document).ready(function () {
-        mainApp.initFunction();
-    });
+  $(document).ready(function() {
+    mainApp.initFunction();
+  });
 
 }(jQuery));
