dd270282b8642a5f8c45d53f64e67c2aaec8cb81
Benjamin Falk <ben@falkencreative.com>
2016-04-03 18:42:20 -0400

    Highlight Negatives Feature rewrite
    Code cleanup, bug fixes and hopefully a Safari fix for #189.

--- src/common/res/features/highlight-negatives-negative/main.js
+++ src/common/res/features/highlight-negatives-negative/main.js
@@ -1,36 +1,40 @@
 (function poll() {
   if ( typeof ynabToolKit !== "undefined" && ynabToolKit.pageReady === true ) {
+  
     ynabToolKit.highlightNegativesNegative = (function(){
+    
       return {
         invoke: function() {
-          var availableBalances = $('.budget-table-cell-available-div.user-data');
-          var categoryBalance = {};
-
-          $(availableBalances).each(function () {
-            categoryBalance = $(this).find('.user-data.currency').html(); // get the value
-              categoryBalance = Number( categoryBalance.replace(/[^\d.-]/g, '') ); // force data type as number
-
-
-            if ( categoryBalance < 0 ) {
-
-                if ( $(this).find('.user-data.currency').hasClass('cautious') ) {
-                  $(this).find('.user-data.currency').removeClass('cautious').addClass('negative');
-                }
-            }
-
-          });
+        
+          // look in budget, inspector
+      	  $.each($('.budget-table-cell-available-div.user-data .cautious, .budget-inspector .cautious'), function(key, val) {
+      		
+      		var num = $(this).text();
+      		if (parseInt(num.replace('$', '')) < 0 )
+      		{
+      			$(this).removeClass('cautious').addClass('negative');
+      		}
+      	  });
         },
 
         observe: function(changedNodes) {
-          if (changedNodes.has('budget-inspector')) {
-            // The user has changed their budget row selection
+        
+          // look for changes to budgeting cells, inspector
+          if (
+            changedNodes.has('navlink-budget active') ||
+            changedNodes.has('budget-table-cell-available-div user-data') ||
+          	changedNodes.has('budget-inspector')) {
             ynabToolKit.highlightNegativesNegative.invoke();
           }
         }
+        
       };
     })(); // Keep feature functions contained within this object
 
-    ynabToolKit.highlightNegativesNegative.invoke(); // call itself once
+	// run on load if in budgeting section
+	if (/budget/.test(window.location.href)) {
+	  ynabToolKit.highlightNegativesNegative.invoke();
+	}
 
   } else {
     setTimeout(poll, 250);
