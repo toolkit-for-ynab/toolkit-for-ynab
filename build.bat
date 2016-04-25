c6dde1e13f92822033b02d64ba38d0df5712fa4f
Kevin Brown <kevinbrowntech@gmail.com>
2016-04-25 17:45:33 +1000

    Updated docs and took a first stab at build.bat script from Mac. Will test on windows machine shortly.

--- build.bat
+++ build.bat
@@ -1,11 +1,22 @@
 @ECHO OFF
 
-python populateFeaturesFiles.py
-python lib/kango-framework-latest/kango.py build .
+echo "[   INFO] Checking code style with JSCS..."
+npm run jscs --silent || exit
 
-echo "[   INFO] Moving Safari extension icons into place..."
-pushd output\safari\*.safariextension\icons
-  del button.png
-  move button-safari.png button.png
-  move button-safari@2x.png button@2x.png
-popd
+# Populate feature files like feed changes by reading through the code and hooking up
+# all the calls we need, as well as processing settings files
+python populateFeaturesFiles.py
+
+# Transpile the source/ directory, putting the files in src/ which is where Kango expects to see them.
+echo "[   INFO] Transpiling code with Babel on ES2015 preset..."
+rd /s /q src
+npm run babel --silent || exit
+
+# Copy any non JS files across that babel didn't bring with it. Ignore existing files we just transpiled.
+robocopy source src /E /XC /XN /XO
+
+# Run the Kango build.
+python lib\kango-framework-latest\kango.py build .
+
+# Clean up.
+rd /s /q src
