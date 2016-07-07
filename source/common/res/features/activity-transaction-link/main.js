e8b0c1e7d17fd8b9ed114f106b6cb77f107d8102
Josh Madewell <josh.madewell@gmail.com>
2016-07-07 12:11:24 -0500

    feature to link activity rows to their transaction on the accounts page

--- source/common/res/features/activity-transaction-link/main.js
+++ source/common/res/features/activity-transaction-link/main.js
@@ -1,18 +1,67 @@
 (function poll() {
   if (typeof ynabToolKit !== 'undefined' && ynabToolKit.pageReady === true) {
     ynabToolKit.activityTransactionLink = (function () {
+      var selectedTransaction;
+      var waitForRowView = false;
+
       function initialize() {
-        console.log('doin it');
+        var budgetController = ynabToolKit.shared.containerLookup('controller:budget');
+        var selectedActivityTransactions = budgetController.selectedActivityTransactions;
+
+        $('.activity-rows').children().each(function (index, row) {
+          $(row).on('click', function () {
+            selectedTransaction = selectedActivityTransactions[index];
+            $('.nav-account-name[title="' + selectedTransaction.accountName + '"]').trigger('click');
+            waitForRowView = true;
+          });
+        });
+      }
+
+      function resetFiltersAndShowSelectedTransaction(accountsController) {
+        function getTransactionIndex() {
+          accountsController.removeObserver('contentResults', getTransactionIndex);
+          var contentResults = accountsController.get('contentResults');
+          var transactionIndex = contentResults.indexOf(selectedTransaction);
+          showSelectedTransaction(accountsController, contentResults, transactionIndex);
+        }
+
+        accountsController.addObserver('contentResults', getTransactionIndex);
+        accountsController.filters.resetFilters();
+      }
+
+      function findSelectedTransaction() {
+        var accountsController = ynabToolKit.shared.containerLookup('controller:accounts');
+        var contentResults = accountsController.get('contentResults');
+        var transactionIndex = contentResults.indexOf(selectedTransaction);
+
+        if (transactionIndex === -1) {
+          resetFiltersAndShowSelectedTransaction(accountsController);
+        } else {
+          showSelectedTransaction(accountsController, contentResults, transactionIndex);
+        }
+      }
+
+      function showSelectedTransaction(accountsController, contentResults, transactionIndex) {
+        var rowView = ynabToolKit.shared.getEmberViewByContainerKey('view:ynab-grid/rows');
+        var containerView = rowView.get('containerView');
+        var recordHeight = rowView.get('recordHeight');
+        var container = containerView.get('element');
+        $(container).scrollTop(recordHeight * transactionIndex);
+        rowView.gridView.uncheckAllBut(selectedTransaction);
       }
 
       return {
-        invoke() {
+        invoke: function invoke() {
           Ember.Instrumentation.subscribe('render.view', {
-            before: function () {},
-            after: function (event, timestamp, view) {
-              console.log('in here', view);
+            before: function before() {},
+            after: function after(event, timestamp, view) {
               if (view.containerKey === 'view:modals/budget/activity') {
-                initialize();
+                return initialize();
+              }
+
+              if (view.containerKey === 'view:ynab-grid/rows' && waitForRowView) {
+                waitForRowView = false;
+                setTimeout(findSelectedTransaction, 250);
               }
             }
           });
