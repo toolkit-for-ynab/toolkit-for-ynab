9c387ff0c9842eea7fcd8ee9923d029787cc0db6
Josh Madewell <josh.madewell@gmail.com>
2016-09-08 07:39:31 -0700

    don't need the displayEnd logic anymore

--- source/common/res/features/toggle-splits/main.js
+++ source/common/res/features/toggle-splits/main.js
@@ -1,63 +1,11 @@
 (function poll() {
   if (typeof ynabToolKit !== 'undefined' && ynabToolKit.actOnChangeInit === true) {
     ynabToolKit.toggleSplits = (function () {
-      var calculatingDisplayEnd = false;
-
-      function setDisplayEnd() {
-        if (ynabToolKit.toggleSplits.setting === 'show') return;
-
-        calculatingDisplayEnd = true;
-        var rowView = ynabToolKit.shared.getEmberViewByContainerKey('view:ynab-grid/rows');
-        var accountsController = ynabToolKit.shared.containerLookup('controller:accounts');
-        var contentResults = accountsController.get('contentResults');
-        var containerView = rowView.get('containerView');
-        var gridView = rowView.get('gridView');
-        var displayStart = containerView.get('displayStart');
-        var recordsPerBody = containerView.get('recordsPerBody');
-
-        // this calculation was taken from YNAB's current codebase
-        var wouldBeDisplayEnd = Math.round(Math.min(displayStart + 4 * recordsPerBody, gridView.get('total')));
-
-        var fitOnPage = wouldBeDisplayEnd - displayStart;
-        var displayedCount = 0;
-        var displayEnd = displayStart;
-
-        for (var i = displayStart; i < contentResults.length; i++) {
-          if (displayedCount === fitOnPage) break;
-
-          if (contentResults[i].get('parentEntityId')) {
-            if (ynabToolKit.toggleSplits.setting === 'hide') {
-              displayEnd++;
-              continue;
-            } else {
-              displayedCount++;
-            }
-          } else {
-            displayedCount++;
-            displayEnd++;
-          }
-        }
-
-        Ember.run.next(function () {
-          containerView.set('displayEnd', displayEnd);
-          calculatingDisplayEnd = false;
-        });
-      }
-
-      function addScrollListener() {
-        var rowView = ynabToolKit.shared.getEmberViewByContainerKey('view:ynab-grid/rows');
-        rowView.get('containerView').addObserver('scrollTop', setDisplayEnd);
-
-        if (rowView.get('containerView').get('scrollTop') === 0) {
-          setDisplayEnd();
-        }
-      }
-
       function hideSubTransactions() {
         ynabToolKit.toggleSplits.setting = 'hide';
         $('.ynab-grid-body .ynab-grid-body-sub:not(.is-editing)').hide();
         $(".ynab-grid-cell-subCategoryName[title^='Split']").css('font-weight', 700);
-        setDisplayEnd();
+        // setDisplayEnd();
       }
 
       function showSubTransactions() {
@@ -95,17 +43,12 @@
 
               $('#toggle-splits > i').toggle();
             });
-
-            addScrollListener();
           }
         },
 
         observe: function observe(changedNodes) {
           if (changedNodes.has('ynab-grid-body')) {
             ynabToolKit.toggleSplits.invoke();
-            if (!calculatingDisplayEnd) {
-              setDisplayEnd();
-            }
           }
         }
       };
