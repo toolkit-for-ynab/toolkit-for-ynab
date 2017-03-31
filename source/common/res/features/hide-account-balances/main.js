6714b8a14014002a324d4c7a7c04aec9a895a223
U-FINANCE\ncjfsm <ncjfsm@FSA-700.finance.rit.edu>
2017-03-31 14:14:05 -0400

    Fixed main.js to match current standard.

--- source/common/res/features/hide-account-balances/main.js
+++ source/common/res/features/hide-account-balances/main.js
@@ -1,28 +1,28 @@
 (function poll() {
-  if (typeof ynabToolKit !== 'undefined' && ynabToolKit.pageRead === true) {
-    
-    function exportData () {
-    }
-    
-    return {
-      invoke() {
-        // Does nothing for now.
-        // Should perform any setup.
-      }
+	if (typeof ynabToolKit !== 'undefined' && ynabToolKit.pageReady === true) {
+		ynabToolKit.hideAccountBalancesFeature = (function(){
+			// Supporting functions or variables.
+		
+			return {
+				invoke() {
+					// Does nothing for now.
+					// Action is to delete or hide nav-account-value for either.
+				}
       
-      observe(changedNodes) {
-        // Does nothing for now.
-        // Should perform any actions related to changed nodes.
-	      // Need to monitor nav-accounts when active
-	      // Need to monitor nav-account-block when looking for type
-	      // Need to monitor nav-account-row when looking for indiv accounts
-	      // Action is to delete or hide nav-account-value for either.
-      }
-      
-      // Functions accessible from elsewhere.
-    }
-  }
-  else {
-    setTimeout(poll, 250);
-  }
+				observe(changedNodes) {
+					// Does nothing for now.
+					// Should perform any actions related to changed nodes.
+					// Need to monitor nav-accounts when active
+					// Need to monitor nav-account-block when looking for type
+					// Need to monitor nav-account-row when looking for indiv accounts
+					//ynabToolKit.hideAccountBalancesFeature.invoke();
+				}
+			}
+		});
+		
+		ynabToolKit.hideAccountBalancesFeature.invoke(); // Run on load.
+	}
+	else {
+		setTimeout(poll, 250);
+	}
 }());
