d4bb2dabd5b093c8569bc08c7cadd42c32b0a6ca
U-FINANCE\ncjfsm <ncjfsm@FSA-700.finance.rit.edu>
2017-03-31 14:37:52 -0400

    Added some basic logic and a note to see if it can be done similarly to import-notification.

--- source/common/res/features/hide-account-balances/main.js
+++ source/common/res/features/hide-account-balances/main.js
@@ -2,20 +2,34 @@
 	if (typeof ynabToolKit !== 'undefined' && ynabToolKit.pageReady === true) {
 		ynabToolKit.hideAccountBalancesFeature = (function(){
 			// Supporting functions or variables.
+			// Note: May want to use jquery's .hide function and follow the example of "import-notification"
+			var nodes;
 		
 			return {
 				invoke() {
 					// Does nothing for now.
 					// Action is to delete or hide nav-account-value for either.
+
+					// Need to monitor nav-account-block when looking for type
+					// Need to monitor nav-account-row when looking for indiv accounts
+					
+					if (hideAccountTypeTotals && nodes.has('nav-account-block')) {
+						// Hide type balance
+					}
+					
+					if (hideAccountTotals && nodes.has('nav-account-row')) {
+						// Hide account balance.
+					}
 				}
       
 				observe(changedNodes) {
+					if (changedNodes.has('nav-accounts')) {
+						nodes = changedNodes;
+						ynabToolKit.hideAccountBalancesFeature.invoke();
+					}
 					// Does nothing for now.
 					// Should perform any actions related to changed nodes.
 					// Need to monitor nav-accounts when active
-					// Need to monitor nav-account-block when looking for type
-					// Need to monitor nav-account-row when looking for indiv accounts
-					//ynabToolKit.hideAccountBalancesFeature.invoke();
 				}
 			}
 		});
