2bd69f5e2f852679af896ca6f55423017974e519
Alan Hoskins <alanhoskins@gmail.com>
2017-04-04 23:43:12 -0400

    Code refactoring. Move menu.

--- sauce/features/accounts/customFlagNames/index.js
+++ sauce/features/accounts/customFlagNames/index.js
@@ -1,6 +1,7 @@
 import Feature from 'core/feature';
 import * as toolkitHelper from 'helpers/toolkit';
 import flags from './flags.json';
+const fs = require('fs');
 
 const redFlagLabel = flags.red.label;
 const blueFlagLabel = flags.blue.label;
@@ -9,32 +10,9 @@ const yellowFlagLabel = flags.yellow.label;
 const greenFlagLabel = flags.green.label;
 const purpleFlagLabel = flags.purple.label;
 
-const $modalOverlay =
-  $('<div class="modal-generic modal-overlay active" style="display:none;"></div>');
-const $modal = $('<div id="flags-modal" class="modal"></div>');
-const $modalHeader = $('<div class="modal-header">Customize Flag Names</div><hr>');
-const $modalContent = $('<div class="modal-content"><ul class="flags-list"></ul></div><hr>');
-const $modalActions =
-  $('<div class="modal-actions">' +
-    '<button class="button button-primary" id="flags-save">OK <i class="flaticon stroke checkmark-2"><!----></i></button>' +
-    '<button class="button button-cancel" id="flags-cancel">Cancel <i class="flaticon stroke x-2"><!----></i></button>' +
-    '</div>' +
-    '<div class="modal-arrow" style="position:absolute;width: 0;height: 0;bottom: 100%;left:150px;border: solid transparent;border-color: transparent;border-width: 15px;border-bottom-color: #ffffff"></div>');
-
 export default class CustomFlagNames extends Feature {
   constructor() {
     super();
-  }
-
-  setupModal() {
-    $modalOverlay.append($modal);
-    $modal.append($modalHeader);
-    $modal.append($modalContent);
-    $modal.append($modalActions);
-    $('body').append($modalOverlay);
-    this.addFlagListItems();
-    this.setModalStyles();
-    this.addEventListeners();
   }
 
   shouldInvoke() {
@@ -62,66 +40,61 @@ export default class CustomFlagNames extends Feature {
 
     if (changedNodes.has('ynab-u modal-popup modal-account-flags ember-view modal-overlay active')) {
       $('.ynab-flag-red .label, .ynab-flag-red .label-bg').html(redFlagLabel);
-      // $('.ynab-flag-red .label-bg').html(redFlagLabel);
       $('.ynab-flag-blue .label, .ynab-flag-blue .label-bg').html(blueFlagLabel);
-      // $('.ynab-flag-blue .label-bg').html(blueFlagLabel);
       $('.ynab-flag-orange .label, .ynab-flag-orange .label-bg').html(orangeFlagLabel);
-      // $('.ynab-flag-orange .label-bg').html(orangeFlagLabel);
       $('.ynab-flag-yellow .label, .ynab-flag-yellow .label-bg').html(yellowFlagLabel);
-      // $('.ynab-flag-yellow .label-bg').html(yellowFlagLabel);
       $('.ynab-flag-green .label, .ynab-flag-green .label-bg').html(greenFlagLabel);
-      // $('.ynab-flag-green .label-bg').html(greenFlagLabel);
       $('.ynab-flag-purple .label, .ynab-flag-purple .label-bg').html(purpleFlagLabel);
-      // $('.ynab-flag-purple .label-bg').html(purpleFlagLabel);
+
+      $('.modal-account-flags .modal').css({ height: '22em' })
+        .append('<div id="account-flags-actions" style="padding: 0 .3em"><button id="flags-edit" class="button button-primary">Edit <i class="flaticon stroke compose-3"><!----></i></button></div>');
+
+      this.addEventListeners();
     }
   }
 
   onRouteChanged() {
     if (!this.shouldInvoke()) return;
-    this.addFlagsMenu();
-    this.setupModal();
+
     this.invoke();
   }
 
-  addFlagsMenu() {
-    $('.accounts-toolbar-right')
-      .append('<button id="flags-menu" title="Edit Flag Names" class="button">Flags<i class="flaticon stroke down"><!----></i></button>');
-  }
-
-  addFlagListItems() {
-    $('ul.flags-list').empty();
-    for (var key in flags) {
-      var flag = flags[key];
-      $('ul.flags-list').append('<li><input type="text" style="color: #fff; fill: ' + flag.color + '; background-color: ' + flag.color + '; height: 30px; padding:.5em .7em;" value="' + flag.label + '" placeholder="' + flag.label + '" /></li>');
-    }
-  }
-
-  setModalStyles() {
-    $('ul.flags-list').css({ 'text-align': 'left', padding: '.4em 0', margin: '0', zoom: '1', 'list-style-type': 'none' });
-    $('ul.flags-list li').css({ color: '#588697', 'text-align': 'left', 'list-style': 'none', 'font-size': '1em', padding: '0 .3em', 'padding-bottom': '.3em', position: 'relative' });
-  }
-
   addEventListeners() {
-    $('#flags-menu').click(function () {
-      const offset = $('#flags-menu').offset();
-      const height = $('#flags-menu').height();
-      const width = $('#flags-menu').width();
-      const top = offset.top + height + 30;
-      const left = offset.left + width - $modal.width() / 2;
-      $modal.css({ width: '20em', left: left, top: top });
-      $modalOverlay.show();
-    });
-    $('#flags-cancel').click(function () {
-      $modalOverlay.hide();
-    });
-    $('#flags-save').click(function () {
-      $modalOverlay.hide();
-    });
-    $modalOverlay.click(function () {
-      $modalOverlay.hide();
-    });
-    $modal.click(function (event) {
-      event.stopPropagation();
+    $('#flags-edit').click(function () {
+      $('.modal-account-flags .modal-list').empty();
+
+      for (var key in flags) {
+        let flag = flags[key];
+        $('.modal-account-flags .modal-list').append('<li><input type="text" id="' + key + '" class="flag-input" style="color: #fff; fill: ' + flag.color + '; background-color: ' + flag.color + '; height: 30px; padding:0 .7em; margin-bottom: .3em; border: none;" value="' + flag.label + '" placeholder="' + flag.label + '" /></li>');
+      }
+
+      $('#account-flags-actions').empty();
+
+      $('#account-flags-actions').append('<button id="flags-cancel" class="button button-primary">Cancel <i class="flaticon stroke x-2"><!----></i></button>');
+
+      $('#account-flags-actions').append('<button id="flags-save" class="button button-primary" style="float:right">Save <i class="flaticon stroke checkmark-2"><!----></i></button>');
+
+      $('input.flag-input').focus(function () {
+        $(this).css({
+          color: '#000'
+        });
+      });
+
+      $('input.flag-input').blur(function () {
+        console.log('$(this)', $(this));
+        console.log('this', this);
+        $(this).css({
+          color: '#fff'
+        });
+      });
+
+      $('#flags-cancel').click(function () {
+        $('.modal-overlay').click();
+      });
+
+      $('#flags-save').click(function () {
+        $('.modal-overlay').click();
+      });
     });
   }
 }
