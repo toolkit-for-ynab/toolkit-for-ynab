eec0ba40b9a5e665911c18c872eae964adac2e08
Daniel Bird <danieldbird@gmail.com>
2017-04-24 22:29:27 +1200

    Basic functionality

---
+++ sauce/features/budget/display-goal-amount/index.js
@@ -0,0 +1,48 @@
+import Feature from 'core/feature';
+import * as toolkitHelper from 'helpers/toolkit';
+
+export default class DisplayTargetGoalAmount extends Feature {
+  constructor() {
+    super();
+  }
+
+  shouldInvoke() {
+    return toolkitHelper.getCurrentRouteName().indexOf('budget') !== -1;
+  }
+
+  invoke() {
+    // var i = 1;
+    $('.budget-table-header .budget-table-cell-name').after('<li class=\'budget-table-cell-goal\' style=\'font-size: .75em; text-align: right;\'>GOAL</li>');
+    $('.budget-table-row.is-master-category li.budget-table-cell-name').after('<li class=\'budget-table-cell-goal\' style=\'text-align: right;\'></li>');
+    $('.budget-table-row.is-sub-category li.budget-table-cell-name').after('<li class=\'budget-table-cell-goal\' style=\'text-align: right; font-size: 80%; color: gray;\'></li>');
+    $('.budget-table-row.is-sub-category').each((index, element) => {
+      const emberId = element.id;
+      const viewData = toolkitHelper.getEmberView(emberId).data;
+      const { subCategory } = viewData;
+      const monthlyFunding = subCategory.get('monthlyFunding');
+      const targetBalance = subCategory.get('targetBalance');
+      if (!monthlyFunding && !targetBalance) {
+        $('#' + emberId + '.budget-table-row.is-sub-category li.budget-table-cell-goal').text('$0.00');
+      } else if (monthlyFunding === 0) {
+        // console.log(i + ': ' + subCategory.get('name') + subCategory.get('targetBalance') / 1000); i++;
+        $('#' + emberId + '.budget-table-row.is-sub-category li.budget-table-cell-goal').text('$' + targetBalance / 1000);
+      } else if (targetBalance === 0) {
+        // console.log(i + ': ' + subCategory.get('name') + subCategory.get('monthlyFunding') / 1000); i++;
+        $('#' + emberId + '.budget-table-row.is-sub-category li.budget-table-cell-goal').text('$' + monthlyFunding / 1000);
+      }
+    });
+  }
+
+  observe(changedNodes) {
+    if (!this.shouldInvoke()) return;
+    if (changedNodes.has('fieldset') && changedNodes.has('budget-inspector-goals')) {
+      $('.budget-table-cell-goal').remove();
+      this.invoke();
+    }
+  }
+
+  onRouteChanged() {
+    if (!this.shouldInvoke()) return;
+    this.invoke();
+  }
+}
---
+++ sauce/features/budget/display-goal-amount/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'DisplayTargetGoalAmount',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Display Target Goal Amount',
+  description: 'Displays the target goal amount for every category with a goal.'
+};
