57fb45d9cd400afcac11d2e0a19f1a023ce1f507
Daniel Bird <danieldbird@gmail.com>
2017-04-30 16:15:58 +1200

    Add warning colour to goal amount
    - Added colour to the target goal amount if you have budgeted over your
    goal. This indicates it requires attention / revising goal amount.
    - Fixed background on checkbox unchecked.

--- sauce/features/budget/display-goal-amount/index.js
+++ sauce/features/budget/display-goal-amount/index.js
@@ -21,8 +21,7 @@ export default class DisplayTargetGoalAmount extends Feature {
 
     $('.budget-table-row.is-sub-category li.budget-table-cell-name').append(
       $('<div>', { class: 'budget-table-cell-goal' }).css({
-        background: '-webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 10%,rgba(255,255,255,1) 100%)', position: 'absolute', 'font-size': '80%', color: 'gray',
-        'padding-left': '.75em', 'padding-right': '1px', 'line-height': '2.55em'
+        background: '-webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 10%,rgba(255,255,255,1) 100%)', position: 'absolute', 'font-size': '80%', 'padding-left': '.75em', 'padding-right': '1px', 'line-height': '2.55em'
       })
     );
 
@@ -30,17 +29,34 @@ export default class DisplayTargetGoalAmount extends Feature {
       const emberId = element.id;
       const viewData = toolkitHelper.getEmberView(emberId).data;
       const { subCategory } = viewData;
+      const { monthlySubCategoryBudget } = viewData;
       const { monthlySubCategoryBudgetCalculation } = viewData;
       const goalType = subCategory.get('goalType');
       const monthlyFunding = subCategory.get('monthlyFunding');
       const targetBalance = subCategory.get('targetBalance');
       const targetBalanceDate = monthlySubCategoryBudgetCalculation.get('goalTarget');
+      const budgetedAmount = monthlySubCategoryBudget.get('budgeted');
       if (goalType === 'MF') {
         $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').text(toolkitHelper.formatCurrency(monthlyFunding));
+        if (budgetedAmount <= monthlyFunding) {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'grey' });
+        } else {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'red' });
+        }
       } else if (goalType === 'TB') {
         $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').text(toolkitHelper.formatCurrency(targetBalance));
+        if (budgetedAmount <= targetBalance) {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'grey' });
+        } else {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'red' });
+        }
       } else if (goalType === 'TBD') {
         $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').text(toolkitHelper.formatCurrency(targetBalanceDate));
+        if (budgetedAmount <= targetBalanceDate) {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'grey' });
+        } else {
+          $('#' + emberId + '.budget-table-row.is-sub-category div.budget-table-cell-goal').css({ color: 'red' });
+        }
       }
     });
   }
@@ -52,14 +68,11 @@ export default class DisplayTargetGoalAmount extends Feature {
       this.invoke();
     }
 
-    if (changedNodes.has('budget-table-row is-sub-category is-checked')
+    if (changedNodes.has('budget-table-row is-sub-category') || changedNodes.has('budget-table-row is-sub-category is-checked')
       || changedNodes.has('budget-table-cell-goal')) {
       $('.budget-table-row.is-sub-category li.budget-table-cell-name .budget-table-cell-goal').css({
-        background: '-webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 10%,rgba(255,255,255,1) 100%)', color: 'gray'
-      });
-      $('.budget-table-row.is-checked li.budget-table-cell-name .budget-table-cell-goal').css({
-        background: '#005a6e', color: '#fff'
-      });
+        background: '-webkit-linear-gradient(left, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 10%,rgba(255,255,255,1) 100%)' });
+      $('.budget-table-row.is-checked li.budget-table-cell-name .budget-table-cell-goal').css({ background: '#005a6e' });
     }
   }
 
--- sauce/features/budget/display-goal-amount/settings.js
+++ sauce/features/budget/display-goal-amount/settings.js
@@ -3,6 +3,6 @@ module.exports = {
   type: 'checkbox',
   default: false,
   section: 'budget',
-  title: 'Display Target Goal Amount',
-  description: 'Displays the target goal amount for every category with a goal.'
+  title: 'Display Target Goal Amount And Overspend Warning',
+  description: 'Displays the target goal amount for every category with a goal, and warns you when you spend over your goal.'
 };
