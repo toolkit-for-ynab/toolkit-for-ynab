fc2270148f9a5b48387b7bc4810d9a97851b10d9
Daniel Bird <danieldbird@Daniels-iMac.local>
2017-05-12 16:33:41 +1200

    Display goal amount feature

---
+++ sauce/features/budget/display-goal-amount/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'DisplayTargetGoalAmount',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Display Target Goal Amount And Overspend Warning',
+  description: 'Displays the target goal amount for every category with a goal, and warns you when you spend over your goal.'
+};
