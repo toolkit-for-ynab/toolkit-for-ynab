e8f6b7869d49f2f087fbd96128523b0628d600af
Kevin Brown <kevinbrowntech@gmail.com>
2017-06-07 22:53:32 +1000

    Silencing some extra noise from the build process.

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ rem all the calls we need, as well as processing settings files
 python generateFeedChanges.py
 
 rem Generate legacy and new settings
-call npm run gen-settings
+call npm run gen-settings --silent || goto :errexit
 
 rem Generate features/index.js for the new features
-call npm run gen-featureIndex
+call npm run gen-featureIndex --silent || goto :errexit
 
 echo [   INFO] Checking code style with ESLint...
 call npm run legacy-eslint --silent || goto :errexit
@@ -20,7 +20,7 @@ call npm run legacy-babel --silent || goto :errexit
 
 rem Run webpack to grab all new framework features
 echo [   INFO] Webpacking new framework...
-call npm run webpack
+call npm run webpack --silent || goto :errexit
 
 rem Copy any non JS files across that babel didn't bring with it. Ignore existing files we just transpiled.
 echo [   INFO] Copying non JS files...
