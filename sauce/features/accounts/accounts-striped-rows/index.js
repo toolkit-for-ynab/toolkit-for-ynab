8bc3b2e4aa78ba40d0dfc5c035fa1853782c87a2
Kevin Brown <kevinbrowntech@gmail.com>
2017-06-14 20:19:11 +1000

    Refactored accounts striped rows feature to use new webpack feature framework.

---
+++ sauce/features/accounts/accounts-striped-rows/index.js
@@ -0,0 +1,5 @@
+import Feature from 'core/feature';
+
+export default class AccountsStripedRows extends Feature {
+  injectCSS() { return require('./index.css'); }
+}
---
+++ sauce/features/accounts/accounts-striped-rows/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'accountsStripedRows',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Striped Transaction Rows',
+  description: 'Shows a light gray background on every other transaction row.',
+};
