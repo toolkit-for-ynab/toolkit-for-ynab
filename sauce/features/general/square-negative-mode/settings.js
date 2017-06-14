66a75c505d56a6b840f18536ca39b61357da6537
Kevin Brown <kevinbrowntech@gmail.com>
2017-06-14 21:05:28 +1000

    Refactored square negative mode feature to use webpack framework.

---
+++ sauce/features/general/square-negative-mode/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'SquareNegativeMode',
+  type: 'checkbox',
+  default: false,
+  section: 'general',
+  title: 'Square Negative Mode',
+  description: 'Changes the round borders on all negative numbers to square. Helps them become more of an eyesore so you want to get rid of them!'
+};
