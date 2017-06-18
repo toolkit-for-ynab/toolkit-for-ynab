02a4430229b2d066e0c3f5681d160118ac5b039c
Josh Madewell <josh.madewell@gmail.com>
2017-06-18 13:11:57 -0500

    Fixing an issue with close reconcile in firefox
    Firefox was for some, still unknown reason, failing when we called
    .click() on an element that was definitely on the page. Either way,
    I've opted to use the `closeModal` action on the application controller
    in YNAB which circumvents this altogether.

---
+++ sauce/features/accounts/autoCloseReconcile/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'AutoCloseReconcile',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Auto-Close Reconcile Popup',
+  description: 'Auto-close the "Account Reconciled!" popup window after a short time'
+};
