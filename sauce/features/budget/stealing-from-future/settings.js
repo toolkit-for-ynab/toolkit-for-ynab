0826b139f1b925f972c9a647203a23473a790c43
Josh Madewell <josh.madewell@gmail.com>
2017-06-21 23:03:11 -0500

    migrating stealing from next month to new framework and fixing bugs

---
+++ sauce/features/budget/stealing-from-future/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'StealingFromFuture',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Stealing From Future Alert',
+  description: "Highlights \"Budget Next Month\" red you've gone negative as some point in the future"
+};
