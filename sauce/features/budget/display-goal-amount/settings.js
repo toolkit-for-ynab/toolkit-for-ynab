14245a68562e42603c6526678fefcdefd378d179
Georg J.P. Link <linkgeorg@gmail.com>
2017-06-30 14:13:06 -0500

    corrected default value

--- sauce/features/budget/display-goal-amount/settings.js
+++ sauce/features/budget/display-goal-amount/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'DisplayTargetGoalAmount',
   type: 'select',
-  default: false,
+  default: '0',
   section: 'budget',
   title: 'Display Target Goal Amount And Overbudget Warning',
   description: 'Adds a \'Goal\' column which displays the target goal amount for every category with a goal, and a warning in red if you have budgeted beyond your goal.',
