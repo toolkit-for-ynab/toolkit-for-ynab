f09a409b091820aef6bf7f7d15482b1865269adc
Benjamin Falk <ben@falkencreative.com>
2017-09-09 22:41:09 -0400

    Bugfixes: Privacy Mode Improvements
    Bug fixes and improvements to the Privacy Mode feature. Moved from
    /source/ to /sauce/, and improved styling of YNAB and Toolkit reports
    when privacy mode is enabled.

---
+++ sauce/features/general/privacy-mode/index.js
@@ -0,0 +1,63 @@
+import { Feature } from 'core/feature';
+
+export class PrivacyMode extends Feature {
+
+  shouldInvoke() {
+    return true;
+    // console.log(this.settings.enabled);
+    // return this.settings.enabled !== '0';
+  }
+
+  invoke() {
+    let toggle = ynabToolKit.shared.getToolkitStorageKey('privacy-mode', 'boolean');
+    if (typeof toggle === 'undefined') {
+      ynabToolKit.shared.setToolkitStorageKey('privacy-mode', false);
+    }
+
+    if (ynabToolKit.options.PrivacyMode === '2') {
+      if (!$('#toolkit-togglePrivacy').length) {
+        $('nav.sidebar.logged-in .sidebar-contents').after('<button id="toolkit-togglePrivacy"><i class="ember-view flaticon stroke lock-1"></i></button>');
+
+        let parent = this;
+        $('body').on('click', 'button#toolkit-togglePrivacy', function () {
+          parent.togglePrivacyMode();
+        });
+      }
+    } else if (ynabToolKit.options.PrivacyMode === '1') {
+      ynabToolKit.shared.setToolkitStorageKey('privacy-mode', true);
+    }
+
+    this.updatePrivacyMode();
+  }
+
+  injectCSS() {
+    let css = require('./index.css');
+
+    if (this.settings.enabled === '2') {
+      css += require('./toggle.css');
+    }
+
+    return css;
+  }
+
+  togglePrivacyMode() {
+    $('button#toolkit-togglePrivacy').toggleClass('active');
+
+    let toggle = ynabToolKit.shared.getToolkitStorageKey('privacy-mode', 'boolean');
+    ynabToolKit.shared.setToolkitStorageKey('privacy-mode', !toggle);
+    this.updatePrivacyMode();
+  }
+
+  updatePrivacyMode() {
+    let toggle = ynabToolKit.shared.getToolkitStorageKey('privacy-mode', 'boolean');
+
+    if (toggle) {
+      $('body').addClass('toolkit-privacyMode');
+      $('#toolkit-togglePrivacy i').removeClass('unlock-1').addClass('lock-1');
+    } else {
+      $('body').removeClass('toolkit-privacyMode');
+      $('#toolkit-togglePrivacy i').removeClass('lock-1').addClass('unlock-1');
+    }
+  }
+
+}
---
+++ sauce/features/general/privacy-mode/settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'PrivacyMode',
+  type: 'select',
+  default: '0',
+  section: 'general',
+  title: 'Privacy Mode',
+  description: 'Obscures dollar amounts everywhere until hovered.',
+  options: [
+    { "name": "Disabled", "value": "0" },
+    { "name": "Always On", "value": "1" },
+    { "name": "Add Toggle Button", "value": "2" }
+  ]
+};
