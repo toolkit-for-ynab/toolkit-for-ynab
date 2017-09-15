cc822655829972190476cc2aeb3be5556f2eff7f
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-09-15 14:06:40 -0500

    Added babel-plugin-transform-class-properties and explicitly using it.

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['env']
+          presets: ['env'],
+          plugins: ['transform-class-properties']
         }
       }]
     }, {
