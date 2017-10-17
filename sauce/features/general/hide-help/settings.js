2e3a19e77416f53cbce3fcbe7e0b2f18baaa8cf3
Benjamin Falk <ben@falkencreative.com>
2017-10-17 08:26:23 -0400

    Hide Help Updates
    Moved feature to sauce. Fixed a bug where the user’s hide help setting
    wasn’t saved between YNAB refreshes.

---
+++ sauce/features/general/hide-help/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'hideHelp',
+  type: 'checkbox',
+  default: false,
+  section: 'general',
+  title: 'Hide Help (?) Button',
+  description: 'This feature hides the blue help (?) button in the bottom right corner of the screen. View the account-options popup (click your e-mail in the bottom left) to show or hide the help button.'
+};
