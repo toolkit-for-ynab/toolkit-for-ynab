5d7e8ee556ea13ea29369220d828adf79874f532
David Baldon <david@baldon.org>
2017-10-23 21:37:32 -0500

    Refactor feature calendar-first-day Refactor code

---
+++ sauce/features/accounts/calendar-first-day/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'CalendarFirstDay',
+  type: 'select',
+  default: '0',
+  section: 'accounts',
+  title: 'First Day of the Week in Calendar',
+  description: 'Change the first day of the week when viewing the calendar.',
+  options: [
+    { name: 'Default (Sunday)', value: '0' },
+    { name: 'Monday', value: '1' },
+    { name: 'Tuesday', value: '2' },
+    { name: 'Wednesday', value: '3' },
+    { name: 'Thursday', value: '4' },
+    { name: 'Friday', value: '5' },
+    { name: 'Saturday', value: '6' }
+  ]
+};
