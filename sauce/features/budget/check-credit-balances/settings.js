6a30d087365dfe8e8dc7c9704ee2770880f50ebd
David Baldon <david@baldon.org>
2017-11-03 23:09:30 -0500

    Refactor check-credit-balances Also added a couple of functions to the toolkit that should be useful in multiple features.

---
+++ sauce/features/budget/check-credit-balances/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'CheckCreditBalances',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Paid in Full Credit Card Assist',
+  description: 'Highlights credit card category balances with a yellow warning if the balance of the category does not match the account balance. Adds a button to the Inspector to rectify the difference.'
+};
