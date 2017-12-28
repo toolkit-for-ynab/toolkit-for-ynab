36c76720529dbd0974f186b61ef3edb361c83223
Josh Madewell <josh.madewell@gmail.com>
2017-12-28 13:14:51 -0500

    Refactor ensureDefaultsAreSet to getUserSettings
    Instead of just making sure defaults are set, getUserSettings now ensures
    defaults are set and returns a settings object with all the user-specified
    settings as well. The logic to migrate extension setting names and to ensure
    users settings are stored in browser.storage is now also part of calling
    getUserSettings.

--- sauce/core/settings/index.js
+++ sauce/core/settings/index.js
@@ -1,44 +1,84 @@
 export * from './settings';
-import { allToolkitSettings } from './settings';
+import { browser } from 'toolkit/core/common/web-extensions';
+import { allToolkitSettings, legacySettingMap } from './settings';
 import { ToolkitStorage } from 'toolkit/core/storage';
 
 const storage = new ToolkitStorage();
 
-function getLegacyStorageKeys() {
-  return new Promise(function (resolve, reject) {
-    try {
-      resolve(Object.keys(localStorage));
-    } catch (e) {
-      reject(e);
-    }
+function getKangoKeys() {
+  return new Promise((resolve) => {
+    browser.runtime.sendMessage({ type: 'storage', content: { type: 'keys' } }, (response) => {
+      resolve(response);
+    });
   });
 }
 
-function persistLegacySetting(setting) {
-  const data = localStorage.getItem(setting);
-  return storage.setFeatureSetting(setting, data);
+function persistKangoStorageSetting(setting, persistAs) {
+  const settingName = persistAs || setting;
+
+  return new Promise((resolve) => {
+    browser.runtime.sendMessage({ type: 'storage', content: { type: 'get', itemName: setting } }, (response) => {
+      storage.setFeatureSetting(settingName, response).then(resolve);
+    });
+  });
 }
 
-export function ensureDefaultsAreSet() {
+function updateLegacySetting(legacySetting, newSetting) {
+  return storage.getFeatureSetting(legacySetting).then((legacyValue) => {
+    return storage.setFeatureSetting(newSetting, legacyValue);
+  });
+}
+
+export function getUserSettings() {
   return new Promise(function (resolve) {
     Promise.all([
-      getLegacyStorageKeys(),
+      getKangoKeys(),
       storage.getStoredFeatureSettings()
-    ]).then(([legacyStorageKeys, storedFeatureSettings]) => {
-      const promises = [];
+    ]).then(([kangoKeys, storedFeatureSettings]) => {
+      const settingPromises = allToolkitSettings.map((setting) => {
+        const legacySettingName = legacySettingMap[setting.name];
+        const settingIsPersisted = storedFeatureSettings.includes(setting.name);
+        const leagcySettingPersisted = storedFeatureSettings.includes(legacySettingName);
 
-      allToolkitSettings.forEach(function (setting) {
-        if (!storedFeatureSettings.includes(setting.name)) {
-          if (legacyStorageKeys.includes(setting.name)) {
-            return promises.push(persistLegacySetting(setting.name));
-          }
-
-          promises.push(storage.setFeatureSetting(setting.name, setting.default));
+        if (settingIsPersisted) {
+          // this should be the case for all users once they've loaded the toolkit post web-extensions
+          return storage.getFeatureSetting(setting.name);
+        } else if (leagcySettingPersisted) {
+          // this will be the case for any feature that has been migrated post web-extensions
+          return updateLegacySetting(legacySettingName, setting.name)
+            .then(storage.getFeatureSetting(setting.name));
         }
+
+        // if we've not already returned then this is either the first-load of the extension post
+        // web-extensions or this is an entirely new feature. check the former case first and then
+        // assume the latter.
+
+        // kango stored settings in localStorage of the background page so look there first
+        const isInKangoStorage = kangoKeys.includes(setting.name);
+        const isLegacySettingInKangoStorage = kangoKeys.includes(legacySettingName);
+
+        if (isInKangoStorage) {
+          // we have the setting so go ahead and persist it to storage and carry on
+          return persistKangoStorageSetting(setting.name)
+            .then(storage.getFeatureSetting(setting.name));
+        } else if (isLegacySettingInKangoStorage) {
+          // this is a migration feature -- need to persist the new setting name
+          return persistKangoStorageSetting(legacySettingName, setting.name)
+            .then(storage.getFeatureSetting(setting.name));
+        }
+
+        // if we're still here then all we have left is a new feature. persist the default.
+        return storage.setFeatureSetting(setting.name, setting.default)
+          .then(storage.getFeatureSetting(setting.name));
       });
 
-      Promise.all(promises).then(function () {
-        resolve();
+      Promise.all(settingPromises).then((persistedSettings) => {
+        const userSettings = allToolkitSettings.reduce((allSettings, currentSetting, index) => {
+          allSettings[currentSetting.name] = persistedSettings[index];
+          return allSettings;
+        }, {});
+
+        resolve(userSettings);
       });
     });
   });
