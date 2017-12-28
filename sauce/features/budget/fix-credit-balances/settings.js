07392005f7602b493c0abd6bd490a92d843aa48d
Jacob Haynes <jacob@Jacobs-MacBook-Pro.local>
2017-12-27 17:48:18 -0700

    Add a new feature which automatically updates the credit card balance to match the current balance. It builds on 'Paid In Full Credit Card Assist' but does the rectification automatically instead of relying on the user hitting a couple buttons. This allows behavior more like YNAB4 where credit cards are treated like negative cash.\n\nTests:\nEnsure that multiple credit cards mainain the right balanace after adding and removing transactions and after chaning the balance carried over from last month.\nUse chrome debugger to ensure that updates aren't done for accounts that alrealready have the correct balance.\nEnsure that on overpayed accounts don't end up with a negative balance.

---
+++ sauce/features/budget/fix-credit-balances/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'FixCreditBalances',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Auto Paid in Full Credit Cards',
+  description: 'Makes the credit card category balances match the account balance.'
+};
