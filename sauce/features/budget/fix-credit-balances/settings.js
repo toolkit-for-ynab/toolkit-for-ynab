8f519e18eb9ec2686dc1a1a3362dc8c3fb949da9
Jacob Haynes <jacob@Jacobs-MacBook-Pro.local>
2017-12-28 22:56:51 -0700

    remove the seperate fix-credit-balances featue that was uniifed with check-credit-card

--- sauce/features/budget/fix-credit-balances/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'FixCreditBalances',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Auto Paid in Full Credit Cards',
-  description: 'Makes the credit card category balances match the account balance.'
-};
