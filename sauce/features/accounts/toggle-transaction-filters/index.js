ce0de7683387a3dcaeaf3e5cd377597768df4e07
David Baldon <david@baldon.org>
2017-12-28 19:57:45 -0600

    Response to PR change request Add onRouteChanged() Move click handlers off of the document body and to the buttons themselves.

--- sauce/features/accounts/toggle-transaction-filters/index.js
+++ sauce/features/accounts/toggle-transaction-filters/index.js
@@ -1,5 +1,7 @@
 import { Feature } from 'toolkit/core/feature';
-import * as toolkitHelper from 'toolkit/helpers/toolkit';
+// import * as toolkitHelper from 'toolkit/helpers/toolkit';
+import { getCurrentRouteName } from 'toolkit/helpers/toolkit';
+import { controllerLookup } from 'toolkit/helpers/toolkit';
 
 export class ToggleTransactionFilters extends Feature {
   injectCSS() {
@@ -7,21 +9,23 @@ export class ToggleTransactionFilters extends Feature {
   }
 
   shouldInvoke() {
-    return toolkitHelper.getCurrentRouteName().indexOf('account') > -1;
+    return getCurrentRouteName().indexOf('account') > -1;
+  }
+
+  onRouteChanged() {
+    if (!this.shouldInvoke()) return;
+
+    this.invoke();
   }
 
   invoke() {
-    if (!this.shouldInvoke()) return;
-
-    // create buttons
+    // create or update buttons
     this.initToggleButtons();
-
-    // add functionality on click
-    $('body').on('click', 'button#toolkit-toggleReconciled', this.toggleReconciled);
-    $('body').on('click', 'button#toolkit-toggleScheduled', this.toggleScheduled);
   }
 
   observe(changedNodes) {
+    if (!this.shouldInvoke()) return;
+
     // activate button styles if filters potentially change
     // activate if switch to individual account, or all accounts views
     if (
@@ -32,8 +36,8 @@ export class ToggleTransactionFilters extends Feature {
   }
 
   toggleReconciled() {
-    var container = toolkitHelper.controllerLookup('accounts');
-    var settingReconciled = !container.filters.get('reconciled');
+    let container = controllerLookup('accounts');
+    let settingReconciled = !container.filters.get('reconciled');
     container.filters.get('propertiesToSet').setProperties({ reconciled: settingReconciled });
     container.filters.applyFilters();
 
@@ -45,8 +49,8 @@ export class ToggleTransactionFilters extends Feature {
   }
 
   toggleScheduled() {
-    var container = toolkitHelper.controllerLookup('accounts');
-    var settingScheduled = !container.filters.get('scheduled');
+    let container = controllerLookup('accounts');
+    let settingScheduled = !container.filters.get('scheduled');
     container.filters.get('propertiesToSet').setProperties({ scheduled: settingScheduled });
     container.filters.applyFilters();
 
@@ -74,22 +78,25 @@ export class ToggleTransactionFilters extends Feature {
 
   initToggleButtons() {
     // get internal filters
-    var container = toolkitHelper.controllerLookup('accounts');
-    var settingReconciled = container.filters.get('reconciled');
-    var settingScheduled = container.filters.get('scheduled');
+    let container = controllerLookup('accounts');
+    let settingReconciled = container.filters.get('reconciled');
+    let settingScheduled = container.filters.get('scheduled');
 
     // insert or edit buttons
     if (!$('#toolkit-toggleReconciled').length) {
       // create buttons if they don't already exist
-      if (ynabToolKit.options.toggleTransactionFilters === '2') {
-        // show both text and icons
-        $('.accounts-toolbar .accounts-toolbar-right').append('<button id="toolkit-toggleReconciled" class="button" title="Toggle Reconciled Transactions"><i class="flaticon solid lock-1 is-reconciled"></i> Reconciled</button>');
-        $('.accounts-toolbar .accounts-toolbar-right').append('<button id="toolkit-toggleScheduled" class="button" title="Toggle Scheduled Transactions"><i class="flaticon solid clock-1 is-reconciled"></i> Scheduled</button>');
-      } else {
-        // show only icons
-        $('.accounts-toolbar .accounts-toolbar-right').append('<button id="toolkit-toggleReconciled" class="button button-icononly" title="Toggle Reconciled Transactions"><i class="flaticon solid lock-1 is-reconciled"></i></button>');
-        $('.accounts-toolbar .accounts-toolbar-right').append('<button id="toolkit-toggleScheduled" class="button button-icononly" title="Toggle Scheduled Transactions"><i class="flaticon solid clock-1 is-reconciled"></i></button>');
-      }
+      $('.accounts-toolbar .accounts-toolbar-right')
+        .append($('<button>', { id: 'toolkit-toggleReconciled', class: 'button', title: 'Toggle Reconciled Transactions' })
+          .append($('<i>', { class: 'flaticon solid lock-1 is-reconciled' })
+          // show both text and icons or just the icon
+          .append(this.settings.enabled === '2' ? ' Reconciled' : '')
+          .click(() => { this.toggleReconciled(); })));
+      $('.accounts-toolbar .accounts-toolbar-right')
+        .append($('<button>', { id: 'toolkit-toggleScheduled', class: 'button', title: 'Toggle Scheduled Transactions' })
+          .append($('<i>', { class: 'flaticon solid clock-1 is-reconciled' })
+          // show both text and icons or just the icon
+          .append(this.settings.enabled === '2' ? ' Scheduled' : '')
+            .click(() => { this.toggleScheduled(); })));
 
       this.updateToggleButtons(settingReconciled, settingScheduled);
     } else {
