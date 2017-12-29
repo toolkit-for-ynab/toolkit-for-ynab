aa8c6f2fe813a6c8dc50a2b3cea63f9b7724f6e6
Josh Madewell <josh.madewell@gmail.com>
2017-12-28 18:25:15 -0600

    Clean up build scripts

--- build.bat
+++ build.bat
@@ -6,14 +6,14 @@ rem Populate feature files like feed changes by reading through the code and hoo
 rem all the calls we need, as well as processing settings files
 python generateFeedChanges.py
 
-rem Generate legacy and new settings
-call yarn gen-settings || goto :errexit
+echo [   INFO] Checking code style with ESLint...
+call yarn legacy-eslint || goto :errexit
 
 rem Generate features/index.js for the new features
 call yarn gen-featureIndex || goto :errexit
 
-echo [   INFO] Checking code style with ESLint...
-call yarn legacy-eslint || goto :errexit
+rem Generate legacy and new settings
+call yarn gen-settings || goto :errexit
 
 rem Run webpack to grab all new framework features
 echo [   INFO] Webpacking new framework...
@@ -21,18 +21,11 @@ call yarn webpack || goto :errexit
 
 rem Transpile the source/ directory, putting the files in src/ which is where Kango expects to see them.
 echo [   INFO] Transpiling code with Babel on ES2015 preset...
-rem rd /s /q src
 call yarn legacy-babel || goto :errexit
 
 rem Copy any non JS files across that babel didn't bring with it. Ignore existing files we just transpiled.
 echo [   INFO] Copying non JS files...
-robocopy source src /E /XC /XN /XO /NFL /NDL /NJH /NJS
-
-rem Run the Kango build.
-python lib\kango-framework-latest\kango.py build .
-
-rem Clean up.
-rd /s /q src
+robocopy sauce/extension/legacy dist/toolkit /E /XC /XN /XO /NFL /NDL /NJH /NJS
 
 goto :exit
 
