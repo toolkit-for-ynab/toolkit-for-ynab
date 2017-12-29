72122feefe1432f6c1c0c3e13ad39404d6de7e37
Jacob Haynes <jacob@Jacobs-MacBook-Pro.local>
2017-12-28 22:55:35 -0700

    Unified the new automatic credit card balance feature with the existing one and added a way to only keep paid in full on a single credit card\n\nTests:\nCheck automatic credit card balance when changing the balance and when changing leftover from last month\nEnsure that only budgets with #PIF get changed in PIF only mode

--- sauce/features/budget/check-credit-balances/settings.js
+++ sauce/features/budget/check-credit-balances/settings.js
@@ -1,8 +1,29 @@
 module.exports = {
   name: 'CheckCreditBalances',
-  type: 'checkbox',
+  type: 'select',
   default: false,
   section: 'budget',
   title: 'Paid in Full Credit Card Assist',
-  description: 'Highlights credit card category balances with a yellow warning if the balance of the category does not match the account balance. Adds a button to the Inspector to rectify the difference.'
+  description: 'This keeps track of Credit Card Payments that aren\'t fully budgeted by either highlighting and providing a rectify button or automatically rectifying..',
+  options: [
+    { "name": "Disabled", "value": "disabled" },
+    { "name": "Highlight Mismatch In Yellow and add Rectify Button to the inspector", "value": "highlight" },
+    { "name": "Automatically Rectify", "value": "rectify" },
+    { "name": "Automatically Rectify accounts with '#PIF' in the name only", "value": "rectifyPIF" },
+  ], 
+  actions: {
+    "highlight": [
+      "injectCSS", "main.css",
+      "injectScript", "main.js"
+    ],
+    "rectify": [
+      "injectCSS", "main.css",
+      "injectScript", "main.js"
+    ],
+    "rectifyPIF": [
+      "injectCSS", "main.css",
+      "injectScript", "main.js"
+    ]
+  }
 };
+
