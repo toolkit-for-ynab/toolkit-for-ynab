e43dbf6c4896b460d85b92576275123ed8a98623
Kevin Brown <kevinbrowntech@gmail.com>
2018-01-07 17:30:02 +1100

    Revert "Removed extra files from merge."
    This reverts commit 9840df5418283988b243eb926a97055ba8be0265.

---
+++ sauce/features/reports/compact-income-vs-expense/index.js
@@ -0,0 +1,34 @@
+import { Feature } from 'toolkit/core/feature';
+import { getCurrentRouteName } from 'toolkit/helpers/toolkit';
+
+export class CompactIncomeVsExpense extends Feature {
+  injectCSS() { return require('./index.css'); }
+
+  shouldInvoke() {
+    return getCurrentRouteName().indexOf('reports.income-expense') > -1;
+  }
+
+  invoke() {
+    let viewWidth = $('.reports-content').width();
+    let columnCount = $('.income-expense-column.income-expense-column-header').length;
+    let tableWidth = columnCount * 115 + 200 + 32;
+    let percentage = Math.ceil(tableWidth / viewWidth * 100);
+
+    $('.income-expense-table').css({ width: percentage + '%' });
+  }
+
+  observe(changedNodes) {
+    if (!this.shouldInvoke()) return;
+
+    if (changedNodes.has('income-expense-row') ||
+        changedNodes.has('income-expense-column income-expense-number')) {
+      this.invoke();
+    }
+  }
+
+  onRouteChanged() {
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+}
