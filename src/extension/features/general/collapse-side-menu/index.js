78a6cc35c86bdf686efca1f8d79a3453fc4ba0b2
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-03-24 10:42:09 -0700

    Fixed the styling of the Toolkit's sidebar icons and an issue with the collapse sidebar feature. (#1240)
    * Fix styling for Toolkit navlink icons and refactor collapse sidebar
    
    * Restyle nav items for a smoother feel when collapsing

--- src/extension/features/general/collapse-side-menu/index.js
+++ src/extension/features/general/collapse-side-menu/index.js
@@ -1,223 +1,89 @@
 import { Feature } from 'toolkit/extension/features/feature';
-import { i10n } from 'toolkit/extension/utils/toolkit';
+import { getToolkitStorageKey, i10n, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
 
 export class CollapseSideMenu extends Feature {
-  collapseBtn = '';
-  originalButtons = {}
-  originalSizes = {
-    sidebarWidth: 0,
-    contentLeft: 0,
-    headerLeft: 0,
-    contentWidth: 0,
-    inspectorWidth: 0
-  }
+  injectCSS() { return require('./index.css'); }
 
-  injectCSS() {
-    return require('./index.css');
-  }
-
-  shouldInvoke() {
-    return true;
-  }
+  shouldInvoke() { return true; }
 
   invoke() {
-    this.collapseBtn =
-      $('<li>', { class: 'ember-view ynabtk-navlink-collapse' }).append($('<a>', { class: 'ynabtk-collapse-link' }).append($('<span>', { class: 'ember-view flaticon stroke left-circle-4' })).append(i10n('toolkit.collapse', 'Collapse')));
-
-    this.setupBtns();
+    this.createCollapseButton();
+    this.applyState();
   }
 
   observe(changedNodes) {
-    if (changedNodes.has('layout user-logged-in')) {
-      if ($('.nav-main').length) {
-        this.setupBtns();
-      }
-    }
-
     if (changedNodes.has('nav-main')) {
-      let numNavLinks = $('.nav-main').children().length;
-      let collapseIndex = $('.nav-main').children().index($('.ynabtk-navlink-collapse'));
-      let numCollapsedLinks = $('.collapsed-buttons').children().length;
-
-      if (numNavLinks > (collapseIndex + 1) || numNavLinks > numCollapsedLinks) {
-        $('.ynabtk-navlink-collapse').remove();
-
-        this.setUpCollapseBtn();
-        this.setUpCollapsedButtons();
+      if (getToolkitStorageKey('isCollapsed', 'boolean')) {
+        this.getHideElements().hide();
       }
+
+      this.invoke();
     }
   }
 
-  setOriginalSizes() {
-    this.originalSizes = ({
-      sidebarWidth: $('.sidebar').width(),
-      contentLeft: $('.content').css('left'),
-      headerLeft: $('.budget-header, .accounts-header').css('left'),
-      contentWidth: $('.budget-content').outerWidth(),
-      inspectorWidth: $('.budget-inspector').outerWidth()
-    });
+  onRouteChanged() {
+    if (getToolkitStorageKey('isCollapsed', 'boolean')) {
+      this.getHideElements().hide();
+    }
+
+    this.invoke();
   }
 
-  // Add buttons and handlers to screen
-  setupBtns() {
-    // Don't proceed if buttons already exist
-    if ($('.ynabtk-navlink-collapse').is(':visible') || $('.navbar-expand').is(':visible')) {
+  createCollapseButton() {
+    if ($('.ynabtk-navlink-collapse').length) {
       return;
     }
 
-    this.setUpCollapseBtn();
-    this.setUpCollapsedButtons();
-  }
-
-  setUpCollapseBtn() {
-    let _this = this;
-    $('.nav-main').append(this.collapseBtn);
-    $('body').on('click', '.ynabtk-navlink-collapse', _this, this.collapseMenu);
-  }
-
-  setUpCollapsedButtons() {
-    let expandBtns = this.getUnCollapseBtnGroup();
-
-    $('.sidebar').prepend(expandBtns);
-
-    if ($('.sidebar-contents').is(':visible')) {
-      $('.collapsed-buttons').hide();
-    }
-  }
-
-  getUnCollapseBtnGroup() {
-    let navChildren = $('.nav-main').children();
-    let navChildrenLength = navChildren.length;
-    let collapsedBtnContainer = $('.collapsed-buttons');
-
-    if (collapsedBtnContainer.length) {
-      collapsedBtnContainer.children().remove();
-      collapsedBtnContainer.hide();
-    } else {
-      collapsedBtnContainer = $('<div>', { class: 'collapsed-buttons', style: 'display: none' });
-    }
-
-    var clickFunction = function (event) {
-      const _this = event.data; // the feature
-      let linkClass = this.className.replace(' active', '').trim();
-
-      $(_this.originalButtons[linkClass]).click();
-      _this.deactivateCollapsedActive();
-      $(this).addClass('active');
-    };
-
-    for (let i = 0; i < navChildrenLength; i++) {
-      let child = navChildren[i];
-      let span = $(child).find('span')[0];
-
-      // If this is the collapse button, skip
-      if (span && !child.className.includes('ynabtk-navlink-collapse')) {
-        let btnClasses = span.className;
-        let button = $('<button>');
-        button.addClass(btnClasses);
-        button.addClass('button button-prefs');
-
-        let listItem = $(child).find('li')[0] || child;
-        let linkClass = listItem.className.replace(' active', '').trim();
-
-        let link = $('<a>');
-        link.attr('href', '#');
-        link.addClass(linkClass);
-        link.html(button);
-        link.click(this, clickFunction);
-
-        this.originalButtons[linkClass] = 'ul.nav-main li.' + linkClass + ' a';
-
-        // Set proper class so the active styling can be applied
-        if (btnClasses.indexOf('mail-1') > -1) {
-          button.addClass('collapsed-budget');
-        } else if (btnClasses.indexOf('graph-1') > -1) {
-          button.addClass('collapsed-reports');
-        } else if (btnClasses.indexOf('government-1') > -1) {
-          button.addClass('collapsed-account');
-        } else {
-          // Fallback if we don't know what the button is.
-          button.addClass('collapsed');
-        }
-
-        collapsedBtnContainer.append(link);
-      }
-    }
-
-    // Add uncollapse button
-    let collapseBtn = $('<button>', { class: 'button button-prefs flaticon stroke right-circle-4 navbar-expand' });
-
-    collapsedBtnContainer.append(collapseBtn);
-
-    $('body').on('click', '.navbar-expand', this, this.expandMenu);
-
-    return collapsedBtnContainer;
-  }
-
-  // Handle clicking expand button. Puts things back to original sizes
-  expandMenu(event) {
-    let originalSizes = event.data.originalSizes;
-
-    $('.collapsed-buttons').hide();
-    $('.sidebar > .ember-view').fadeIn();
-    $('.ynabtk-navlink-collapse').show();
-
-    $('.sidebar').animate({ width: originalSizes.sidebarWidth });
-    $('.content').animate({ left: originalSizes.contentLeft }, function () {
-      $('.layout').removeClass('collapsed');
+    const button = $('<li>', {
+      class: 'ember-view ynabtk-navlink-collapse'
+    }).append($('<a>', {
+      class: 'ynabtk-collapse-link'
+    }).append($('<span>', {
+      class: 'ember-view ynabtk-collapse-icon flaticon stroke left-circle-4'
+    })).append(i10n('toolkit.collapse', 'Collapse'))).click(() => {
+      const isCollapsed = getToolkitStorageKey('isCollapsed', 'boolean');
+      setToolkitStorageKey('isCollapsed', !isCollapsed);
+      this.applyState();
     });
 
-    $('.budget-header').animate({ left: originalSizes.headerLeft });
-    if ($('.budget-content').is(':visible')) {
-      if (ynabToolKit.options.InspectorWidth !== '0') {
-        $('.budget-inspector')[0].style.setProperty('--toolkit-inspector-minwidth', 'inherit');
-      }
-    }
+    $('.nav-main').append(button);
   }
 
-  // Handle clicking the collapse button
-  collapseMenu(event) {
-    let _this = event.data;
-    // resize-inspector feature could have changed these so fetch current sizes.
-    _this.setOriginalSizes();
-    _this.setActiveButton($('.nav-main li.active').attr('class'));
-    $('.ynabtk-navlink-collapse').hide();
-    $('.sidebar > .ember-view').hide();
-    $('.collapsed-buttons').fadeIn();
+  applyState() {
+    const isCollapsed = getToolkitStorageKey('isCollapsed', 'boolean');
 
-    $('.sidebar').animate({ width: '40px' });
-    $('.content').animate({ left: '40px' }, 400, 'swing', function () {
-      // Need to remove width after animation completion
-      $('.ynab-grid-header').removeAttr('style');
-
-      // We don't use these in our CSS, it's mostly so other features can observe
-      // for collapse/expand and update sizes / do whatever. E.g. reports needs
-      // to resize its canvas when this happens.
-      $('.ynabtk-navlink-collapse').removeClass('expanded').addClass('collapsed');
-      $('.layout').addClass('collapsed');
-    });
-
-    $('.budget-header').animate({ left: '40px' });
-    if ($('.budget-content').is(':visible')) {
-      if (ynabToolKit.options.InspectorWidth !== '0') {
-        $('.budget-inspector')[0].style.setProperty('--toolkit-inspector-minwidth', '276 px');
-      }
-    }
-  }
-
-  // Add the active style to correct button
-  setActiveButton(button) {
-    if (typeof button !== 'undefined') {
-      this.deactivateCollapsedActive();
-      button = button.replace('active', '').replace('ember-view', '').trim();
-      $('.collapsed-buttons a.' + button).addClass('active');
+    if (isCollapsed) {
+      Promise.all([
+        $('.ynab-u.sidebar').animate({ width: '3rem' }).promise(),
+        $('.ynab-u.content').animate({ left: '3rem' }).promise(),
+        $('.budget-header').animate({ left: '3rem' }).promise()
+      ]).then(() => {
+        $('.ynabtk-navlink-reports-link span').addClass('ynabtk-nav-link-collapsed');
+        $('.ynabtk-collapse-link span').addClass('ynabtk-nav-link-collapsed');
+        $('.ynabtk-collapse-icon').removeClass('left-circle-4').addClass('right-circle-4');
+        this.getHideElements().hide();
+      });
     } else {
-      $('.collapsed-buttons a').removeClass('active');
+      Promise.all([
+        $('.ynab-u.sidebar').animate({ width: '260px' }).promise(),
+        $('.ynab-u.content').animate({ left: '260px' }).promise(),
+        $('.budget-header').animate({ left: '260px' }).promise()
+      ]).then(() => {
+        $('.ynabtk-navlink-reports-link span').removeClass('ynabtk-nav-link-collapsed');
+        $('.ynabtk-collapse-link span').removeClass('ynabtk-nav-link-collapsed');
+        $('.ynabtk-collapse-icon').removeClass('right-circle-4').addClass('left-circle-4');
+        this.getHideElements().show();
+      });
     }
   }
 
-  // Deactivate collapsed buttons
-  deactivateCollapsedActive() {
-    $('.collapsed-buttons a').removeClass('active');
+  getHideElements() {
+    return $(`
+      .button-prefs.button-prefs-budget,
+      .nav-accounts,
+      .button-sidebar.nav-add-account,
+      .referral-program,
+      .button-prefs.button-prefs-user
+    `);
   }
 }
