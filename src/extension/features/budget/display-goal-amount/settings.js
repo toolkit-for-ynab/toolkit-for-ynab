d427b4ab31e9b93e452096e7f5c93f1ddf9524d5
Josh Madewell <josh.madewell@gmail.com>
2018-03-30 11:43:29 -0500

    Add "no-emphasis" option to display goal amount and use CSS for styles

--- src/extension/features/budget/display-goal-amount/settings.js
+++ src/extension/features/budget/display-goal-amount/settings.js
@@ -8,6 +8,7 @@ module.exports = {
   options: [
     { name: 'Do not display goal amount (default)', value: '0' },
     { name: 'Display goal amount and warn of overbudget with red', value: '1' },
-    { name: 'Display goal amount but show overbudget as green', value: '2' }
+    { name: 'Display goal amount but show overbudget as green', value: '2' },
+    { name: 'Display goal amount with no emphasis', value: '3' }
   ]
 };
