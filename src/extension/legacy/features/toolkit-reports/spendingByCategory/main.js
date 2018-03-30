ea99659bf2b5390942a7c6f51a9730d40218a579
Josh Madewell <josh.madewell@gmail.com>
2018-03-29 22:29:41 -0500

    Filter out all internal master categories from spending reports

--- src/extension/legacy/features/toolkit-reports/spendingByCategory/main.js
+++ src/extension/legacy/features/toolkit-reports/spendingByCategory/main.js
@@ -138,17 +138,13 @@
           const isTransfer = masterCategoryId === null || subCategoryId === null;
           const ynabCategory = categoriesViewModel.getMasterCategoryById(masterCategoryId);
           const isInternalDebtCategory = isTransfer ? false : ynabCategory.isDebtPaymentMasterCategory();
-          const payee = transaction.getPayee();
-          const isStartingBalance = payee && payee.getInternalName() === ynab.constants.InternalPayees.StartingBalance;
 
           return (
             transaction.getAmount() &&
             !transaction.get('isSplit') &&
             !isTransfer &&
             !isInternalDebtCategory &&
-            !isStartingBalance &&
-            !isStartingBalance &&
-            !transaction.get('inflow')
+            !ynabCategory.isInternalMasterCategory()
           );
         },
 
