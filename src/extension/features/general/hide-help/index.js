64339a84259df48244bdbecde7ab48b41a5915bc
Josh Madewell <joshmade@twitch.tv>
2018-04-06 10:34:00 -0500

    Use the YNAB component to hide the help button

--- src/extension/features/general/hide-help/index.js
+++ src/extension/features/general/hide-help/index.js
@@ -1,51 +1,62 @@
 import { Feature } from 'toolkit/extension/features/feature';
 import { getToolkitStorageKey, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
+import { getEmberView, controllerLookup } from 'toolkit/extension/utils/toolkit';
 
 export class HideHelp extends Feature {
   injectCSS() { return require('./index.css'); }
 
-  shouldInvoke() {
-    return true;
-  }
+  shouldInvoke() { return true; }
 
   observe(changedNodes) {
     if (changedNodes.has('ynab-u modal-popup modal-user-prefs ember-view modal-overlay active')) {
-      this.invoke();
+      this.updatePopupButton();
     }
   }
 
   invoke() {
-    const hide = getToolkitStorageKey('hide-help', true);
+    const initialState = getToolkitStorageKey('hide-help', true);
+    this.setHiddenState(initialState);
+  }
 
-    if (hide) {
-      $('body').addClass('toolkit-hide-help');
+  getBeacon() {
+    return getEmberView($('.self-service').attr('id'));
+  }
+
+  isBeaconHidden() {
+    return this.getBeacon().getBeaconContainer().css('display') === 'none';
+  }
+
+  setHiddenState(state) {
+    setToolkitStorageKey('hide-help', state);
+    const beacon = this.getBeacon();
+    if (state) {
+      beacon.hideContainer();
+    } else {
+      beacon.showContainer();
     }
-
-    this.updatePopupButton();
   }
 
   updatePopupButton() {
-    let $modal = $('.modal-user-prefs .modal');
-    let $modalList = $('.modal-user-prefs .modal-list');
+    const isBeaconHidden = this.isBeaconHidden();
+    const $modal = $('.modal-user-prefs .modal');
+    const $modalList = $('.modal-user-prefs .modal-list');
 
     if ($('.ynab-toolkit-hide-help', $modalList).length) return;
 
-    let $label = 'Show';
-    if ($('#hs-beacon').is(':visible')) { $label = 'Hide'; }
+    // it's possible the beacon doesn't actually match the state we think it should so don't
+    // set the label based on what we have in local storage so we should always use the source
+    // of truth when toggling. We'll use local storage to get the initial state.
+    const label = isBeaconHidden ? 'Hide' : 'Show';
 
     $(`<li class="ynab-toolkit-hide-help">
       <button>
         <i class="flaticon stroke help-2"></i>
-        ` + $label + ` Help Button
+        ` + label + ` Help Button
       </button>
      </li>
     `).click(() => {
-      const hide = getToolkitStorageKey('hide-help', true);
-      setToolkitStorageKey('hide-help', !hide);
-
-      $('body').toggleClass('toolkit-hide-help');
-      const accountController = ynabToolKit.shared.containerLookup('controller:accounts');
-      accountController.send('closeModal');
+      this.setHideState(!isBeaconHidden);
+      controllerLookup('controller:accounts').send('closeModal');
     }).appendTo($modalList);
 
     $modal.css({ height: '+=12px' });
