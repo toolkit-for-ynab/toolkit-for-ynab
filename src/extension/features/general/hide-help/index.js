011f93562062f1a9f810ae3f4c6aa317a7226872
Josh Madewell <josh.madewell@gmail.com>
2018-04-07 20:40:20 -0500

    Revert back to hiding the help button with CSS

--- src/extension/features/general/hide-help/index.js
+++ src/extension/features/general/hide-help/index.js
@@ -1,8 +1,10 @@
 import { Feature } from 'toolkit/extension/features/feature';
-import { getToolkitStorageKey, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
-import { getEmberView, controllerLookup } from 'toolkit/extension/utils/toolkit';
+import { i10n, getToolkitStorageKey, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
+import { controllerLookup } from 'toolkit/extension/utils/ember';
 
 export class HideHelp extends Feature {
+  injectCSS() { return require('./index.css'); }
+
   shouldInvoke() { return true; }
 
   observe(changedNodes) {
@@ -16,26 +18,17 @@ export class HideHelp extends Feature {
     this.setHiddenState(initialState);
   }
 
-  getBeacon() {
-    return getEmberView($('.self-service').attr('id'));
-  }
-
-  isBeaconHidden() {
-    return this.getBeacon().getBeaconContainer().css('display') === 'none';
-  }
-
-  setHiddenState(state) {
+  setHiddenState = (state) => {
     setToolkitStorageKey('hide-help', state);
-    const beacon = this.getBeacon();
     if (state) {
-      beacon.hideContainer();
+      $('body').addClass('toolkit-hide-help');
     } else {
-      beacon.showContainer();
+      $('body').removeClass('toolkit-hide-help');
     }
   }
 
   updatePopupButton() {
-    const isBeaconHidden = this.isBeaconHidden();
+    const isHidden = getToolkitStorageKey('hide-help', true);
     const $modal = $('.modal-user-prefs .modal');
     const $modalList = $('.modal-user-prefs .modal-list');
 
@@ -44,7 +37,7 @@ export class HideHelp extends Feature {
     // it's possible the beacon doesn't actually match the state we think it should so don't
     // set the label based on what we have in local storage so we should always use the source
     // of truth when toggling. We'll use local storage to get the initial state.
-    const label = isBeaconHidden ? 'Hide' : 'Show';
+    const label = isHidden ? i10n('app.show', 'Show') : i10n('app.hide', 'Hide');
 
     $(`<li class="ynab-toolkit-hide-help">
       <button>
@@ -53,8 +46,8 @@ export class HideHelp extends Feature {
       </button>
      </li>
     `).click(() => {
-      this.setHideState(!isBeaconHidden);
-      controllerLookup('controller:accounts').send('closeModal');
+      this.setHiddenState(!isHidden);
+      controllerLookup('accounts').send('closeModal');
     }).appendTo($modalList);
 
     $modal.css({ height: '+=12px' });
