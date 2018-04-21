50a2eb894904fbbf9722e53bc3ccade82c8cf86b
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-04-21 16:58:01 -0500

    Feature (YNAB Reports and Toolkit Reports): Highlight the income-vs-expense row on hover. This feature will be enabled by default. (#1278)
    * Add a highlight on hover to income-vs-expense tookit report
    
    * Add highlight-on-hover feature for native income vs expense. Default: true
    
    * Use same color for toolkit report

---
+++ src/extension/features/reports/income-expense-hover-highlight/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'IncomeVsExpenseHoverHighlight',
+  type: 'checkbox',
+  default: true,
+  section: 'reports',
+  title: 'Highlight Income vs Expense Row on Hover',
+  description: 'Provides a highlight over the currently hovered row on the native YNAB Income vs Expense report.'
+};
