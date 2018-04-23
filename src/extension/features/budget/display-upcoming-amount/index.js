75e01028b553a88548fb4b3d2b4dd3c2dcb002d0
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-04-23 09:55:20 -0500

    Display upcoming (#1280)
    * Fix an issue causing multiple headers to get added
    
    * Add feature to display upcoming amount next to category activity
    
    * Provide description in tooltip of upcoming total

---
+++ src/extension/features/budget/display-upcoming-amount/index.js
@@ -0,0 +1,33 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { getCurrentRouteName } from 'toolkit/extension/utils/ynab';
+import { getEmberView } from 'toolkit/extension/utils/ember';
+import { formatCurrency } from 'toolkit/extension/utils/currency';
+
+export class DisplayUpcomingAmount extends Feature {
+  injectCSS() { return require('./index.css'); }
+
+  shouldInvoke() {
+    return getCurrentRouteName().indexOf('budget') !== -1;
+  }
+
+  invoke() {
+    $('.budget-table-row.is-sub-category').each((_, element) => {
+      const { monthlySubCategoryBudgetCalculation, subCategory } = getEmberView(element.id).data;
+
+      if (monthlySubCategoryBudgetCalculation && monthlySubCategoryBudgetCalculation.upcomingTransactions) {
+        $('.budget-table-cell-activity', element)
+          .addClass('toolkit-activity-upcoming')
+          .prepend($('<div>', {
+            class: 'toolkit-activity-upcoming-amount',
+            title: `Total upcoming transaction amount in this month for ${subCategory.get('name')}`,
+            text: formatCurrency(monthlySubCategoryBudgetCalculation.upcomingTransactions)
+          }));
+      }
+    });
+  }
+
+  onRouteChanged() {
+    if (!this.shouldInvoke()) return;
+    this.invoke();
+  }
+}
