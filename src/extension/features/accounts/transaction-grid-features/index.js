a242c04357423578f24cf337c1e05db6e8d68347
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-05-02 22:08:28 -0500

    Fixed an issue where running balance would not load if navigating to a budget/account from the "Select a Budget" page. (#1289)
    * Add an optional invoke() function to the TransactionGridFeature sub-features
    
    * Initialize the running balance for all accounts in the new invoke method
    
    Before, we were only initializing running balance in willInvoke this
    caused a bug where the code would run before you've selected a budget
    (on the users/budgets page). Now, we will always attempt to initialize
    the running balance on accounts whenever the feature is invoked which
    happens on route changes. We've included a guard preventing this function
    from processing once it's already initialized.

--- src/extension/features/accounts/transaction-grid-features/index.js
+++ src/extension/features/accounts/transaction-grid-features/index.js
@@ -4,6 +4,7 @@ import { RunningBalance } from './running-balance';
 import { CheckNumbers } from './check-numbers';
 import { ReconciledTextColor } from './reconciled-text-color';
 import { controllerLookup, componentLookup } from 'toolkit/extension/utils/ember';
+import { logToolkitError } from 'toolkit/core/common/errors/with-toolkit-error';
 
 export class TransactionGridFeatures extends Feature {
   features = [
