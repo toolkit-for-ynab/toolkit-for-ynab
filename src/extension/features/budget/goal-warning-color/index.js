f6b9091f3fd3e437583d3a0b82f5353a2acb88f3
Jon Scheiding <jonscheiding@gmail.com>
2018-05-18 21:19:53 -0400

    Re-incorporate legacy budget row styles into "blue indicator" feature

--- src/extension/features/budget/goal-warning-color/index.js
+++ src/extension/features/budget/goal-warning-color/index.js
@@ -1,5 +1,7 @@
 import { Feature } from 'toolkit/extension/features/feature';
 
+import '../../../legacy/features/budget-category-info/main';
+
 export class GoalWarningColor extends Feature {
   injectCSS() { return require('./index.css'); }
 }
