4327ece7cea64c418ebbeca4e1dc7e7d5d0624f7
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-05-20 13:18:22 -0500

    Use isTransferTransaction to determine if a transaction is transfer (#1308)

--- src/extension/utils/transaction.js
+++
@@ -1,6 +0,0 @@
-export function isTransfer(transaction) {
-  let masterCategoryId = transaction.get('masterCategoryId');
-  let subCategoryId = transaction.get('subCategoryId');
-
-  return masterCategoryId === null || subCategoryId === null;
-}
