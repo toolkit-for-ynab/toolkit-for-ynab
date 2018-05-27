755c50269cd06991fe48c0a49f9057d63c996367
Josh Madewell <josh.madewell@gmail.com>
2018-05-27 13:12:01 -0500

    Refactor days-of-buffering to improve performance and understanding

--- src/extension/features/budget/days-of-buffering/index.js
+++ src/extension/features/budget/days-of-buffering/index.js
@@ -1,50 +1,110 @@
+import { i10n } from 'toolkit/extension/utils/toolkit';
 import { getEntityManager } from 'toolkit/extension/utils/ynab';
 import { Feature } from 'toolkit/extension/features/feature';
-import { getCurrentRouteName, getSidebarViewModel } from 'toolkit/extension/utils/ynab';
-import { generateReport, outflowTransactionFilter } from './helpers';
-import { render, shouldRender } from './render';
+import { currentRouteIsBudgetPage, getSidebarViewModel } from 'toolkit/extension/utils/ynab';
+import { formatCurrency } from 'toolkit/extension/utils/currency';
 
 export class DaysOfBuffering extends Feature {
+  _lookbackMonths = parseInt(ynabToolKit.options.DaysOfBufferingHistoryLookup);
+  _lookbackDays = this._lookbackMonths * 30;
+
   injectCSS() { return require('./index.css'); }
 
-  transactionFilter = null;
-
-  constructor() {
-    super();
-    this.historyLookup = parseInt(ynabToolKit.options.DaysOfBufferingHistoryLookup);
-    this.lastRenderTime = 0;
-    this.observe = this.invoke;
+  shouldInvoke() {
+    return currentRouteIsBudgetPage() && !document.querySelector('toolkit-days-of-buffering');
   }
 
   invoke() {
-    if (!this.shouldInvoke() || !shouldRender(this.lastRenderTime)) return;
+    const elligibleTransactions = getEntityManager().getAllTransactions().filter(this._elligibleTransactionFilter);
+    const onBudgetBalance = getSidebarViewModel().getOnBudgetAccountsBalance();
+    const calcuation = this._calculateDaysOfBuffering(onBudgetBalance, elligibleTransactions);
+    this._updateDisplay(calcuation);
+  }
 
-    if (this.transactionFilter === null) {
-      this.transactionFilter = outflowTransactionFilter(this.historyLookup);
+  onRouteChanged() {
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+
+  _updateDisplay(calcuation) {
+    const { averageDailyOutflow, daysOfBuffering, totalDays, totalOutflow } = calcuation;
+    const daysOfBufferingContainer = document.querySelector('.toolkit-days-of-buffering');
+    let $displayElement = $(daysOfBufferingContainer);
+    if (!daysOfBufferingContainer) {
+      $displayElement = $('<div>', { class: 'budget-header-item budget-header-days toolkit-days-of-buffering' })
+        .append($('<div>', {
+          class: 'budget-header-days-age',
+          title: i10n('budget.dob.tooltip', 'Don\'t like AoM? Try this out instead!')
+        }))
+        .append($('<div>', {
+          class: 'budget-header-days-label',
+          text: i10n('budget.dob.title', 'Days of Buffering'),
+          title: i10n('budget.dob.tooltip', 'Don\'t like AoM? Try this out instead!')
+        }));
+
+      $('.budget-header-flexbox').append($displayElement);
     }
 
-    const transactions = getEntityManager().getAllTransactions().filter(this.transactionFilter);
-    let report = { ableToGenerate: true };
+    if (calcuation.notEnoughDates) {
+      $('.budget-header-days-age', $displayElement).text('???');
+      $('.budget-header-days-age', $displayElement).attr('title', i10n('budget.dob.noHistory', 'Your budget history is less than 15 days. Go on with YNAB a while.'));
+    } else {
+      const dayText = daysOfBuffering === 1.0 ? i10n('budget.ageOfMoneyDays.one', 'day') : i10n('budget.ageOfMoneyDays.other', 'days');
+      $('.budget-header-days-age', $displayElement).text(`${daysOfBuffering} ${dayText}`);
+      $('.budget-header-days-age', $displayElement).attr('title', `${i10n('budget.dob.outflow', 'Total outflow')}: ${formatCurrency(totalOutflow)}
+${i10n('budget.dob.days', 'Total days of budgeting')}: ${totalDays}
+${i10n('budget.dob.avgOutflow', 'Average daily outflow')}: ~${formatCurrency(averageDailyOutflow)}`);
+    }
+  }
 
-    try {
-      report = generateReport(transactions, this.accountBalance());
-    } catch (e) {
-      report = { ableToGenerate: false };
+  _calculateDaysOfBuffering = (balance, transactions) => {
+    const { dates, totalOutflow } = transactions.reduce((reduced, current) => {
+      const { amount, date } = current.getProperties('amount', 'date');
+      reduced.dates.set(date.format());
+      reduced.totalOutflow += amount;
+      return reduced;
+    }, { dates: new Map(), totalOutflow: 0 });
+    const averageDailyOutflow = Math.abs(totalOutflow / this._lookbackDays);
+
+    let daysOfBuffering = balance / averageDailyOutflow;
+    if (daysOfBuffering < 10) {
+      daysOfBuffering = daysOfBuffering.toFixed(1);
+    } else {
+      daysOfBuffering = Math.floor(daysOfBuffering);
     }
 
-    this.render(report);
+    const notEnoughDates = dates.size < 15;
+    if (notEnoughDates) {
+      daysOfBuffering = null;
+    }
+
+    return {
+      averageDailyOutflow,
+      daysOfBuffering,
+      notEnoughDates,
+      totalDays: dates.size,
+      totalOutflow
+    };
   }
 
-  shouldInvoke() {
-    return getCurrentRouteName().indexOf('budget') > -1;
-  }
+  _elligibleTransactionFilter = (transaction) => {
+    const today = new ynab.utilities.DateWithoutTime();
 
-  render(report) {
-    render(report);
-    this.lastRenderTime = Date.now();
-  }
+    let isElligibleDate = false;
+    if (this._lookbackDays === 0) {
+      isElligibleDate = true;
+    } else {
+      isElligibleDate = transaction.get('date').daysApart(today) < this._lookbackDays;
+    }
 
-  accountBalance() {
-    return getSidebarViewModel().getOnBudgetAccountsBalance();
+    return (
+      isElligibleDate &&
+      !transaction.get('isTombstone') &&
+      !transaction.get('payee.isInternal') &&
+      !transaction.isTransferTransaction() &&
+      transaction.get('account.onBudget') &&
+      transaction.get('amount') < 0
+    );
   }
 }
