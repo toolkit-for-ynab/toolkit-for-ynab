cf08f47e50bc4f9164d777ae5203232ccf90bcef
David Baldon <dbaldon@users.noreply.github.com>
2018-06-21 10:49:36 -0500

    Updated show import notification in navigation (#1364)
    sidebar to use underlining to indicate accounts with transactions to be
    imported. The account name will be underlined in white or red depending
    on the users selection.

--- src/extension/features/general/import-notification/index.js
+++ src/extension/features/general/import-notification/index.js
@@ -1,9 +1,10 @@
 import { Feature } from 'toolkit/extension/features/feature';
 import { getEmberView } from 'toolkit/extension/utils/ember';
+import { l10n } from 'toolkit/extension/utils/toolkit';
 
 export class ImportNotification extends Feature {
   isActive = false;
-  importClass = 'import-notification';
+  importClass = 'ynabtk-import-notification-underline';
 
   injectCSS() { return require('./index.css'); }
 
@@ -47,24 +48,31 @@ export class ImportNotification extends Feature {
   checkImportTransactions() {
     this.isActive = true;
 
-    $('.' + this.importClass).remove();
     $('.nav-account-row').each((index, row) => {
       let account = getEmberView($(row).attr('id')).get('data');
+      let accountSpan = $(row).find('.nav-account-name > .nav-account-name-val > span');
+      if (accountSpan.length) {
+        // Remove the title attribute and our underline class in case the account no longer has txns to be imported
+        $(accountSpan).removeAttr('title').removeClass(this.importClass);
 
-      // Check for both functions should be temporary until all users have been switched to new bank data
-      // provider but of course we have no good way of knowing when that has occurred.
-      if (typeof account.getDirectConnectEnabled === 'function' && account.getDirectConnectEnabled() ||
-          typeof account.getIsDirectImportActive === 'function' && account.getIsDirectImportActive()) {
-        let t = new ynab.managers.DirectImportManager(ynab.YNABSharedLib.defaultInstance.entityManager, account);
-        let transactions = t.getImportTransactionsForAccount(account);
+        let currentTitle = $(row).find('.nav-account-name').prop('title');
 
-        if (transactions.length >= 1) {
-          $(row)
-            .find('.nav-account-notification')
-            .append('<a class="notification ' + this.importClass + '">' + transactions.length + '</a>');
+        // Check for both functions should be temporary until all users have been switched to new bank data
+        // provider but of course we have no good way of knowing when that has occurred.
+        if (typeof account.getDirectConnectEnabled === 'function' && account.getDirectConnectEnabled() ||
+            typeof account.getIsDirectImportActive === 'function' && account.getIsDirectImportActive()) {
+          let t = new ynab.managers.DirectImportManager(ynab.YNABSharedLib.defaultInstance.entityManager, account);
+          let transactions = t.getImportTransactionsForAccount(account);
+
+          if (transactions.length >= 1) {
+            $(accountSpan)
+              .addClass(this.importClass)
+              .attr('title', currentTitle + ` - ${transactions.length} ` + l10n('toolkit.import.notification', 'transaction(s) to be imported.'));
+          }
         }
       }
     });
+
     this.isActive = false;
   }
 }
