a15cfa20aa292812f8fcefbc3bd8981026c5c70f
Josh Madewell <josh.madewell@gmail.com>
2018-09-09 23:11:50 -0500

    Adding collections and utils for reports

--- src/extension/utils/date.js
+++ src/extension/utils/date.js
@@ -1,5 +1,35 @@
+import { l10nMonth, MonthStyle } from './toolkit';
+import { getEntityManager } from './ynab';
+
 export function getCurrentDate(format) {
   return ynabDate(format, false);
+}
+
+export function getToday() {
+  const today = new ynab.utilities.DateWithoutTime();
+  return today.clone();
+}
+
+export function getFirstMonthOfBudget() {
+  return getEntityManager().getFirstMonthForBudget().clone();
+}
+
+export function localizedMonthAndYear(date) {
+  const month = date.getMonth(MonthStyle.Short);
+  return `${l10nMonth(month)} ${date.getYear()}`;
+}
+
+export function sortByTransactionDate(a, b) {
+  const dateA = a.get('date');
+  const dateB = b.get('date');
+
+  if (dateA.isBefore(dateB)) {
+    return -1;
+  } else if (dateA.isAfter(dateB)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 function ynabDate(format) {
