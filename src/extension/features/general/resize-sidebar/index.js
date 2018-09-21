ee8e0eee04574099b24b435f8ce181c4fab56917
Wolfsblvt <wolfsblvt@gmx.de>
2018-09-21 09:29:13 +0200

    Resize sidebar, first checkin (not really working yet)

---
+++ src/extension/features/general/resize-sidebar/index.js
@@ -0,0 +1,77 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { getToolkitStorageKey, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
+/* global $, console, require */
+
+const resizerClass = 'sidebar-resizer';
+const widthCssVar = '--tk-number-resize-sidebar-width';
+const widthStorageKey = 'resize-sidebar-widthsssssssss';
+const mindWidth = 200;
+const maxWidth = 800;
+
+export class ResizeSidebar extends Feature {
+  injectCSS() {
+    return require('./index.css');
+  }
+
+  isMouseDown = false;
+
+  shouldInvoke() { return true; }
+
+  invoke() {
+    const width = getToolkitStorageKey(widthStorageKey);
+    this.updateProperty(width);
+    this.addSidebarResizer();
+  }
+
+  addSidebarResizer() {
+    const sidebar = $('nav.sidebar');
+
+    // Build the resizer element
+    const resizer = $('<div>', { class: resizerClass });
+
+    // Adds the resizer after the sidebar now
+    $(sidebar).after(resizer);
+
+    // Subscribe event on resizer to start resizing
+    $(resizer).click((event) => event.stopPropagation())
+      .mousedown((event) => {
+        this.toggleResize(true, event);
+      });
+  }
+
+  onMouseMove(event) {
+    if (!this.isMouseDown) { return; }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.updateProperty(event.clientX);
+  }
+
+  onMouseUp(event) {
+    this.toggleResize(false, event);
+  }
+
+  toggleResize(isResizing, event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isMouseDown = isResizing;
+    if (isResizing) {
+      $('body').on('mousemove', this.bindOnMouseMove = this.onMouseMove.bind(this));
+      $('body').on('mouseup', this.bindOnMouseUp = this.onMouseUp.bind(this));
+    } else {
+      $('body').off('mousemove', this.bindOnMouseMove);
+      $('body').off('mouseup', this.bindOnMouseUp);
+    }
+  }
+
+  updateProperty(width) {
+    if (typeof width !== 'number') { return; }
+
+    // Keep the width between our defined borders
+    width = Math.max(mindWidth, Math.min(width, maxWidth));
+
+    $(':root')[0].style.setProperty(widthCssVar, width);
+    setToolkitStorageKey(widthStorageKey, width);
+  }
+}
