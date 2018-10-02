e4c6a322396c3d161512556345dabc5d2f7d2076
OlegWock <olegwock@gmail.com>
2018-10-02 23:27:07 +0300

    Automatically mark transaction as cleared #1429

---
+++ src/extension/features/accounts/auto-mark-as-cleared/index.js
@@ -0,0 +1,26 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
+
+export class AutomaticallyMarkAsCleared extends Feature {
+  shouldInvoke() {
+    return isCurrentRouteAccountsPage();
+  }
+
+  observe(changedNodes) {
+    if (!changedNodes.has('flaticon copyright solid')) return;
+
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+
+
+  invoke() {
+    console.log('Click clag!');
+    // Calling click at DOM node and not jQuery because jQuery sometimes doesn't work properly
+    let $markClearedButton = $('.ynab-cleared:not(.is-cleared)');
+    if ($markClearedButton.length !== 0) {
+      $markClearedButton[0].click();
+    }
+  }
+}
