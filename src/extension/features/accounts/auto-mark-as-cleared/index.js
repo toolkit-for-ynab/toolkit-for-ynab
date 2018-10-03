080a1829b46ed8d7623bb5bc85ef220f170b75e6
Oleg <OlegWock@gmail.com>
2018-10-03 17:39:39 +0300

    Added "Automatically mark as cleared" feature (#1432)
    * Automatically mark transaction as cleared #1429
    
    * Removed redundant console.log call
    
    * Don't trigger on transaction editing
    
    * Don't trigger on transaction editing
    
    * Typo

---
+++ src/extension/features/accounts/auto-mark-as-cleared/index.js
@@ -0,0 +1,25 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
+
+export class AutomaticallyMarkAsCleared extends Feature {
+  shouldInvoke() {
+    return isCurrentRouteAccountsPage();
+  }
+
+  observe(changedNodes) {
+    if (!changedNodes.has('flaticon copyright solid')) return;
+
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+
+
+  invoke() {
+    // Calling click at DOM node and not jQuery because jQuery sometimes doesn't work properly
+    let $markClearedButton = $('.is-adding .ynab-cleared:not(.is-cleared)');
+    if ($markClearedButton.length !== 0) {
+      $markClearedButton[0].click();
+    }
+  }
+}
