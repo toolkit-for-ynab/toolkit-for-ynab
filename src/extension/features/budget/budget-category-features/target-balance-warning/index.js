a64ff685daaa2a7db1ca43c157551b5109d457ad
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-10-07 02:10:04 -0500

    Update selectors/styles for goal-warning-color and target balance warning (#1444)

--- src/extension/features/budget/budget-category-features/target-balance-warning/index.js
+++ src/extension/features/budget/budget-category-features/target-balance-warning/index.js
@@ -2,6 +2,8 @@ import { Feature } from 'toolkit/extension/features/feature';
 import { isCurrentRouteBudgetPage } from 'toolkit/extension/utils/ynab';
 import { CategoryAttributes } from 'toolkit/extension/features/budget/budget-category-features';
 
+const REMOVE_CLASSES = ['positive', 'zero'];
+
 export class TargetBalanceWarning extends Feature {
   shouldInvoke() { return isCurrentRouteBudgetPage(); }
 
@@ -9,17 +11,32 @@ export class TargetBalanceWarning extends Feature {
     const targetBalance = ynab.constants.SubCategoryGoalType.TargetBalance;
     const targetBalanceRows = [...document.querySelectorAll(`.budget-table-row[data-${CategoryAttributes.GoalType}="${targetBalance}"`)];
     targetBalanceRows.forEach((element) => {
-      const currencyElement = element.querySelector('.ynab-new-budget-available-number .currency');
-
-      if (!currencyElement) {
-        return;
-      }
+      const inspectorSelectors = [
+        '.budget-inspector .inspector-overview-available dt',
+        '.budget-inspector .inspector-overview-available .ynab-new-budget-available-number',
+        '.budget-inspector .inspector-overview-available .ynab-new-budget-available-number .currency'
+      ].join(',');
+      const rowElements = $('.ynab-new-budget-available-number', element);
+      const inspectorElements = $(inspectorSelectors);
 
       if (element.hasAttribute(`data-${CategoryAttributes.GoalUnderFunded}`)) {
-        currencyElement.classList.remove('positive');
-        currencyElement.classList.add('cautious');
+        REMOVE_CLASSES.forEach((className) => {
+          rowElements.removeClass(className);
+        });
+
+        rowElements.addClass('cautious');
       } else {
-        currencyElement.classList.remove('cautious');
+        rowElements.removeClass('cautious');
+      }
+
+      if ($(`.budget-inspector[data-${CategoryAttributes.GoalUnderFunded}]`).length) {
+        REMOVE_CLASSES.forEach((className) => {
+          inspectorElements.removeClass(className);
+        });
+
+        inspectorElements.addClass('cautious');
+      } else {
+        inspectorElements.removeClass('cautious');
       }
     });
   }
