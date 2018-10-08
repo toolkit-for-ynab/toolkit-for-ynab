ab7481070a5f6e471b285c9860576e8a0c7c2304
Josh Madewell <joshmadewell@users.noreply.github.com>
2018-10-08 10:57:58 -0500

    Fixed an issue where "Unhighlight Positive Inflows" hid the new budget page icons added by YNAB. (#1448)
    * Remove legacy toolkit report classes, add currency to react-reports
    
    * Adding some reusable color variables
    
    * Update styles for remove-positive-highlight to not hide icons
    
    The hope was to rewrite this using SASS but unfortunately, SASS injects
    styles into the bundle which means we can't be smart about when we load
    it, it's always loaded. There may be things we can do in the future
    to make it so features can use SASS but until then, I'll just commit the
    compiled css.

--- src/extension/features/toolkit-reports/pages/net-worth/components/legend/component.jsx
+++ src/extension/features/toolkit-reports/pages/net-worth/components/legend/component.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { formatCurrency } from 'toolkit/extension/utils/currency';
+import { Currency } from 'toolkit-reports/common/components/currency';
 import './styles.scss';
 
 export const Legend = (props) => ((
@@ -10,21 +10,21 @@ export const Legend = (props) => ((
         <div className="tk-net-worth-legend__icon-debts"></div>
         <div className="tk-mg-l-05">Debts</div>
       </div>
-      <div>{formatCurrency(props.debts)}</div>
+      <div><Currency value={props.debts} /></div>
     </div>
     <div className="tk-mg-05 tk-pd-r-1 tk-border-r">
       <div className="tk-flex tk-mg-b-05 tk-align-items-center">
         <div className="tk-net-worth-legend__icon-assets"></div>
         <div className="tk-mg-l-05">Assets</div>
       </div>
-      <div>{formatCurrency(props.assets)}</div>
+      <div><Currency value={props.assets} /></div>
     </div>
     <div className="tk-mg-05 tk-pd-r-1">
       <div className="tk-flex tk-mg-b-05 tk-align-items-center">
         <div className="tk-net-worth-legend__icon-net-worths"></div>
         <div className="tk-mg-l-05">Net Worth</div>
       </div>
-      <div>{formatCurrency(props.netWorth)}</div>
+      <div><Currency value={props.netWorth} /></div>
     </div>
   </React.Fragment>
 ));
