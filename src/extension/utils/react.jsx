c478c0631cd6c97704772cf3b444b327b709e972
Josh Madewell <josh.madewell@gmail.com>
2019-02-10 14:46:36 -0600

    Reactify and fix hide-help-button

--- src/extension/utils/react.jsx
+++ src/extension/utils/react.jsx
@@ -1,32 +1,32 @@
 import * as ReactDOM from 'react-dom';
 
-export function componentAppend(component, element) {
+export function componentAppend(renderable, element) {
   const div = document.createElement('div');
-  ReactDOM.render(component, div);
+  ReactDOM.render(renderable, div);
   while (div.children.length) {
     element.append(div.firstChild);
   }
 }
 
-export function componentPrepend(component, element) {
+export function componentPrepend(renderable, element) {
   const div = document.createElement('div');
-  ReactDOM.render(component, div);
+  ReactDOM.render(renderable, div);
   while (div.children.length) {
     element.prepend(div.firstChild);
   }
 }
 
-export function componentAfter(component, element) {
+export function componentAfter(renderable, element) {
   const div = document.createElement('div');
-  ReactDOM.render(component, div);
+  ReactDOM.render(renderable, div);
   while (div.children.length) {
     element.after(div.firstChild);
   }
 }
 
-export function componentBefore(component, element) {
+export function componentBefore(renderable, element) {
   const div = document.createElement('div');
-  ReactDOM.render(component, div);
+  ReactDOM.render(renderable, div);
   while (div.children.length) {
     element.before(div.firstChild);
   }
