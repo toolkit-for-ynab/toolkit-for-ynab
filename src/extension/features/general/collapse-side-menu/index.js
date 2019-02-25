1d3302f7441dc1c4c19248bd7306adba35094d20
Josh Madewell <josh.madewell@gmail.com>
2019-02-24 21:30:56 -0600

    Change text of collapse button to expand when collapased

--- src/extension/features/general/collapse-side-menu/index.js
+++ src/extension/features/general/collapse-side-menu/index.js
@@ -40,14 +40,17 @@ export class CollapseSideMenu extends Feature {
       class: 'ynabtk-collapse-link'
     }).append($('<span>', {
       class: 'ember-view ynabtk-collapse-icon flaticon stroke left-circle-4'
-    })).append(l10n('toolkit.collapse', 'Collapse'))).click(() => {
+    })).append($('<span>', {
+      class: 'tk-collapse-toggle-text',
+      text: getToolkitStorageKey('isCollapsed', false) ? l10n('toolkit.expand', 'Expand') : l10n('toolkit.collapse', 'Collapse')
+    }))).click(() => {
       const isCollapsed = getToolkitStorageKey('isCollapsed');
       setToolkitStorageKey('isCollapsed', !isCollapsed);
       this.applyState();
     });
 
     $('.nav-main').append(button);
-    $('.ynabtk-collapse-link, .ynabtk-navlink-reports-link').addClass('collapsable');
+    $('.ynabtk-collapse-link, .ynabtk-navlink-reports-link');
   }
 
   applyState() {
@@ -64,6 +67,9 @@ export class CollapseSideMenu extends Feature {
         $('.ynabtk-collapse-link span').addClass('ynabtk-nav-link-collapsed');
         $('.ynabtk-collapse-icon').removeClass('left-circle-4').addClass('right-circle-4');
         this.getHideElements().hide();
+
+        $('.tk-collapse-toggle-text').text(l10n('toolkit.expand', 'Expand'));
+
         Ember.run.next(() => {
           window.dispatchEvent(new Event('resize'));
         });
@@ -79,6 +85,9 @@ export class CollapseSideMenu extends Feature {
         $('.ynabtk-collapse-link span').removeClass('ynabtk-nav-link-collapsed');
         $('.ynabtk-collapse-icon').removeClass('right-circle-4').addClass('left-circle-4');
         this.getHideElements().show();
+
+        $('.tk-collapse-toggle-text').text(l10n('toolkit.collapse', 'Collapse'));
+
         Ember.run.next(() => {
           window.dispatchEvent(new Event('resize'));
         });
@@ -88,11 +97,10 @@ export class CollapseSideMenu extends Feature {
 
   getHideElements() {
     return $(`
-      .button-prefs.button-prefs-budget,
       .nav-accounts,
       .button-sidebar.nav-add-account,
       .referral-program,
-      .button-prefs.button-prefs-user
+      .sidebar-nav-menu
     `);
   }
 }
