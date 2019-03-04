c53dfec1ddad81a4bb159f3adf4721ed3fb7a56b
Corinne Hoener <corinne.hoener@gmail.com>
2019-03-03 20:00:55 -0500

    Feature: Hide Total Available (#1542)
    * Add Hide Total Available feature
    
    * Linting fixes

---
+++ src/extension/features/budget/hide-total-available/index.js
@@ -0,0 +1,32 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteBudgetPage } from 'toolkit/extension/utils/ynab';
+import { l10n } from 'toolkit/extension/utils/toolkit';
+
+/**
+ * Hides the "Total Available" section of the budget inspector.
+ */
+export class HideTotalAvailable extends Feature {
+  shouldInvoke() {
+    return isCurrentRouteBudgetPage();
+  }
+
+  onRouteChanged() {
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+
+  invoke() {
+    const budgetInspector = $('.budget-inspector');
+
+    // Attempt to target the english string, `TOTAL AVAILABLE`, but if that's not
+    // found, try the localized version of the string.
+    const englishHeading = 'TOTAL AVAILABLE';
+    const localizedHeading = l10n('inspector.totalAvailable', englishHeading);
+    const headingEl =
+      budgetInspector.find(`h3:contains(${englishHeading})`)[0] ||
+      budgetInspector.find(`h3:contains(${localizedHeading})`)[0];
+
+    $(headingEl).nextUntil('h3').andSelf().addClass('hidden');
+  }
+}
