909d16bb5132989bb9ce58e0baca6cd781cd9851
Andrew Gisler <36006378+ajgisler@users.noreply.github.com>
2019-08-08 09:23:25 -0500

    Feature: Add ability to mark Monthly Funding Goal as a "Max Spending Goal"  (#1609)
    * Added checkbox to Monthly Funding Goal so a goal can be set as the max desired spending each month.
    
    * Fixed several bugs. Backing up code.
    
    * Recommitted.
    
    * Fixed compatibility issues between features goal-warning-color and remove-positive-highlight.
    
    * Fixed several issues with the Available column showing wrong coloring including with other toolkit features.
    
    * Fixed CSS.

---
+++ src/extension/features/budget/budget-spending-goal/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'BudgetSpendingGoal',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Spending Goal',
+  description:
+    'Allows the monthly funding goal to be set as a max spending goal for each category.',
+};
