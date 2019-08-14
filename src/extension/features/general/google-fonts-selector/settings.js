e66e457dc6cea1989907f8de37f512d84a6f4716
Pieter Beulque <pieterbeulque@gmail.com>
2019-08-14 22:00:50 +0200

    Add system-ui font option (#1627)

--- src/extension/features/general/google-fonts-selector/settings.js
+++ src/extension/features/general/google-fonts-selector/settings.js
@@ -4,7 +4,7 @@ module.exports = {
   default: '0',
   section: 'general',
   title: 'Interface Font',
-  description: 'Select a font from the Google Fonts library.',
+  description: 'Select a font from the Google Fonts library or choose to use your system font.',
   options: [
     { name: 'Default', value: '0' },
     { name: 'Open Sans', value: '1' },
@@ -12,5 +12,6 @@ module.exports = {
     { name: 'Roboto Condensed', value: '3' },
     { name: 'Droid Sans', value: '4' },
     { name: 'Inconsolata', value: '5' },
+    { name: 'System font', value: '6' },
   ],
 };
