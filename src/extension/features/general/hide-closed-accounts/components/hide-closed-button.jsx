82838266c0bd7aff7f8a7e0f6fdb1adb38280356
Alan Hoskins <alanhoskins@pop-os.localdomain>
2019-10-13 10:47:58 -0400

    Add feature to hide closed accounts

---
+++ src/extension/features/general/hide-closed-accounts/components/hide-closed-button.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import { controllerLookup } from 'toolkit/extension/utils/ember';
+import { l10n, getToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
+
+export const HideClosedButton = ({ toggleHiddenState }) => {
+  const isHidden = getToolkitStorageKey('hide-closed', true);
+  const label = isHidden ? l10n('app.show', 'Show') : l10n('app.hide', 'Hide');
+
+  const toggleHidden = () => {
+    toggleHiddenState(!isHidden);
+    controllerLookup('application').send('closeModal');
+  };
+
+  return (
+    <li onClick={toggleHidden}>
+      <button>
+        <i className="flaticon stroke help-2" />
+        {` ${label}`} Closed Accounts
+      </button>
+    </li>
+  );
+};
+
+HideClosedButton.propTypes = {
+  toggleHiddenState: PropTypes.func.isRequired,
+};
