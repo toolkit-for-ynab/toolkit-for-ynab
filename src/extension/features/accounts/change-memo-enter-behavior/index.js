a150ab759d279b520044aae63a424557f842a237
Alan Hoskins <alanhoskins@pop-os.localdomain>
2019-10-13 20:43:21 -0400

    Changed memo fields default behavior on enter

---
+++ src/extension/features/accounts/change-memo-enter-behavior/index.js
@@ -0,0 +1,38 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
+
+export class ChangeMemoEnterBehavior extends Feature {
+  shouldInvoke() {
+    return (
+      isCurrentRouteAccountsPage() &&
+      $('.ynab-grid-body-row.is-adding, .ynab-grid-body-row.is-editing').length
+    );
+  }
+
+  invoke() {
+    const $addEditRow = $('.ynab-grid-body-row.is-adding, .ynab-grid-body-row.is-editing');
+    const $memoInput = $('.ynab-grid-cell-memo input', $addEditRow);
+
+    if (!$memoInput[0].getAttribute('data-toolkit-memo-behavior')) {
+      $memoInput[0].setAttribute('data-toolkit-memo-behavior', true);
+      $memoInput.keydown(this.applyNewEnterBehavior);
+    }
+  }
+
+  applyNewEnterBehavior(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      $('.ynab-grid-cell-outflow input').focus();
+    }
+  }
+
+  observe(changedNodes) {
+    if (!changedNodes.has('ynab-grid-body')) return;
+
+    if (this.shouldInvoke()) {
+      this.invoke();
+    }
+  }
+}
