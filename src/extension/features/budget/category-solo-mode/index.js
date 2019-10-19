282b97d7106d2e88b6a879bc66dfb51efdb73a76
Philip Sommer <phso@mailbox.org>
2019-10-19 21:40:12 +0200

    with additional checkbox

--- src/extension/features/budget/category-solo-mode/index.js
+++ src/extension/features/budget/category-solo-mode/index.js
@@ -1,21 +1,43 @@
 import { Feature } from 'toolkit/extension/features/feature';
+import { isCurrentRouteBudgetPage } from 'toolkit/extension/utils/ynab';
 
 export class CategorySoloMode extends Feature {
+  // ===== LISTENERS ======
   shouldInvoke() {
-    // since we are adding the listener to the entire document,
-    // we should invoke on page load.
-    return true;
+    return isCurrentRouteBudgetPage();
+  }
+
+  onRouteChanged() {
+    if (this.shouldInvoke()) this.invoke();
   }
 
   invoke() {
+    $('.budget-toolbar').append(
+      '<span class="ember-view"><input type="checkbox" id="cat-solo-mode" class="ember-view" checked>' +
+        '<label class="ember-view button" for="cat-solo-mode">Enable Category Solo Mode</label></span>'
+    );
     $('.js-budget-table-cell-collapse').on('click', this.toggleCategorySoloMode);
+    $('.budget-toolbar #cat-solo-mode').on('click', this.initializeState);
+    setTimeout(this.initializeState, 500);
   }
 
+  // ===== Custom functions ======
+  initializeState = () => {
+    if ($('.budget-toolbar #cat-solo-mode').prop('checked')) {
+      $('.js-budget-table-cell-collapse.down')
+        .first()
+        .click()
+        .click();
+    }
+  };
+
   toggleCategorySoloMode = event => {
-    if ($(event.target).hasClass('right')) {
-      $('.js-budget-table-cell-collapse.down').each(function() {
-        if ($(this).id !== event.target.id && $(this).hasClass('down')) $(this).click();
-      });
+    if ($('.budget-toolbar #cat-solo-mode').prop('checked')) {
+      if ($(event.target).hasClass('right')) {
+        $('.js-budget-table-cell-collapse.down').each(function() {
+          if ($(this).id !== event.target.id) $(this).click();
+        });
+      }
     }
   };
 }
