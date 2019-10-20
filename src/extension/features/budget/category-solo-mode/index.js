9e6842aa1be42ddb7a8646fe6fd069697c78ca42
Philip Sommer <phso@mailbox.org>
2019-10-20 20:06:40 +0200

    Added localStorage Support for Feature

--- src/extension/features/budget/category-solo-mode/index.js
+++ src/extension/features/budget/category-solo-mode/index.js
@@ -13,8 +13,9 @@ export class CategorySoloMode extends Feature {
 
   invoke() {
     $('.budget-toolbar').append(
-      '<span class="ember-view"><input type="checkbox" id="cat-solo-mode" class="ember-view" checked>' +
-        '<label class="ember-view button" for="cat-solo-mode">Enable Category Solo Mode</label></span>'
+      '<span class="ember-view"><input type="checkbox" id="cat-solo-mode" class="ember-view" ' +
+        localStorage.catSoloMode +
+        '><label class="ember-view button" for="cat-solo-mode">Enable Category Solo Mode</label></span>'
     );
     $('.js-budget-table-cell-collapse').on('click', this.toggleCategorySoloMode);
     $('.budget-toolbar #cat-solo-mode').on('click', this.initializeState);
@@ -24,10 +25,13 @@ export class CategorySoloMode extends Feature {
   // ===== Custom functions ======
   initializeState = () => {
     if ($('.budget-toolbar #cat-solo-mode').prop('checked')) {
+      localStorage.catSoloMode = 'checked';
       $('.js-budget-table-cell-collapse.down')
         .first()
         .click()
         .click();
+    } else {
+      localStorage.catSoloMode = '';
     }
   };
 
