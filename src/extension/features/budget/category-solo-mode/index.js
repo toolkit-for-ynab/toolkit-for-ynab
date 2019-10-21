799b14283b865a86a649bd56e893d75b47539166
Philip Sommer <phso@mailbox.org>
2019-10-21 14:42:30 +0200

    Added toggle-master-categories feature as well
    Added this to my code and removed it as separate plugin.

--- src/extension/features/budget/category-solo-mode/index.js
+++ src/extension/features/budget/category-solo-mode/index.js
@@ -7,24 +7,31 @@ export class CategorySoloMode extends Feature {
     return isCurrentRouteBudgetPage();
   }
 
+  injectCSS() {
+    return require('./index.css');
+  }
+
   onRouteChanged() {
     if (this.shouldInvoke()) this.invoke();
   }
 
   invoke() {
     $('.budget-toolbar').append(
-      '<span class="ember-view"><input type="checkbox" id="cat-solo-mode" class="ember-view" ' +
+      '<span class="ember-view"><button id="all-category-expand-button">&#8597;</button><input type="checkbox" id="cat-solo-mode" class="ember-view" ' +
         localStorage.catSoloMode +
-        '><label class="ember-view button" for="cat-solo-mode">Enable Category Solo Mode</label></span>'
+        '><label class="ember-view button" for="cat-solo-mode">Solo Mode</label></span>'
     );
-    $('.js-budget-table-cell-collapse').on('click', this.toggleCategorySoloMode);
+    $('.js-budget-table-cell-collapse').on('click', this.toggleCategory);
     $('.budget-toolbar #cat-solo-mode').on('click', this.initializeState);
+    $('.budget-toolbar #all-category-expand-button').on('click', this.toggleAllCategories);
     setTimeout(this.initializeState, 500);
   }
 
   // ===== Custom functions ======
   initializeState = () => {
-    if ($('.budget-toolbar #cat-solo-mode').prop('checked')) {
+    var checked = $('.budget-toolbar #cat-solo-mode').prop('checked');
+    $('#all-category-expand-button').attr('disabled', checked);
+    if (checked) {
       localStorage.catSoloMode = 'checked';
       $('.js-budget-table-cell-collapse.down')
         .first()
@@ -35,13 +42,25 @@ export class CategorySoloMode extends Feature {
     }
   };
 
-  toggleCategorySoloMode = event => {
+  toggleCategory = event => {
     if ($('.budget-toolbar #cat-solo-mode').prop('checked')) {
       if ($(event.target).hasClass('right')) {
         $('.js-budget-table-cell-collapse.down').each(function() {
           if ($(this).id !== event.target.id) $(this).click();
         });
       }
+    }
+  };
+
+  toggleAllCategories = () => {
+    if ($('.js-budget-table-cell-collapse.right').length === 0) {
+      $('.js-budget-table-cell-collapse.down').each(function() {
+        $(this).click();
+      });
+    } else {
+      $('.js-budget-table-cell-collapse.right').each(function() {
+        $(this).click();
+      });
     }
   };
 }
--- src/extension/features/budget/category-solo-mode/settings.js
+++ src/extension/features/budget/category-solo-mode/settings.js
@@ -3,6 +3,7 @@ module.exports = {
   type: 'checkbox',
   default: false,
   section: 'budget',
-  title: 'Solo mode to have only one category open at once',
-  description: 'Keeps only the selected category open and collapses all others',
+  title: 'Category Solo Mode and Toggle All Categories',
+  description:
+    'Keeps only the selected category open collapsing all others and adds a button to the Budget Toolbar to open or close all master categories at once.',
 };
