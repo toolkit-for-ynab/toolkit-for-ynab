e4437c0d9758486faccfcaad564c1b6aecd8a512
Josh Madewell <joshmadewell@users.noreply.github.com>
2019-11-21 23:55:12 -0600

    Add auto-enable running balance feature for native running balance (#1716)

---
+++ src/extension/features/accounts/auto-enable-running-balance/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'AutoEnableRunningBalance',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Automatically Enable Running Balance',
+  description: `Enables YNAB's native "Running Balance" by default for each account register.`,
+};
