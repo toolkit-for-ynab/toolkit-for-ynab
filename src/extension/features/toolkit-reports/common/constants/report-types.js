2407e35d22084f5330de1d8ab23829e5a5bb14c4
Tony Vo <tonyvo@cs.washington.edu>
2019-11-23 10:31:57 -0800

    move tab to end

--- src/extension/features/toolkit-reports/common/constants/report-types.js
+++ src/extension/features/toolkit-reports/common/constants/report-types.js
@@ -1,26 +1,22 @@
 export const ReportKeys = {
-  AccountsReport: 'accounts-report',
   NetWorth: 'net-worth',
   SpendingByCategory: 'spending-by-category',
   SpendingByPayee: 'spending-by-payee',
   IncomeVsExpense: 'income-vs-expense',
   IncomeBreakdown: 'income-breakdown',
+  AccountsReport: 'accounts-report',
 };
 
 export const ReportNames = {
-  AccountsReport: 'Accounts Report',
   NetWorth: 'Net Worth',
   SpendingByCategory: 'Spending By Category',
   SpendingByPayee: 'Spending By Payee',
   IncomeVsExpense: 'Income vs. Expense',
   IncomeBreakdown: 'Income Breakdown',
+  AccountsReport: 'Accounts Report',
 };
 
 export const REPORT_TYPES = [
-  {
-    key: ReportKeys.AccountsReport,
-    name: ReportNames.AccountsReport,
-  },
   {
     key: ReportKeys.NetWorth,
     name: ReportNames.NetWorth,
@@ -40,5 +36,9 @@ export const REPORT_TYPES = [
   {
     key: ReportKeys.IncomeBreakdown,
     name: ReportNames.IncomeBreakdown,
+  },
+  {
+    key: ReportKeys.AccountsReport,
+    name: ReportNames.AccountsReport,
   },
 ];
