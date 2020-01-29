dfc49ce055a6ce041b7d48248db36981c52af430
Dan Ford <dan.ford@yachthavens.com>
2020-01-29 11:05:14 +0000

    add settings option

---
+++ src/extension/features/reports/spending-activity-copy/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'SpendingActivityCopy',
+  type: 'checkbox',
+  default: false,
+  section: 'reports',
+  title: 'Add Copy Transactions button to the Spending Report Activity Popup',
+  description:
+    'Adds a button to the spending report activity popup to allow you to copy the transactions to the clipboard (able to be pasted into a spreadsheet app).',
+};
