9b62d7c2ab0d72438be49133568865e8786855c7
Bryan Taylor <bryan.a.taylor@gmail.com>
2020-01-29 22:56:01 -0600

    Incorporated suggestions, but also went back to the initial approach of using an overlay to preserve Ember references to the account notification component.
    Tested various resize scenarios including window resizing. All pass.
    
    Improved efficiency of the MutationObserver by only listening to changes of the
    'style' attribute of the :root element.

--- src/extension/features/accounts/bottom-notification-bar/index.js
+++ src/extension/features/accounts/bottom-notification-bar/index.js
@@ -3,10 +3,16 @@ import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
 import { componentLookup } from 'toolkit/extension/utils/ember';
 import { addToolkitEmberHook } from 'toolkit/extension/utils/toolkit';
 
+const WIDTH_CSS_VAR = '--tk-number-resize-sidebar-width-from-code';
 const POSITIONED_AT_BOTTOM_CSS_VAR = '--tk-positioned-at-bottom';
+const NOTIFICATION_BAR_CSS_KEY = 'accounts-notification';
 
 export class BottomNotificationBar extends Feature {
+  lastResizedWidth = -1;
+
   injectCSS() {
+    // Make the account notification bar a floating overlay. We can't just reposition the bar in the
+    // DOM because it breaks the Ember references.
     return require('./index.css');
   }
 
@@ -14,20 +20,81 @@ export class BottomNotificationBar extends Feature {
     return isCurrentRouteAccountsPage();
   }
 
-  invoke() {
-    function moveNotification(element) {
-      if ($(element)[0].style.getPropertyValue(POSITIONED_AT_BOTTOM_CSS_VAR) === '1') {
-        return;
-      }
+  updateNotificationBarPositionAndWidth(type) {
+    // Update the notification bar left position with the newly resized width of the sidebar
 
-      $(element)[0].style.setProperty(POSITIONED_AT_BOTTOM_CSS_VAR, '1');
-      $(element).appendTo($(element).parent());
+    // Retrieve the value set by the resizer plugin
+    let resizedWidth = $(':root')[0].style.getPropertyValue(WIDTH_CSS_VAR);
+    if (!resizedWidth) {
+      // Fall back on the static sidebar width
+      resizedWidth = $('.sidebar').width();
     }
 
+    if (resizedWidth && (type === 'force' || this.lastResizedWidth !== resizedWidth)) {
+      this.updateLeftPosAndWidth(resizedWidth);
+      this.lastResizedWidth = resizedWidth;
+    }
+  }
+
+  updateLeftPosAndWidth(left) {
+    // Update the left position and width of the notification bar so it stays aligned with the transaction view
+    $('.' + NOTIFICATION_BAR_CSS_KEY).css('left', left + 'px');
+    $('.' + NOTIFICATION_BAR_CSS_KEY)
+      .css('width', '100vw')
+      .css('width', '-=' + left + 'px');
+  }
+
+  onRouteChanged() {
+    if (this.shouldInvoke()) {
+      this.updateNotificationBarPositionAndWidth();
+    }
+  }
+
+  invoke() {
+    // Avoid double invoke()s - this happens occasionally
+    let element = $('.' + NOTIFICATION_BAR_CSS_KEY);
+    if ($(element)[0].style.getPropertyValue(POSITIONED_AT_BOTTOM_CSS_VAR) === '1') {
+      return;
+    }
+    $(element)[0].style.setProperty(POSITIONED_AT_BOTTOM_CSS_VAR, '1');
+
+    // Listen for resizes
+    const MutationObserver =
+      window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
+    // Only listen for changes to the 'style' attribute (the sidebar resizer plugin sets the resized value here)
+    const config = {
+      attributes: true,
+      attributeFilter: ['style'],
+    };
+
+    // When the window is resized set the notification bar width
+    let $this = this;
+    function forcePositionAndWidthUpdate() {
+      $this.updateNotificationBarPositionAndWidth('force');
+    }
+
+    // Listen for changes to the 'style' attribute set by the sidebar resizer plugin
+    let sidebarObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.type === 'attributes') {
+          $this.updateNotificationBarPositionAndWidth();
+        }
+      });
+    });
+
+    // Set up our resize listeners
+    sidebarObserver.observe($(':root')[0], config);
+    window.addEventListener('resize', forcePositionAndWidthUpdate);
+
+    // When the component is initially shown we need to set up the position. This works when if it is repeatedly
+    // hidden and shown (destroyed and rendered)
     const accountNotificationProto = Object.getPrototypeOf(
       componentLookup('accounts/account-notification')
     );
+    addToolkitEmberHook(this, accountNotificationProto, 'didRender', forcePositionAndWidthUpdate);
 
-    addToolkitEmberHook(this, accountNotificationProto, 'didRender', moveNotification);
+    // Set initial position and width - works with or without resizer plugin
+    this.updateNotificationBarPositionAndWidth();
   }
 }
