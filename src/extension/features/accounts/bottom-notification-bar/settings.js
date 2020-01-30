d881c30c03a18d441e8cc1989baf2fe87c400c3d
jlubea <bryan.a.taylor@gmail.com>
2020-01-30 11:52:47 -0600

    Added bottom-notification-bar feature ... (#1764)
    * Added bottom-notification-bar feature to move the notification bar to a fixed position bottom location.
    Fixed typo in spare change description.
    
    * Fixed issue where the notification bar wasn't preperly sized when changing a
    transaction date from future scheduled to current (today or earlier).
    
    * Simplified by utilizing recommendations, but not yet working since the
    notification bar does not hide when it should.
    
    * Incorporated suggestions, but also went back to the initial approach of using an
    overlay to preserve Ember references to the account notification component.
    
    Tested various resize scenarios including window resizing. All pass.
    
    Improved efficiency of the MutationObserver by only listening to changes of the
    'style' attribute of the :root element.
    
    * Force set of initial position and width for bottom notification bar.
    
    * Use flex layout order property to reposition the bar. Much simpler.

---
+++ src/extension/features/accounts/bottom-notification-bar/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'BottomNotificationBar',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Bottom Notification Bar',
+  description:
+    'Move the notification bar to the bottom as an overlay. This prevents the transactions from "jumping around."',
+};
