eb2a2bc02cfb2d535bb04fcabd536c176da1fc71
BlueCodeThree <carliemhamilton@gmail.com>
2020-02-29 23:24:13 +1000

    adds striped rows to categories on the budget section

---
+++ src/extension/features/budget/striped-budget-rows/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'StripedBudgetRows',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Stripped Budget Rows',
+  description: 'Shows a light gray background on alternating category rows.',
+};
