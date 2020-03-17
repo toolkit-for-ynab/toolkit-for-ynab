8e82c23c8893b2d49ec8357a76585459b1a4fde1
Josh Madewell <josh.madewell@gmail.com>
2020-03-17 11:42:49 -0500

    issue-1833: Fix title for striped budget row

--- src/extension/features/budget/striped-budget-rows/settings.js
+++ src/extension/features/budget/striped-budget-rows/settings.js
@@ -3,6 +3,6 @@ module.exports = {
   type: 'checkbox',
   default: false,
   section: 'budget',
-  title: 'Stripped Budget Rows',
+  title: 'Striped Budget Rows',
   description: 'Shows a light gray background on alternating category rows.',
 };
