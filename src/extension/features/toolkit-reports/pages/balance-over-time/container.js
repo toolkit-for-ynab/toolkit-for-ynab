3a0d43b6bb696dcf25c5b9f004ebda4fe15a4e91
Tony Vo <tonyt.vo22@gmail.com>
2020-06-06 15:28:21 -0700

    remove old component

--- src/extension/features/toolkit-reports/pages/balance-over-time/container.js
+++ src/extension/features/toolkit-reports/pages/balance-over-time/container.js
@@ -1,10 +1,10 @@
 import { withReportContext } from 'toolkit-reports/common/components/report-context';
 import { BalanceOverTimeComponent } from './BalanceOverTime';
 
-function mapReportContextToProps(context) {
+const mapReportContextToProps = context => {
   return {
     filters: context.filters,
     allReportableTransactions: context.allReportableTransactions,
   };
-}
+};
 export const BalanceOverTime = withReportContext(mapReportContextToProps)(BalanceOverTimeComponent);
