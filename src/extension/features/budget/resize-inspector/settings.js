11b5f21934baf21af3a2926b2443e40ba029d5a7
Josh Madewell <josh.madewell@gmail.com>
2020-06-20 11:18:58 -0500

    Remove all "resize" features now that YNAB has their own implementation

--- src/extension/features/budget/resize-inspector/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ResizeInspector',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Allow Resizing of Inspector',
-  description:
-    'Adds a button to the Budget Toolbar that allows resizing the Budget Inspector to predetermined widths of 33% (YNAB default), 25%, 20%, 15%, or 0%. Note that smaller values maybe not be suitable on small screens.',
-};
--- src/extension/features/general/resize-sidebar/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ResizeSidebar',
-  type: 'checkbox',
-  default: false,
-  section: 'general',
-  title: 'Allow Resizing of Side Menu',
-  description:
-    'Allows the Side Menu on the left to be resized. Resizing also allows longer account names to show up completely.',
-};
