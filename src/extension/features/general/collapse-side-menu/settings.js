b4dfeac13676e5833ae71bebfb06203131f40e09
Josh Madewell <joshmadewell@users.noreply.github.com>
2020-06-20 12:06:21 -0500

    Remove all "resize" features now that YNAB has their own implementation (#1968)

--- src/extension/features/general/collapse-side-menu/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'CollapseSideMenu',
-  type: 'checkbox',
-  default: false,
-  section: 'general',
-  title: 'Collapsable Side Menu',
-  description:
-    'Adds a button that can collapse the menu on the left so you can see more of your accounts or budget data.',
-};
