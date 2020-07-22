aebad8f5fcb06d8ef095ebc8a70323254a063515
Travis Lesicka <tlesicka@users.noreply.github.com>
2020-07-22 10:11:56 -0700

    Added Color Master Category Option

---
+++ src/extension/features/budget/color-master-category-row/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'MasterCategoryRowColor',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Colored Master Category Row',
+  description: 'Adds Color to Master Category Row.',
+};
