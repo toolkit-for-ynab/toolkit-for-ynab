83a9d3e717c4371dd2017981d02bff440bd4ac33
Ryan Valizan <ryan@vnetkc.com>
2020-08-17 00:10:53 -0500

    :tada::fire: feature overhaul started

--- src/extension/features/accounts/hide-memo-col/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'HideMemoCol',
-  type: 'checkbox',
-  default: false,
-  section: 'accounts',
-  title: 'Hide Memo Column',
-  description:
-    'YNAB shows a "Memo" column in the account transaction page. If you\'d rather not see these memos, you can turn this feature on to hide them.',
-};
