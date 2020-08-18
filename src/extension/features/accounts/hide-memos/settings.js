b0adac8ff4a8c24b3ad24237ca2a3f9835fd46b0
Ryan Valizan <ryan@vnetkc.com>
2020-08-17 20:40:03 -0500

    :pencil2: typo in description of feature
    Update src/extension/features/accounts/hide-memos/settings.js
    
    credit: @a-p-o
    
    Co-authored-by: Alex Ordonez <a-p-o@users.noreply.github.com>

--- src/extension/features/accounts/hide-memos/settings.js
+++ src/extension/features/accounts/hide-memos/settings.js
@@ -4,5 +4,5 @@ module.exports = {
   default: false,
   section: 'accounts',
   title: 'Hideable Memo Column',
-  description: 'Allows Menu column to be toggle on/off using View filter on the accounts page.',
+  description: 'Allows Memo column to be toggle on/off using View filter on the accounts page.',
 };
