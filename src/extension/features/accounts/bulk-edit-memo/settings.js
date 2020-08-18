662f81bdc3c87d8782487cf00051afab684a0817
Josh Madewell <josh.madewell@gmail.com>
2020-08-17 20:55:28 -0500

    feat: make bulk edit memo a default feature

--- src/extension/features/accounts/bulk-edit-memo/settings.js
+++ src/extension/features/accounts/bulk-edit-memo/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'BulkEditMemo',
   type: 'checkbox',
-  default: false,
+  default: true,
   section: 'accounts',
   title: 'Bulk Edit Memo',
-  description: 'Adds option to edit memo field when editing multiple transactions.',
+  description: 'Adds option to edit memo on transactions to the edit menu.',
 };
