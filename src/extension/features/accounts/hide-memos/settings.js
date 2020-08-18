8408e6918d3b0c862c5cc9d39ef347c60f2e5741
Ryan Valizan <ryan@vnetkc.com>
2020-08-18 16:10:07 -0500

    :recycle: support more view options and imp desc
    This is a refactor: of PR #2119

--- src/extension/features/accounts/hide-memos/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'HideMemos',
-  type: 'checkbox',
-  default: true,
-  section: 'accounts',
-  title: 'Hidable Memo Column',
-  description:
-    'Allows memo column to be toggled on/off using the "View" filter on the accounts page.',
-};
---
+++ src/extension/features/accounts/toggle-account-columns/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ToggleAccountColumns',
+  type: 'checkbox',
+  default: true,
+  section: 'accounts',
+  title: 'Toggle Memo Column',
+  description:
+    'Adds option to toggle the memo column as visible or not to the account register "View" menu',
+};
