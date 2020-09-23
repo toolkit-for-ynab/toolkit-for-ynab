44f1df2de25c9a54c711858aa521cc399980ab57
Tony Vo <tonyvo@Tonys-MBP.hsd1.wa.comcast.net>
2020-09-22 23:54:24 -0700

    styling

--- src/extension/features/accounts/auto-reconcile/components/ReconcileInputModal.jsx
+++ src/extension/features/accounts/auto-reconcile/components/ReconcileInputModal.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles.scss';
 
 export const ReconcileInputModal = ({ isShowing, onClose }) => {
-  if (!isShowing) return null;
+  const [reconcileAmount, setReconcileAmount] = useState(0.0);
+  if (!isShowing) {
+    return null;
+  }
+  let handleReconcileAmount = e => {
+    console.log(e);
+    console.log('Curr' + reconcileAmount);
+    setReconcileAmount(e.target.value);
+  };
   return (
     <div className="tk-modal-container">
-      <div className="tk-reconcile-modal">
-        <div> Modal Header </div>
-        <div> Modal Content </div>
-        <div>
-          <button className="button button-primary" onClick={onClose}>
-            Close
-          </button>
+      <div className="tk-modal-content">
+        <div className="tk-reconcileinput-content">
+          <h2>Auto Reconcile</h2>
+          <div className="tk-modal-body">
+            <p>Enter your ammount</p>
+            <input
+              type="text"
+              className="auto-reconcile-input"
+              value={reconcileAmount}
+              onChange={e => setReconcileAmount(e.target.value)}
+            />
+          </div>
+          <div className="tk-modal-footer-action">
+            <button
+              className="auto-reconcile-action button button-primary"
+              onClick={() => {
+                console.log('Foo');
+              }}
+            >
+              {' '}
+              AutoReconcile{' '}
+            </button>
+            <button className="button button-primary" onClick={onClose}>
+              {' '}
+              Close{' '}
+            </button>
+          </div>
         </div>
       </div>
     </div>
