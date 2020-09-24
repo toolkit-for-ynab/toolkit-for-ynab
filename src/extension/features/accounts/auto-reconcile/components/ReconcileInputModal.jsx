ab263763186411dad9397c1c0532c79bec6b5068
Tony Vo <tonyt.vo22@gmail.com>
2020-09-24 00:00:13 -0700

    add workaround for backspace

--- src/extension/features/accounts/auto-reconcile/components/ReconcileInputModal.jsx
+++ src/extension/features/accounts/auto-reconcile/components/ReconcileInputModal.jsx
@@ -1,35 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../styles.scss';
 
 export const ReconcileInputModal = ({ isShowing, onClose }) => {
-  const [reconcileAmount, setReconcileAmount] = useState(0.0);
+  const [reconcileAmount, setReconcileAmount] = useState('');
   if (!isShowing) {
     return null;
   }
-  let handleReconcileAmount = e => {
-    console.log(e);
-    console.log('Curr' + reconcileAmount);
-    setReconcileAmount(e.target.value);
+
+  // Note: Workaround for ynab having an event listener that overrides the backspace.
+  // We'll add our own event listener to remove it.
+  let handleKeyDown = event => {
+    if (event.keyCode === 8 && reconcileAmount && reconcileAmount.length >= 1) {
+      setReconcileAmount(reconcileAmount.substring(0, reconcileAmount.length - 1));
+    }
   };
+
+  let handleAutoReconcileSubmit = () => {
+    // Validate that its a valid amount
+    // If any errors, set error on input
+    // Do the calculation:
+    // - Inputs: CurrentAmount, AccountId
+    // __toolkitUtils.getEntityManager().getAccountById('7bbf8e31-6746-46b6-81de-144735bf4c5c').getAccountCalculation()
+    //   - clearedBalance
+    //   - unclearedBalance
+  };
+
   return (
     <div className="tk-modal-container">
       <div className="tk-modal-content tk-auto-reconcile-modal">
-        <h2>Auto Reconcile</h2>
-        <p>Enter your ammount</p>
+        <h2 className="auto-reconcile-header">Auto Reconcile</h2>
+        <p className="auto-reconcile-prompt">
+          What is your <strong>current</strong> account balance?
+        </p>
         <input
           type="text"
           className="auto-reconcile-input"
           value={reconcileAmount}
           onChange={e => setReconcileAmount(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
 
         {/* // Footer */}
         <div className="tk-modal-footer-action">
           <button
             className="auto-reconcile-action button button-primary tk-mg-r-1"
-            onClick={() => {
-              console.log('Foo');
-            }}
+            onClick={handleAutoReconcileSubmit}
           >
             {' '}
             AutoReconcile{' '}
