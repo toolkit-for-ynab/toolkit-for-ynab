f825c3b2b7fc9151ba756cc8e803285c15c5cfe8
Tony Vo <tonyt.vo22@gmail.com>
2020-10-04 20:05:14 -0700

    default false

--- src/extension/features/accounts/auto-reconcile/settings.js
+++ src/extension/features/accounts/auto-reconcile/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'AutoReconcile',
   type: 'checkbox',
-  default: true,
+  default: false,
   section: 'accounts',
   title: 'Auto Reconcile',
   description: 'Try to auto reconcile by finding which transactions add up to a current ammount',
