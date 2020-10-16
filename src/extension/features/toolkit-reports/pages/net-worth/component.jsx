8e706227109fdb143073e8eaa4ff9b768fb09d81
Tony Vo <tonyt.vo22@gmail.com>
2020-10-16 14:17:24 -0700

    fix coloring for net-worth and inflow/outflow

--- src/extension/features/toolkit-reports/pages/net-worth/component.jsx
+++ src/extension/features/toolkit-reports/pages/net-worth/component.jsx
@@ -74,13 +74,19 @@ export class NetWorthComponent extends React.Component {
       legend: { enabled: false },
       title: { text: '' },
       tooltip: { enabled: false },
-      xAxis: { categories: labels },
+      xAxis: {
+        categories: labels,
+        labels: {
+          style: { color: 'var(--label_primary)' },
+        },
+      },
       yAxis: {
         title: { text: '' },
         labels: {
           formatter: function() {
             return formatCurrency(this.value);
           },
+          style: { color: 'var(--label_primary)' },
         },
       },
       series: [
