ebc3fab29299db4aad676d99b61ea9e610665afb
Tony Vo <tonyt.vo22@gmail.com>
2020-10-18 14:10:43 -0700

    feat: show reconciled balance in account header (#2165)

---
+++ src/extension/features/accounts/reconcile-balance/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ReconcileBalance',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Show Reconciled Balance',
+  description:
+    'Show the current reconciled balance excluding cleared and non-reconciled transactions',
+};
