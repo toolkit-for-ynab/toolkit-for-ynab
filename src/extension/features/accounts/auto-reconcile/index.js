546d5448eaeb273b07bc91b3c5410afb8d9cea7c
Tony Vo <tonyt.vo22@gmail.com>
2020-10-24 13:47:11 -0700

    clean up to index js, use jquery only

--- src/extension/features/accounts/auto-reconcile/index.js
+++ src/extension/features/accounts/auto-reconcile/index.js
@@ -4,11 +4,12 @@ import { AutoReconcileContainer } from './components/AutoReconcileContainer';
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const YNAB_RECONCILE_SELECTOR = '.accounts-header-reconcile';
+const YNAB_RECONCILE_BUTTON = '.accounts-header-reconcile';
 const YNAB_APPLICATION_BODY = '.ember-application';
 const AUTO_RECONCILE_CONTAINER_ID = 'tk-auto-reconcile-container';
-export const YNAB_RECONCILE_INPUT_MODAL = '.accounts-adjustment.account-flash-notification';
+export const YNAB_ADJUSTMENT_SELECTOR = '.accounts-adjustment.account-flash-notification';
 export const AUTO_RECONCILE_MODAL_PORTAL = 'tk-auto-reconcile-portal';
+export const YNAB_RECONCILE_INPUT_MODAL = '.modal-account-reconcile-enter-balance';
 
 export class AutoReconcile extends Feature {
   constructor() {
@@ -17,22 +18,7 @@ export class AutoReconcile extends Feature {
   }
 
   shouldInvoke() {
-    // Only if the ynab reconcile button is present, we should invoke.
-    return $(YNAB_RECONCILE_SELECTOR).length > 0 && isCurrentRouteAccountsPage();
-  }
-
-  /**
-   * Attach an input listener to the reconcile input field
-   * Set our reconcile value on input change to be used
-   * @returns {void}
-   */
-  _attachInputListener() {
-    $('.modal-account-reconcile-enter-balance')
-      .find('input')
-      .on('input', e => {
-        this._reconcileInputValue = e.target.value;
-        console.log('Value: ', this._reconcileInputValue);
-      });
+    return $(YNAB_RECONCILE_BUTTON).length > 0 && isCurrentRouteAccountsPage();
   }
 
   observe(changedNodes) {
@@ -43,38 +29,14 @@ export class AutoReconcile extends Feature {
     }
   }
 
-  _createModalPortal() {
-    let portal = document.getElementById(AUTO_RECONCILE_MODAL_PORTAL);
-    if (!portal) {
-      // Create the container for our React Modal
-      let portalDiv = document.createElement('div');
-      portalDiv.setAttribute('id', AUTO_RECONCILE_MODAL_PORTAL);
-
-      // Append it as a child of the parent
-      document.querySelector(YNAB_APPLICATION_BODY).append(portalDiv);
-    }
-  }
-
-  _createAutoReconcileContainer() {
-    let container = document.getElementById(AUTO_RECONCILE_CONTAINER_ID);
-    if (!container) {
-      // Create the container for our React Button
-      let autoReconcileContainer = document.createElement('span');
-      autoReconcileContainer.setAttribute('class', 'tk-mg-r-1 tk-auto');
-      autoReconcileContainer.setAttribute('id', AUTO_RECONCILE_CONTAINER_ID);
-
-      // Append the button next to the ok button
-      let parent = document.querySelector(YNAB_RECONCILE_INPUT_MODAL);
-      if (parent) {
-        parent.appendChild(autoReconcileContainer);
-      }
-    }
-  }
-
   invoke() {
+    if (!this.shouldInvoke()) {
+      return;
+    }
+
     setTimeout(() => {
-      // Create the elements for the reconcile button and modal portal
-      this._createAutoReconcileContainer();
+      // Create the elements for the clear assistance button and modal portal
+      this._createFeatureContainer();
       this._createModalPortal();
 
       // Render the react component as part of the container
@@ -86,5 +48,48 @@ export class AutoReconcile extends Feature {
         );
       }
     }, 50);
+  }
+
+  /**
+   * Attach an input listener to the reconcile input field
+   * Set our reconcile value on input change to be used
+   * @returns {void}
+   */
+  _attachInputListener() {
+    let inputElement = $(YNAB_RECONCILE_INPUT_MODAL).find('input');
+    if (inputElement.length) {
+      inputElement.on('input', e => {
+        this._reconcileInputValue = e.target.value;
+      });
+    }
+  }
+
+  /**
+   * Create the react modal portal in the DOM if its not already present
+   */
+  _createModalPortal() {
+    let portal = $(`#${AUTO_RECONCILE_MODAL_PORTAL}`);
+    if (!portal.length) {
+      // Append it as a child of the ynab application
+      let ynabApp = $(YNAB_APPLICATION_BODY);
+      if (ynabApp.length) {
+        ynabApp.append(`<div id='${AUTO_RECONCILE_MODAL_PORTAL}'></div>`);
+      }
+    }
+  }
+
+  /**
+   * Create the feature container in the DOM if its not already present
+   */
+  _createFeatureContainer() {
+    let container = $(`#${AUTO_RECONCILE_CONTAINER_ID}`);
+    if (!container.length) {
+      // Append the container next to the create adjustment button
+      let parent = $(YNAB_ADJUSTMENT_SELECTOR);
+
+      if (parent) {
+        parent.append(`<span class='tk-mg-r-1' id='${AUTO_RECONCILE_CONTAINER_ID}'></span>`);
+      }
+    }
   }
 }
