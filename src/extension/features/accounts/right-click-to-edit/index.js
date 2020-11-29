a029c091cfb1ba993db94d6fae49cf68a0071c74
Toeler <dkmenice@gmail.com>
2020-11-30 09:56:22 +1300

    issue-2156: fix right click menu clipping out of window (#2212)
    Added some calculations of the menu position and make a few attempts at moving it so it stays within the window. Most notably by placing to the left or right of the cursor if there is not enough room above or below the selected row for the menu to fully show

--- src/extension/features/accounts/right-click-to-edit/index.js
+++ src/extension/features/accounts/right-click-to-edit/index.js
@@ -1,6 +1,11 @@
 import { Feature } from 'toolkit/extension/features/feature';
 import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
 
+const SCREEN_PADDING = 8;
+const ROW_ARROW_DOWN_START = 34 * 0.75;
+const ROW_ARROW_UP_START = 34 * 0.25;
+const ARROW_HORIZONTAL_OFFSET = 8;
+
 export class RightClickToEdit extends Feature {
   isCurrentlyRunning = false;
 
@@ -30,29 +35,85 @@ export class RightClickToEdit extends Feature {
     $('.accounts-toolbar-edit-transaction').click();
 
     // determine if modal needs to be positioned above or below clicked element
-    let height = $('.modal-account-edit-transaction-list .modal').outerHeight();
-    let below = event.pageY + height > $(window).height() ? false : true; // eslint-disable-line no-unneeded-ternary
+    const $modal =  $('.modal-account-edit-transaction-list .modal');
+    const modalHeight = $modal.outerHeight();
+    const modalWidth = $modal.outerWidth();
+    const halfModalWidth = modalWidth / 2;
+    const $modalArrow =  $modal.find('.modal-arrow');
+    const modalArrowHeight = $modalArrow.outerHeight();
+    const modalArrowWidth = $modalArrow.outerWidth();
+    const halfModalArrowWidth = modalArrowWidth / 2;
+    const $window = $(window);
+    const windowHeight = $window.height();
+    const windowWidth = $window.width();
+    const modalClipsTop = event.pageY - modalHeight < SCREEN_PADDING;
+    const modalClipsBottom = event.pageY + modalHeight > windowHeight - SCREEN_PADDING;
+    const modalClipsLeft = event.pageX - modalWidth < SCREEN_PADDING;
+    const modalClipsRight = event.pageX + modalWidth > windowWidth - SCREEN_PADDING;
 
     // sometimes, offset is undefined -- if this is the case, just show the normal un-positioned edit menu
-    let offset = $element.offset(); // move context menu
+    const offset = $element.offset(); // move context menu
     if (!offset) {
       return;
     }
 
     Ember.run.next(() => {
-      if (below) {
-        // position below
-        $('.modal-account-edit-transaction-list .modal')
-          .addClass('modal-below')
-          .css('left', event.pageX - 115)
-          .css('top', offset.top + 41);
+      let top;
+      let left = event.pageX - halfModalWidth;
+      let arrowLeft = halfModalWidth - halfModalArrowWidth;
+      let arrowBottom = '100%';
+      let cls
+      if (!modalClipsBottom) {
+        cls = 'modal-below';
+        top = offset.top + modalArrowHeight + ROW_ARROW_DOWN_START;
+      } else if (!modalClipsTop) {
+        cls = 'modal-above';
+        top = offset.top - modalHeight - ROW_ARROW_UP_START;
+        arrowBottom = 'auto';
       } else {
-        // position above
-        $('.modal-account-edit-transaction-list .modal')
-          .addClass('modal-above')
-          .css('left', event.pageX - 115)
-          .css('top', offset.top - height - 8);
+        // Clips both top and bottom, need to position to side
+        top = windowHeight - modalHeight - SCREEN_PADDING;
+        arrowBottom = modalHeight - event.pageY + top - halfModalArrowWidth;
+        if (top > event.pageY - halfModalArrowWidth) {
+          top = `calc(${event.pageY}px - ${$modal.css('border-top-left-radius')} - ${halfModalArrowWidth}px`;
+        }
+        if (!modalClipsRight) {
+          cls = 'modal-right';
+          left = event.pageX + modalArrowHeight + ARROW_HORIZONTAL_OFFSET;
+          arrowLeft = -modalArrowWidth;
+        } else if (!modalClipsLeft) {
+          cls = 'modal-left';
+          left = event.pageX - modalWidth - modalArrowHeight - ARROW_HORIZONTAL_OFFSET;
+          arrowLeft = modalWidth;
+        } else {
+          // Can't display the modal at all without clipping
+          top = event.pageY + halfModalArrowWidth;
+          arrowLeft = halfModalWidth - halfModalArrowWidth;
+        }
       }
+
+      // If the modal clips outside the screen left or right then stick it to the edge
+      if (left + modalWidth > windowWidth - SCREEN_PADDING) {
+        left = windowWidth - modalWidth - SCREEN_PADDING;
+        arrowLeft = event.pageX - left - halfModalArrowWidth;
+      } else if (left < SCREEN_PADDING) {
+        left = SCREEN_PADDING;
+        arrowLeft = event.pageX - SCREEN_PADDING - halfModalArrowWidth;
+      }
+
+      // Re-position any sub lists that would open to the right of the right edge
+      const modalRight = left + modalWidth;
+      $modal.find('.modal-list>li>ul')
+        .filter((_, subList) => modalRight + $(subList).outerWidth() > windowWidth)
+        .addClass('modal-sub-left');
+
+      $modal.addClass(cls)
+        .css('left', left)
+        .css('top', top);
+
+      $modalArrow
+        .css('left', arrowLeft)
+        .css('bottom', arrowBottom);
     });
 
     return false;
