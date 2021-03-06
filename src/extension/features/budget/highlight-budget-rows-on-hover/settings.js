7ccaa1fba50dd8ffb65cdaf89528f555cc6b9ccb
Alex Corn <alex@alexcorn.com>
2021-03-06 02:56:34 -0500

    feat: add "Highlight Budget Rows On Hover" feature

---
+++ src/extension/features/budget/highlight-budget-rows-on-hover/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'HoveredBudgetRows',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Highlight Budget Rows On Hover',
+  description: 'Shows a light gray background on category rows when hovered over.',
+};
