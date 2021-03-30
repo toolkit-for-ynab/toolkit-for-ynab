48b23fccbf0227fff3c85fad9a96d08ab46a9757
Yusef Ouda <yiouda92@gmail.com>
2021-03-29 19:01:52 -0500

    Adds new setting for a confetti animation on reconciliation

---
+++ src/extension/features/accounts/reconcile-confetti/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ReconcileConfetti',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Show Reconcile Confetti',
+  description:
+    'When an account is sucessfully reconciled, show a fun confetti animation to mark your tremendous achievement.',
+};
