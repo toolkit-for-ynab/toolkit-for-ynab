ee72efcc3bab99bcaaceb324bef61fb9345af40d
George H. Peden <george@peden.ws>
2021-04-04 13:33:56 -0700

    add days since reconciled

---
+++ src/extension/features/accounts/days-since-reconciled/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'DaysSinceReconciled',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Show Days Since Reconciled',
+  description: 'Show the number of days since account was last reconciled in the header',
+};
