28984a897a315e14a05969c300e84bacfeb86fe6
George H. Peden <george@peden.ws>
2021-04-07 10:06:44 -0700

    change default to 'disabled' rather than numeric 0
    use string for default

--- src/extension/features/accounts/last-reconciled-date/settings.js
+++ src/extension/features/accounts/last-reconciled-date/settings.js
@@ -1,13 +1,13 @@
 module.exports = {
   name: 'LastReconciledDate',
   type: 'select',
-  default: '0',
+  default: 'disabled',
   section: 'accounts',
   title: 'Show Last Reconciliation Details',
   description:
     'Show the last reconciled date and or days since last reconciled of the current account in the header',
   options: [
-    { name: 'Disabled', value: '0' },
+    { name: 'Disabled', value: 'disabled' },
     { name: 'Date Last Reconciled', value: 'last-date' },
     { name: 'Days Since Reconciled', value: 'days-since' },
     { name: 'Both', value: 'last-date-days-since' },
