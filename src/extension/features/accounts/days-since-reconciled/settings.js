0c4329cfa17f44871ac0526fcc9f314f0570c7bf
George H. Peden <george@peden.ws>
2021-04-06 21:41:32 -0700

    remove previous date since files

--- src/extension/features/accounts/days-since-reconciled/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'DaysSinceReconciled',
-  type: 'checkbox',
-  default: false,
-  section: 'accounts',
-  title: 'Show Days Since Reconciled',
-  description: 'Show the number of days since account was last reconciled in the header',
-};
