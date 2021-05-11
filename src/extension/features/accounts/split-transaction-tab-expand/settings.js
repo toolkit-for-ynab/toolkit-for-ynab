62ff5135aa33af4875d0127034542f11df77474d
Sam Pickering <sam.p@tuta.io>
2021-05-11 01:17:27 +0100

    feat: expand split transaction when tabbing past last row

---
+++ src/extension/features/accounts/split-transaction-tab-expand/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'SplitTransactionTabExpand',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Tab Expands Split Transactions',
+  description:
+    'When entering a split transaction, add a new split automatically when tabbing past the last split (a la YNAB4).',
+};
