ee31c95e42c8d439ef90f6aa37b40c5be49fc01e
Sam Pickering <sam.p@tuta.io>
2021-06-17 23:53:34 +0100

    fix: add support for split transactions to ChangeEnterBehavior (#2352)

--- src/extension/features/accounts/change-enter-behavior/index.js
+++ src/extension/features/accounts/change-enter-behavior/index.js
@@ -3,29 +3,32 @@ import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
 
 export class ChangeEnterBehavior extends Feature {
   shouldInvoke() {
-    return isCurrentRouteAccountsPage() && $('.ynab-grid-body-row.is-adding').length;
+    return isCurrentRouteAccountsPage() && $('.ynab-grid-add-rows').length;
   }
 
   invoke() {
-    const $addRow = $('.ynab-grid-body-row.is-adding');
-    const $memoInput = $('.ynab-grid-cell-memo input', $addRow);
-    const $outflowInput = $('.ynab-grid-cell-outflow input', $addRow);
-    const $inflowInput = $('.ynab-grid-cell-inflow input', $addRow);
+    const $addRows = $('.ynab-grid-add-rows');
+    const $editRows = $('.ynab-grid-body-row.is-editing', $addRows);
+    $editRows.each((index, $addRow) => {
+      const $memoInput = $('.ynab-grid-cell-memo input', $addRow);
+      const $outflowInput = $('.ynab-grid-cell-outflow input', $addRow);
+      const $inflowInput = $('.ynab-grid-cell-inflow input', $addRow);
 
-    if (!$memoInput[0].getAttribute('data-toolkit-save-behavior')) {
-      $memoInput[0].setAttribute('data-toolkit-save-behavior', true);
-      $memoInput.keydown(this.applyNewEnterBehavior);
-    }
+      if (!$memoInput[0].getAttribute('data-toolkit-save-behavior')) {
+        $memoInput[0].setAttribute('data-toolkit-save-behavior', true);
+        $memoInput.keydown(this.applyNewEnterBehavior);
+      }
 
-    if (!$outflowInput[0].getAttribute('data-toolkit-save-behavior')) {
-      $outflowInput[0].setAttribute('data-toolkit-save-behavior', true);
-      $outflowInput.keydown(this.applyNewEnterBehavior);
-    }
+      if (!$outflowInput[0].getAttribute('data-toolkit-save-behavior')) {
+        $outflowInput[0].setAttribute('data-toolkit-save-behavior', true);
+        $outflowInput.keydown(this.applyNewEnterBehavior);
+      }
 
-    if (!$inflowInput[0].getAttribute('data-toolkit-save-behavior')) {
-      $inflowInput[0].setAttribute('data-toolkit-save-behavior', true);
-      $inflowInput.keydown(this.applyNewEnterBehavior);
-    }
+      if (!$inflowInput[0].getAttribute('data-toolkit-save-behavior')) {
+        $inflowInput[0].setAttribute('data-toolkit-save-behavior', true);
+        $inflowInput.keydown(this.applyNewEnterBehavior);
+      }
+    });
   }
 
   applyNewEnterBehavior(event) {
