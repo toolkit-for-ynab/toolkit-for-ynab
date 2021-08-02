cc90955a055d843bad91c08e1ee66d8e0b8d952f
Sam Pickering <83871143+samhpickering@users.noreply.github.com>
2021-08-02 17:58:34 +0100

    refactor: update color blind mode to allow fully customizable colors (#2412)

---
+++ src/extension/features/general/customize-colour-scheme/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'CustomizeColourScheme',
+  type: 'checkbox',
+  default: true,
+  section: 'general',
+  title: 'Customize Colour Scheme',
+  description:
+    "Adds an option in YNAB's Display Menu to customize the default green/orange/red colour scheme to whatever you wish.",
+};
