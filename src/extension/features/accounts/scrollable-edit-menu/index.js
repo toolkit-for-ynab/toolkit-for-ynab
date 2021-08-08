5e0b48d697965c862fdebc431f3a9523bded95db
Sam Pickering <sam.p@tuta.io>
2021-08-09 00:36:04 +0100

    override default modal positioning

--- src/extension/features/accounts/scrollable-edit-menu/index.js
+++ src/extension/features/accounts/scrollable-edit-menu/index.js
@@ -2,6 +2,36 @@ import { Feature } from 'toolkit/extension/features/feature';
 import { addToolkitEmberHook } from 'toolkit/extension/utils/toolkit';
 import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
 
+function calcPosition(target, parent) {
+  if (parent === 0) parent = null;
+  const offsetParent = parent == null ? null : parent.offsetParent();
+  const pos = target.position();
+  const offset = target.offset();
+  const i =
+    (offsetParent == null ? null : offsetParent.css('position')) === 'absolute' ? pos : offset;
+  const el = target.get(0);
+  let left = i.left;
+  let top = i.top;
+  let outerWidth = target.outerWidth();
+  let outerHeight = target.outerHeight();
+  if (!el.getClientRects().length) {
+    let boundingRect = el.getBoundingClientRect();
+    let defaultView = el.ownerDocument.defaultView;
+    left = boundingRect.left + defaultView.pageXOffset;
+    top = boundingRect.top + defaultView.pageYOffset;
+    outerWidth = boundingRect.width;
+    outerHeight = boundingRect.height;
+  }
+  return {
+    x: left,
+    y: top,
+    width: outerWidth,
+    height: outerHeight,
+    offsetX: offset.left,
+    offsetY: offset.top,
+  };
+}
+
 export class ScrollableEditMenu extends Feature {
   injectCSS() {
     return require('./index.css');
@@ -22,7 +52,63 @@ export class ScrollableEditMenu extends Feature {
 
   destroy() {}
 
+  overrideMSIO() {
+    const modal = $('.modal', this.element);
+    const scrollable = $('.js-ynab-modal-scrollable-area', this.element);
+    if (modal && scrollable) {
+      const arrowDir = this.get('arrow');
+      const sizePos = calcPosition(modal, this.immediateParent());
+      const modalY = this.get('modalY');
+      const modalBottom = sizePos.y + scrollable.prop('scrollHeight');
+      const windowHeight = $(window).height();
+      if (arrowDir === 'up' && modalBottom > windowHeight) {
+        const height = windowHeight - sizePos.y;
+        scrollable.css({
+          overflow: 'auto',
+          height: '100%',
+        });
+        modal.css({
+          height: height,
+        });
+      } else if (arrowDir === 'down' && modalY < 0) {
+        const height = sizePos.height - Math.abs(modalY) - 10;
+        scrollable.css({
+          overflow: 'auto',
+          height: '100%',
+        });
+        modal.css({
+          top: 10,
+          height: height,
+        });
+      } else if (arrowDir === 'down' && modalBottom > this.triggerElement.position().top) {
+        const height = this.triggerElement.position().top - 24; // todo: replace constant
+        scrollable.css({
+          overflow: 'auto',
+          height: '100%',
+        });
+        modal.css({
+          top: 10,
+          height: height,
+        });
+      } else {
+        scrollable.css({
+          overflow: 'visible',
+        });
+        modal.css({
+          height: 'auto',
+        });
+      }
+    }
+  }
+
   addScrollWrappers(modalContainer) {
+    const view = Ember.ViewUtils.getElementView(
+      document.querySelector('.modal-account-edit-transaction-list')
+    );
+    view.isScrolledIfOverflowedOutOfWindow = true;
+    view.makeScrollableIfOverflowed = this.overrideMSIO;
+    $('.modal-list', modalContainer).addClass('js-ynab-modal-scrollable-area');
+
     const modal = $('.modal', modalContainer);
 
     const subMenus = $('ul.modal-list > li > ul:not(.scrollable-edit-wrapper)', modal);
@@ -44,9 +130,5 @@ export class ScrollableEditMenu extends Feature {
         wrapper.css('top', top);
       });
     });
-
-    if (modal.offset().top + modal.height() > $(window).height()) {
-      modal.height(Math.max($(window).height() - modal.offset().top, 100));
-    }
   }
 }
