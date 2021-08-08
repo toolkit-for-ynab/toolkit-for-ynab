2c31a0e4482a5c8abb931a69086bff2735bff710
Adam Langbert <adamhl@pm.me>
2021-08-09 00:06:48 +0200

    rename feature to Show Available After Savings

--- src/extension/features/budget/show-available-after-savings/settings.js
+++ src/extension/features/budget/show-available-after-savings/settings.js
@@ -1,11 +1,11 @@
 module.exports = {
-  name: 'SubtractSavingsFromTotalAvailable',
+  name: 'ShowAvailableAfterSavings',
   type: 'checkbox',
   default: false,
   section: 'budget',
-  title: 'Subtract Savings from Total Available',
+  title: 'Show Available After Savings',
   description:
-    'Adds "Available After Savings" to the budget breakdown. This allows you to see how much you have available if you exclude your savings.' +
+    'Shows "Available After Savings" in the budget breakdown. This allows you to see how much you have available if you exclude your savings.' +
     ' Any categories under a category group that includes "Savings" in its name will be taken into account.' +
     ' You can also add "Savings" anywhere in the name of a category to mark it.',
 };
--- src/extension/features/budget/subtract-upcoming-from-available/settings.js
+++ src/extension/features/budget/subtract-upcoming-from-available/settings.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Subtract Upcoming Transactions from Available Balance',
   description:
     'Subtracts upcoming transactions from the available balance for each category.' +
-    ' In other words, treat upcoming transactions as if the money has already been spent. Also adds "Available After Upcoming Transactions" to the budget breakdown.' +
+    ' In other words, treat upcoming transactions as if the money has already been spent. Also shows "Available After Upcoming Transactions" in the budget breakdown.' +
     '\n\nEnabling "Subtract Credit Card Balances from Total Available" totals the amounts in the "Payment" column' +
     ' of your CC category group and subtracts that from the "Available After Upcoming Transactions" in the budget breakdown.' +
     ' This allows you to see how much you have available if you exclude the money "reserved" in your Credit Card Payments category group.' +
