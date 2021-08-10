9894e5274ac32b0454b90af279c7a3ece2a94c3f
Josh Madewell <joshmadewell@users.noreply.github.com>
2021-08-10 17:22:26 -0500

    refactor: remove set multiple flags feature (#2484)

--- src/extension/features/accounts/set-multiple-flags/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'SetMultipleFlags',
-  type: 'checkbox',
-  default: false,
-  section: 'accounts',
-  title: 'Edit Multiple Flags at Once',
-  description:
-    'Adds a button to the edit dialog which allows you to set the flag. If multiple transactions are selected, all transactions are updated.',
-};
