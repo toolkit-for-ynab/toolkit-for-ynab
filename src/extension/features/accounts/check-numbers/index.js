b98a477afd4780ad18ee256b6bb30ba042bae269
Josh Madewell <josh.madewell@gmail.com>
2021-08-10 12:38:38 -0500

    add destroy method to check-numbers

--- src/extension/features/accounts/check-numbers/index.js
+++ src/extension/features/accounts/check-numbers/index.js
@@ -34,26 +34,29 @@ export class CheckNumbers extends Feature {
     });
   }
 
+  destroy() {
+    $('.ynab-grid-cell-tk-check-number').remove();
+  }
+
   insertHeader() {
-    if ($('.ynab-grid-header .ynab-grid-cell-toolkit-check-number').length) return;
+    if ($('.ynab-grid-header .ynab-grid-cell-tk-check-number').length) return;
 
     const $headerRow = $('.ynab-grid-header');
     const checkNumberHeader = $('.ynab-grid-cell-inflow', $headerRow).clone();
     checkNumberHeader.removeClass('ynab-grid-cell-inflow');
-    checkNumberHeader.addClass('ynab-grid-cell-toolkit-check-number');
+    checkNumberHeader.addClass('ynab-grid-cell-tk-check-number');
     checkNumberHeader.text('CHECK NUMBER').css('font-weight', 'normal');
     checkNumberHeader.insertAfter($('.ynab-grid-cell-memo', $headerRow));
-    checkNumberHeader.click((event) => {
+    checkNumberHeader.on('click', (event) => {
       event.preventDefault();
       event.stopPropagation();
     });
 
-    if ($('.ynab-grid-body .ynab-grid-body-row-top .ynab-grid-cell-toolkit-check-number').length)
-      return;
+    if ($('.ynab-grid-body .ynab-grid-body-row-top .ynab-grid-cell-tk-check-number').length) return;
     const $topRow = $('.ynab-grid-body-row-top');
     const topRowCheckNumber = $('.ynab-grid-cell-inflow', $topRow).clone();
     topRowCheckNumber.removeClass('ynab-grid-cell-inflow');
-    topRowCheckNumber.addClass('ynab-grid-cell-toolkit-check-number');
+    topRowCheckNumber.addClass('ynab-grid-cell-tk-check-number');
     topRowCheckNumber.insertAfter($('.ynab-grid-cell-memo', $topRow));
   }
 
@@ -67,13 +70,13 @@ export class CheckNumbers extends Feature {
     const $editingRow = $(element);
     const $inputBox = $('<input placeholder="check number">')
       .addClass('accounts-text-field')
-      .addClass('ynab-grid-cell-toolkit-check-number-input')
-      .blur(function () {
+      .addClass('ynab-grid-cell-tk-check-number-input')
+      .on('blur', function () {
         transaction.set('checkNumber', $(this).val());
       });
 
     $inputBox.val(transaction.checkNumber);
-    $('<div class="ynab-grid-cell ynab-grid-cell-toolkit-check-number"><div>')
+    $('<div class="ynab-grid-cell ynab-grid-cell-tk-check-number"><div>')
       .append($inputBox)
       .insertAfter($('.ynab-grid-cell-memo', $editingRow));
   }
@@ -93,7 +96,7 @@ export class CheckNumbers extends Feature {
     const $currentRow = $(element);
     const checkNumberCell = $('.ynab-grid-cell-memo', $currentRow).clone();
     checkNumberCell.removeClass('ynab-grid-cell-memo');
-    checkNumberCell.addClass('ynab-grid-cell-toolkit-check-number');
+    checkNumberCell.addClass('ynab-grid-cell-tk-check-number');
 
     checkNumberCell.text(transaction.checkNumber || '');
     checkNumberCell.insertAfter($('.ynab-grid-cell-memo', $currentRow));
@@ -102,7 +105,7 @@ export class CheckNumbers extends Feature {
   insertDeadColumn(element) {
     const checkNumberCell = $('.ynab-grid-cell-memo', element).clone();
     checkNumberCell.removeClass('ynab-grid-cell-memo');
-    checkNumberCell.addClass('ynab-grid-cell-toolkit-check-number');
+    checkNumberCell.addClass('ynab-grid-cell-tk-check-number');
     checkNumberCell.insertAfter($('.ynab-grid-cell-memo', element));
     checkNumberCell.empty();
   }
