50d9a9f9a464f92791f3c92ffc867e70438d975e
Josh Madewell <joshmadewell@users.noreply.github.com>
2021-08-11 11:56:11 -0500

    chore: use github actions to publish beta instead of travis (#2485)

--- scripts/applyManifestOverrides.js
+++ scripts/applyManifestOverrides.js
@@ -19,9 +19,10 @@ const changes = require(path.join(workspaceRoot, 'src', `manifest.${env}.json`))
 // Clobber any keys in the beta manifest across.
 Object.assign(manifest, changes);
 
-// If we're on Travis, we should append the build number to the version number.
-if (process.env.TRAVIS_BUILD_NUMBER) {
-  manifest.version += `.${process.env.TRAVIS_BUILD_NUMBER}`;
+// If we're in a github action, append the build number to the version number.
+if (process.env.GITHUB_RUN_ID) {
+  manifest.version += `.${process.env.GITHUB_RUN_ID}`;
+  console.log(`using version: ${manifest.version}`);
 }
 
 // Delete the old one.
