e13a207bd1755c5ae8b68562371569e6f4c0c3bc
Josh Madewell <josh.madewell@gmail.com>
2021-08-17 13:27:59 -0500

    remove budget-spending-goal

--- src/extension/features/budget/budget-spending-goal/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'BudgetSpendingGoal',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Spending Goal',
-  description:
-    'Allows the monthly funding goal to be set as a max spending goal for each category.',
-};
