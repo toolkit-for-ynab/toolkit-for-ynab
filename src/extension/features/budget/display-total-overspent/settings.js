a6845d7ab17fba164b4942afbfc8af7dc6007fb9
Sam Pickering <83871143+samhpickering@users.noreply.github.com>
2021-08-28 19:50:13 +0100

    feature: add total overspending to budget inspector (#2450) (#2482)

---
+++ src/extension/features/budget/display-total-overspent/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'DisplayTotalOverspent',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Show Total Overspent',
+  description:
+    'Adds "Overspending" to the budget inspector, which displays the total overspending from selected categories.',
+};
