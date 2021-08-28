972fd50740218f02add711cf155fb536bad05453
Travis Lesicka <59949694+tlesicka@users.noreply.github.com>
2021-08-28 11:58:17 -0500

    feat: Calculate Internal Rate of Return (#2506)

---
+++ src/extension/features/accounts/calculate-irr/settings.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'CalculateIRR',
+    type: 'select',
+    default: false,
+    section: 'accounts',
+    title: 'Calculate Internal Rate of Return',
+    description:
+      'Calculate Internal Rate of Return for Tracking Accounts. Set Tracking Account contributions to this color to calculate IRR',
+    options: [
+      { name: 'Red', value: 'Red' },
+      { name: 'Orange', value: 'Orange' },
+      { name: 'Yellow (Default Color)', value: 'Yellow' },
+      { name: 'Green', value: 'Green' },
+      { name: 'Blue', value: 'Blue' },
+      { name: 'Purple', value: 'Purple' },
+    ],
+  },
+];
