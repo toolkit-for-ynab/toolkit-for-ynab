fc812e1c7febb6b0210e4f34da8daed9bf5011b7
Don Coleman <coleman.drc@gmail.com>
2021-08-28 10:19:29 -0700

    fix: add income from last month back for new UI (#2491)

--- src/extension/features/budget/income-from-last-month/index.js
+++ src/extension/features/budget/income-from-last-month/index.js
@@ -1,7 +1,7 @@
 import { Feature } from 'toolkit/extension/features/feature';
 import {
   isCurrentRouteBudgetPage,
-  getSelectedMonth,
+  getCurrentBudgetDate,
   getEntityManager,
 } from 'toolkit/extension/utils/ynab';
 import { formatCurrency } from 'toolkit/extension/utils/currency';
@@ -18,80 +18,104 @@ export class IncomeFromLastMonth extends Feature {
 
   invoke() {
     // Do nothing if no header found.
-    if ($('.budget-header-totals-details-values').length === 0) return;
-    const matchMonth = getSelectedMonth().subtractMonths(this.settings.enabled);
-    const previousMonthName = l10nMonth(matchMonth.getMonth(), MonthStyle.Short);
+    if ($('.budget-header-totals').length === 0) return;
 
-    const total = getEntityManager()
-      .getAllTransactions()
-      .reduce((reduced, transaction) => {
-        const isSplit = transaction.getIsSplit();
-        const transactionSubCategory = transaction.get('subCategory');
-        const transactionAmount = transaction.get('amount');
+    // Get current month and year
+    const currentBudgetDate = getCurrentBudgetDate();
+    const currentYear = parseInt(currentBudgetDate.year);
+    const currentMonth = parseInt(currentBudgetDate.month);
 
-        if (
-          !transaction.get('date').equalsByMonth(matchMonth) ||
-          transaction.get('isTombstone') ||
-          !transactionSubCategory
-        ) {
-          return reduced;
-        }
+    // Calculate income month and year based on settings
+    // Adjust year/month when it rolls over
+    const incomeYear = currentMonth - this.settings.enabled > 0 ? currentYear : currentYear - 1;
+    const incomeMonth =
+      currentMonth - this.settings.enabled > 0
+        ? currentMonth - this.settings.enabled
+        : currentMonth - this.settings.enabled + 12;
 
-        if (transactionSubCategory.isImmediateIncomeCategory()) {
-          return reduced + transactionAmount;
-        }
-        if (isSplit) {
-          let subTransactionIncomes = 0;
-          transaction.get('subTransactions').forEach((subTransaction) => {
-            const subTransactionSubCategory = subTransaction.get('subCategory');
-            const subTransactionAmount = subTransaction.get('amount');
+    // Get short name for current and income months
+    const currentMonthName = l10nMonth(currentMonth - 1, MonthStyle.Short);
+    const incomeMonthName = l10nMonth(incomeMonth - 1, MonthStyle.Short);
 
-            if (!subTransactionSubCategory) {
-              return;
-            }
+    // Get all budget calculations from YNAB
+    const allBudgetCalculations = getEntityManager().getAllMonthlyBudgetCalculations();
 
-            if (subTransactionSubCategory.isImmediateIncomeCategory()) {
-              subTransactionIncomes += subTransactionAmount;
-            }
-          });
-
-          return reduced + subTransactionIncomes;
-        }
-
-        return reduced;
-      }, 0);
-
-    if ($('.toolkit-income-from-last-month').length === 0) {
-      $('.budget-header-totals-details-values').prepend(
-        $('<div>', {
-          class: 'budget-header-totals-cell-value toolkit-income-from-last-month user-data',
-        }).append(
-          $('<span>', {
-            class: 'user-data currency positive',
-          })
-        )
+    // Find current month budget calculations
+    const currentBudgetCalculation = allBudgetCalculations.filter((budgetItem) => {
+      const budgetItemDate = budgetItem.monthlyBudgetId.split('/')[1].split('-');
+      return (
+        currentYear === parseInt(budgetItemDate[0]) && currentMonth === parseInt(budgetItemDate[1])
       );
+    })[0];
 
-      $('.budget-header-totals-details-names').prepend(
+    // Find income month budget calculations
+    const incomeBudgetCalculation = allBudgetCalculations.filter((budgetItem) => {
+      const budgetItemDate = budgetItem.monthlyBudgetId.split('/')[1].split('-');
+      return (
+        incomeYear === parseInt(budgetItemDate[0]) && incomeMonth === parseInt(budgetItemDate[1])
+      );
+    })[0];
+
+    // Add the income from last month section and structure, if not already in place
+    if ($('.toolkit-income-from-last-month').length === 0) {
+      $('.budget-header-totals').after(
         $('<div>', {
-          class: 'budget-header-totals-cell-name toolkit-income-from-last-month',
-          css: {
-            'padding-left': '0.3em',
-            'text-align': 'left',
-          },
-        })
+          class: 'budget-header-item budget-header-totals toolkit-income-from-last-month user-data',
+        }).append(
+          $('<div>', {
+            class: 'to-be-budgeted ember-view ynab-breakdown',
+          }).append(
+            $('<div>', {
+              class: 'toolkit-income-from-last-month-income',
+            }),
+            $('<div>', {
+              class: 'toolkit-income-from-last-month-assigned',
+            }),
+            $('<hr>'),
+            $('<div>', {
+              class: 'toolkit-income-from-last-month-variance',
+            })
+          )
+        )
       );
     }
 
-    $('.budget-header-totals-cell-value.toolkit-income-from-last-month span').text(
-      formatCurrency(total)
-    );
+    // Create income line
+    const incomeType = incomeBudgetCalculation.immediateIncome > 0 ? 'positive' : 'negative';
+    const incomeContents = `<div>${l10n(
+      'toolkit.incomeIn',
+      'Income in'
+    )} ${incomeMonthName}</div><div class="user-data budget-breakdown-to-be-budgeted-${incomeType}"><span class="user-data currency ${incomeType}">${formatCurrency(
+      incomeBudgetCalculation.immediateIncome
+    )}</span></div>`;
 
-    $('.budget-header-totals-details-names > .toolkit-income-from-last-month').text(
-      `${l10n('toolkit.incomeIn', 'Income in')} ${previousMonthName}`
-    );
+    // Create assigned line
+    const assignedType = currentBudgetCalculation.budgeted <= 0 ? 'positive' : 'negative';
+    const assignedContents = `<div>${l10n(
+      'toolkit.assignedIn',
+      'Assigned in'
+    )} ${currentMonthName}</div><div class="user-data budget-breakdown-to-be-budgeted-${assignedType}"><span class="user-data currency ${assignedType}">${formatCurrency(
+      currentBudgetCalculation.budgeted
+    )}</span></div>`;
+
+    // Create variance line
+    const varianceAmount =
+      incomeBudgetCalculation.immediateIncome - currentBudgetCalculation.budgeted;
+    const varianceType = varianceAmount > 0 ? 'positive' : 'negative';
+    const varianceContents = `<div>${l10n(
+      'toolkit.varianceIn',
+      'Variance'
+    )}</div><div class="user-data budget-breakdown-to-be-budgeted-${varianceType}"><span class="user-data currency ${varianceType}"> =${formatCurrency(
+      varianceAmount
+    )}</span></div>`;
+
+    // Insert income from last month lines
+    $('.toolkit-income-from-last-month-income').html(incomeContents);
+    $('.toolkit-income-from-last-month-assigned').html(assignedContents);
+    $('.toolkit-income-from-last-month-variance').html(varianceContents);
   }
 
+  // Listen for events that require an update
   observe(changedNodes) {
     if (!this.shouldInvoke()) return;
 
