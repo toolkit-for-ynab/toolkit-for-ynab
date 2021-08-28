27203d5fcc9329f39fa343e9e2f535423031f3d6
Kevin Bennett <kev@drule.org>
2021-08-28 12:12:10 -0600

    feat: add option to filter Credit Cards from DOB calculation (#2264)

--- src/extension/features/budget/days-of-buffering/settings.js
+++ src/extension/features/budget/days-of-buffering/settings.js
@@ -5,8 +5,7 @@ module.exports = [
     default: false,
     section: 'budget',
     title: 'Add Days of Buffering',
-    description:
-      "Add a calculation which shows how long your money would likely last if you never earned another cent based on your average spending from a chosen date range. We know that no month is 'average' but this should give you some idea of how much of a buffer you have. The actual calculation is the sum of all your budget accounts divided by the average daily outflow in the time range.",
+    description: "Add a calculation which shows how long your money would likely last if you never earned another cent based on your average spending from a chosen date range. We know that no month is 'average' but this should give you some idea of how much of a buffer you have. The actual calculation is the sum of all your budget accounts divided by the average daily outflow in the time range. Optionally, you can exclude negative credit card balances for better accuracy when carrying credit card debt.",
     options: [
       { name: 'Look Back Infinitely', value: 'all' },
       { name: 'Look Back 1 Year', value: '12' },
@@ -14,5 +13,5 @@ module.exports = [
       { name: 'Look Back 3 Months', value: '3' },
       { name: 'Look Back 1 Month', value: '1' },
     ],
-  },
+  }
 ];
