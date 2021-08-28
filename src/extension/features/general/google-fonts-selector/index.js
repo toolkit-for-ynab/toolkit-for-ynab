1bc860d5977de148003886a9ba98af6a7eee8ca1
Sam Pickering <83871143+samhpickering@users.noreply.github.com>
2021-08-28 19:49:03 +0100

    fix: interface font feature missing elements, not loading fonts (#2481)

--- src/extension/features/general/google-fonts-selector/index.js
+++ src/extension/features/general/google-fonts-selector/index.js
@@ -1,24 +1,56 @@
 import { Feature } from 'toolkit/extension/features/feature';
 
 export class GoogleFontsSelector extends Feature {
+  createStyleRule(fonts) {
+    return `body,
+      button,
+      html,
+      input,
+      select,
+      textarea {
+        font-family: ${fonts};
+      }`;
+  }
+
+  injectStylesheetLink(url) {
+    $('#tk-google-fonts-selector').remove();
+    $('head').append($('<link>', { href: url, rel: 'stylesheet', id: 'tk-google-fonts-selector' }));
+  }
+
+  destroy() {
+    $('#tk-google-fonts-selector').remove();
+  }
+
   injectCSS() {
     if (this.settings.enabled === '1') {
-      return require('./open-sans.css');
+      this.injectStylesheetLink(
+        'https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700'
+      );
+      return this.createStyleRule("'Open Sans', sans-serif");
     }
     if (this.settings.enabled === '2') {
-      return require('./roboto.css');
+      this.injectStylesheetLink('https://fonts.googleapis.com/css?family=Roboto:400,700,400italic');
+      return this.createStyleRule("'Roboto', sans-serif");
     }
     if (this.settings.enabled === '3') {
-      return require('./roboto-condensed.css');
+      this.injectStylesheetLink(
+        'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,400italic'
+      );
+      return this.createStyleRule("'Roboto Condensed', sans-serif");
     }
     if (this.settings.enabled === '4') {
-      return require('./droid-sans.css');
+      this.injectStylesheetLink('https://fonts.googleapis.com/css?family=Droid+Sans:400,700');
+      return this.createStyleRule("'Droid Sans', sans-serif");
     }
     if (this.settings.enabled === '5') {
-      return require('./inconsolata.css');
+      this.injectStylesheetLink('https://fonts.googleapis.com/css?family=Inconsolata:400,700');
+      return this.createStyleRule("'Inconsolata', monospace");
     }
     if (this.settings.enabled === '6') {
-      return require('./system-ui.css');
+      this.injectStylesheetLink('https://fonts.googleapis.com/css?family=Roboto:400,700,400italic');
+      return this.createStyleRule(
+        "system-ui, -apple-system, 'Segoe UI', 'Roboto', 'Noto Sans', 'Ubuntu', 'Cantarell', sans-serif"
+      );
     }
   }
 }
