95eb7a8e72bf62ebacf152c9b4bea7d167915ff2
Adam Langbert <adamhl@pm.me>
2021-09-01 23:28:02 -0400

    allow custom colors for MasterCategoryRowColor

--- src/extension/features/budget/color-master-category-row/settings.js
+++ src/extension/features/budget/color-master-category-row/settings.js
@@ -1,8 +1,27 @@
-module.exports = {
-  name: 'MasterCategoryRowColor',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Colored Master Category Row',
-  description: 'Adds Color to Master Category Row.',
-};
+module.exports = [
+  {
+    name: 'MasterCategoryRowColor',
+    type: 'checkbox',
+    default: false,
+    section: 'budget',
+    title: 'Colored Master Category Row',
+    description: 'Adds Color to Master Category Row.',
+  },
+  {
+    name: 'MasterCategoryRowColorSelect',
+    type: 'color',
+    default: '#d1d1d6',
+    section: 'budget',
+    title: 'Colored Master Category Row - Default/Classic Theme Color',
+    description:
+      'The color which will be used for the Default and Classic YNAB Themes. The default is #d1d1d6.',
+  },
+  {
+    name: 'MasterCategoryRowDarkColorSelect',
+    type: 'color',
+    default: '#636366',
+    section: 'budget',
+    title: 'Colored Master Category Row - Dark Theme Color',
+    description: 'The color which will be used for the Dark YNAB Theme. The default is #636366.',
+  },
+];
