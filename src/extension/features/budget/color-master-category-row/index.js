c9f8bf570c3152007ef1dc3ee9ba34e3415f122a
adamhl8 <adamhl@pm.me>
2021-09-04 16:06:57 -0400

    feat: allow custom colors for MasterCategoryRowColor (#2542)

--- src/extension/features/budget/color-master-category-row/index.js
+++ src/extension/features/budget/color-master-category-row/index.js
@@ -2,6 +2,12 @@ import { Feature } from 'toolkit/extension/features/feature';
 
 export class MasterCategoryRowColor extends Feature {
   injectCSS() {
-    return require('./index.css');
+    const css = require('./index.css');
+    const defaultThemeColor = ynabToolKit.options.MasterCategoryRowColorSelect;
+    const darkThemeColor = ynabToolKit.options.MasterCategoryRowDarkColorSelect;
+
+    let patchedCSS = css.replace('var(--neutral300)', defaultThemeColor);
+    patchedCSS = patchedCSS.replace('var(--neutral700)', darkThemeColor);
+    return patchedCSS;
   }
 }
