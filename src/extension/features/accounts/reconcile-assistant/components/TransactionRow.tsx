59692c0d396510cdffcff2138ec19fee4889719b
Tony Vo <tonyt.vo22@gmail.com>
2021-10-27 00:02:44 -0700

    change id

--- src/extension/features/accounts/reconcile-assistant/components/TransactionRow.tsx
+++ src/extension/features/accounts/reconcile-assistant/components/TransactionRow.tsx
@@ -6,10 +6,12 @@ interface TransactionRowProps {
 }
 
 export const TransactionRow: React.FC<TransactionRowProps> = ({ transaction }) => {
+  // @ts-ignore
+  let formattedDate = ynab.YNABSharedLib.dateFormatter.formatDate(transaction.date);
   return (
     <div className="ynab-table-row">
       <div className="ynab-table-col">{transaction.account.accountName}</div>
-      <div className="ynab-table-col">{transaction.date.getUTCTime()}</div>
+      <div className="ynab-table-col">{formattedDate}</div>
       <div className="ynab-table-col">
         {transaction.payee && transaction.payee.name ? transaction.payee.name : ''}
       </div>
