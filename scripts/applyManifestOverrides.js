23ec1c7dd301e511a6b05e25616af60a38f9ea7f
Josh Madewell <josh.madewell@gmail.com>
2021-11-03 21:26:51 -0500

    chore: update builds scripts for ios, fix dev build

--- scripts/applyManifestOverrides.js
+++ scripts/applyManifestOverrides.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs').argv;
 
-const validEnvironments = ['beta', 'development'];
-if (!validEnvironments.includes(yargs.env)) {
-  console.log(`Invalid ENVIRONMENT provided. Must be one of: [${validEnvironments.join('|')}]`);
+const validOverrides = ['beta', 'development', 'ios'];
+if (!validOverrides.includes(yargs.type)) {
+  console.log(`Invalid OVERRIDE provided. Must be one of: [${validOverrides.join('|')}]`);
   process.exit(1);
 }
 
-const env = yargs.env;
+const type = yargs.type;
 const workspaceRoot = path.join(__dirname, '..');
 const buildDirectory = path.join(workspaceRoot, 'dist', 'extension');
 const manifestPath = path.join(buildDirectory, 'manifest.json');
 
 const manifest = require(manifestPath);
-const changes = require(path.join(workspaceRoot, 'src', `manifest.${env}.json`));
+const changes = require(path.join(workspaceRoot, 'src', `manifest.${type}.json`));
 
 // Clobber any keys in the beta manifest across.
 Object.assign(manifest, changes);
@@ -31,4 +31,4 @@ fs.unlinkSync(manifestPath);
 // And write our new one.
 fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
 
-console.log(`${env} manifest applied.`);
+console.log(`${type} manifest applied.`);
