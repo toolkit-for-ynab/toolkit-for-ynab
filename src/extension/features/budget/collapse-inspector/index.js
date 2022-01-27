37fc2365a3629052202bb296d475a56493043b63
Sam Pickering <83871143+samhpickering@users.noreply.github.com>
2022-01-27 20:55:13 +0000

    feat: collapse budget inspector (#2673)

---
+++ src/extension/features/budget/collapse-inspector/index.js
@@ -0,0 +1,82 @@
+import { Feature } from 'toolkit/extension/features/feature';
+import { getToolkitStorageKey, setToolkitStorageKey } from 'toolkit/extension/utils/toolkit';
+
+export class CollapseInspector extends Feature {
+  injectCSS() {
+    return require('./index.css');
+  }
+
+  shouldInvoke() {
+    return true;
+  }
+
+  destroy() {
+    $('.tk-collapse-inspector').remove();
+    $('.budget-inspector').removeAttr('tk-collapse-inspector');
+  }
+
+  invoke() {
+    this.addToolkitEmberHook('budget/budget-inspector', 'didRender', this.updateDOM);
+
+    this.setInspectorCollapsed(getToolkitStorageKey('collapse-inspector', false));
+  }
+
+  collapseButton() {
+    return $(`
+      <button class="sidebar-collapse">
+        <svg height="32" width="32" class="ynab-new-icon ember-view">
+          <use href="#icon_sprite_sidebar_expand"></use>
+          <title>Collapse Inspector</title>    
+        </svg>
+      </button>
+    `);
+  }
+
+  expandButton() {
+    return $(`
+      <button class="sidebar-expand">
+        <svg height="32" width="32" class="ynab-new-icon ember-view">
+          <use href="#icon_sprite_sidebar_collapse"></use>
+          <title>Expand Inspector</title>    
+        </svg>
+      </button>
+    `);
+  }
+
+  updateDOM() {
+    if (!$('.budget-inspector').length) return;
+
+    if (this.isInspectorCollapsed) {
+      $('.budget-inspector').attr('tk-collapse-inspector', '');
+    } else {
+      $('.budget-inspector').removeAttr('tk-collapse-inspector');
+    }
+
+    let buttonContainer = $('.tk-collapse-inspector');
+    if (buttonContainer.length === 0) {
+      buttonContainer = $(`<div class="tk-collapse-inspector"></div>`);
+      $('.budget-inspector-content').append(buttonContainer);
+    }
+
+    buttonContainer.empty();
+
+    const button = this.isInspectorCollapsed ? this.expandButton() : this.collapseButton();
+    buttonContainer.append(button);
+
+    const self = this;
+    button.on('click', function () {
+      self.toggleInspectorCollapsed();
+    });
+  }
+
+  setInspectorCollapsed(collapsed) {
+    this.isInspectorCollapsed = collapsed;
+    setToolkitStorageKey('collapse-inspector', collapsed);
+
+    this.updateDOM();
+  }
+
+  toggleInspectorCollapsed() {
+    this.setInspectorCollapsed(!this.isInspectorCollapsed);
+  }
+}
---
+++ src/extension/features/budget/collapse-inspector/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'CollapseInspector',
+  type: 'checkbox',
+  default: false,
+  section: 'budget',
+  title: 'Collapse Budget Inspector',
+  description: 'Add a collapse button to the bottom of the budget inspector.',
+};
