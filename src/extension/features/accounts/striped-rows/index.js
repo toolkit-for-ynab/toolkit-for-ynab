b2c5be015142b944bb03528d2a7f8dff9027155e
samhpickering <83871143d+samhpickering@users.noreply.github.com>
2022-05-01 23:16:52 +0100

    rewrite to apply styles in batches

--- src/extension/features/accounts/striped-rows/index.js
+++ src/extension/features/accounts/striped-rows/index.js
@@ -1,6 +1,13 @@
 import { Feature } from 'toolkit/extension/features/feature';
 
 const STRIPED_ROW_CLASS = 'tk-striped-transaction-row';
+const EXCLUDED_ROW_CLASSES = [
+  'ynab-grid-collapsible-transactions-toggle',
+  'is-scheduled',
+  'ynab-grid-spacer',
+];
+
+const EXCLUDED_ROW_CLASSES_SELECTOR = `:not(.${EXCLUDED_ROW_CLASSES.join('):not(.')})`;
 
 export class AccountsStripedRows extends Feature {
   injectCSS() {
@@ -12,43 +19,42 @@ export class AccountsStripedRows extends Feature {
   }
 
   invoke() {
-    this.addToolkitEmberHook('register/grid-row', 'didRender', this.addStripes);
-    this.addToolkitEmberHook('register/grid-sub', 'didRender', this.addStripes);
+    this.addToolkitEmberHook('register/grid-row', 'didRender', this.setTimeout);
+    this.addToolkitEmberHook('register/grid-sub', 'didRender', this.setTimeout);
   }
 
   destroy() {
     $(`.${STRIPED_ROW_CLASS}`).removeClass(STRIPED_ROW_CLASS);
   }
 
-  isStriped(row) {
-    let offset = 0;
-    let currentRow = row;
-    while (currentRow.previousElementSibling) {
-      offset++;
-      if (currentRow.previousElementSibling.classList.contains(STRIPED_ROW_CLASS)) {
-        return offset % 2 === 0;
-      }
-      currentRow = currentRow.previousElementSibling;
+  setTimeout() {
+    if (this.timeoutHandle != null) {
+      return;
     }
 
-    offset = 0;
-    currentRow = row;
-    while (currentRow.nextElementSibling) {
-      offset++;
-      if (currentRow.nextElementSibling.classList.contains(STRIPED_ROW_CLASS)) {
-        return offset % 2 === 0;
-      }
-      currentRow = currentRow.nextElementSibling;
-    }
-
-    return true;
+    this.timeoutHandle = setTimeout(this.updateStripes.bind(this), 0);
   }
 
-  addStripes(row) {
-    if (this.isStriped(row)) {
-      row.classList.add(STRIPED_ROW_CLASS);
-    } else {
-      row.classList.remove(STRIPED_ROW_CLASS);
-    }
+  updateStripes() {
+    this.timeoutHandle = null;
+
+    const rows = $(`div.ynab-grid-body-row${EXCLUDED_ROW_CLASSES_SELECTOR}`);
+
+    let stripeEvenRows = true;
+    rows.each(function (index) {
+      if (this.classList.contains(STRIPED_ROW_CLASS)) {
+        stripeEvenRows = index % 2 === 0;
+        return false;
+      }
+    });
+
+    rows.each(function (index) {
+      const isEvenRow = index % 2 === 0;
+      if (isEvenRow !== stripeEvenRows && this.classList.contains(STRIPED_ROW_CLASS)) {
+        this.classList.remove(STRIPED_ROW_CLASS);
+      } else if (isEvenRow === stripeEvenRows && !this.classList.contains(STRIPED_ROW_CLASS)) {
+        this.classList.add(STRIPED_ROW_CLASS);
+      }
+    });
   }
 }
