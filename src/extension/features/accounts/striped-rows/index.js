f963b1693a9849dc5301c1a09c221b79ae927acf
samhpickering <83871143d+samhpickering@users.noreply.github.com>
2022-05-01 22:30:46 +0100

    rewrite striped-rows to remember row status (#1490)

--- src/extension/features/accounts/striped-rows/index.js
+++ src/extension/features/accounts/striped-rows/index.js
@@ -1,19 +1,54 @@
 import { Feature } from 'toolkit/extension/features/feature';
 
+const STRIPED_ROW_CLASS = 'tk-striped-transaction-row';
+
 export class AccountsStripedRows extends Feature {
   injectCSS() {
-    const css = require('./index.css');
-    const defaultThemeColor = ynabToolKit.options.AccountsStripedRowsColor;
-    const darkThemeColor = ynabToolKit.options.AccountsStripedRowsDarkColor;
+    return require('./index.css');
+  }
 
-    let defaultThemePatched = false;
-    const patchedCSS = css.replace(/#[A-Fa-f0-9]{6}/gi, (match) => {
-      if (!defaultThemePatched) {
-        defaultThemePatched = true;
-        return defaultThemeColor || match;
+  shouldInvoke() {
+    return true;
+  }
+
+  invoke() {
+    this.addToolkitEmberHook('register/grid-row', 'didRender', this.addStripes);
+    this.addToolkitEmberHook('register/grid-sub', 'didRender', this.addStripes);
+  }
+
+  destroy() {
+    $(`.${STRIPED_ROW_CLASS}`).removeClass(STRIPED_ROW_CLASS);
+  }
+
+  isStriped(row) {
+    let offset = 0;
+    let currentRow = row;
+    while (currentRow.previousElementSibling) {
+      offset++;
+      if (currentRow.previousElementSibling.classList.contains(STRIPED_ROW_CLASS)) {
+        return offset % 2 === 0;
       }
-      return darkThemeColor || match;
-    });
-    return patchedCSS;
+      currentRow = currentRow.previousElementSibling;
+    }
+
+    offset = 0;
+    currentRow = row;
+    while (currentRow.nextElementSibling) {
+      offset++;
+      if (currentRow.nextElementSibling.classList.contains(STRIPED_ROW_CLASS)) {
+        return offset % 2 === 0;
+      }
+      currentRow = currentRow.nextElementSibling;
+    }
+
+    return true;
+  }
+
+  addStripes(row) {
+    if (this.isStriped(row)) {
+      row.classList.add(STRIPED_ROW_CLASS);
+    } else {
+      row.classList.remove(STRIPED_ROW_CLASS);
+    }
   }
 }
