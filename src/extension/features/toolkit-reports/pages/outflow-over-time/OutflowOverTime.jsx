08db9d2bdf0f3f58770736efea1b82a38849bd45
Simon Johansson <simon@simonjohansson.com>
2022-06-21 18:25:05 +0200

    Allow user to toggle cumulative sum

--- src/extension/features/toolkit-reports/pages/outflow-over-time/OutflowOverTime.jsx
+++ src/extension/features/toolkit-reports/pages/outflow-over-time/OutflowOverTime.jsx
@@ -3,27 +3,52 @@ import PropTypes from 'prop-types';
 import { FiltersPropType } from 'toolkit-reports/common/components/report-context/component';
 import {
   calculateCumulativeOutflowPerDate,
+  calculateOutflowPerDate,
   filterTransactions,
   groupTransactions,
   toHighchartsSeries,
 } from './utils';
 import { OutflowGraph } from './OutflowGraph';
+import { useLocalStorage } from 'toolkit/extension/hooks/useLocalStorage';
+import { LabeledCheckbox } from 'toolkit/extension/features/toolkit-reports/common/components/labeled-checkbox';
 
 export const OutflowOverTimeComponent = ({ allReportableTransactions, filters }) => {
   const [outflowSeries, setOutflowSeries] = useState([]);
+  const [cumulativeSum, setCumulativeSum] = useLocalStorage(
+    'outflow-over-time-useCumulativeSum',
+    true
+  );
 
   useEffect(() => {
     const filterOutAccounts = filters.accountFilterIds;
+    const calculateOutflow = cumulativeSum
+      ? calculateCumulativeOutflowPerDate
+      : calculateOutflowPerDate;
+
     setOutflowSeries(
       toHighchartsSeries(
-        calculateCumulativeOutflowPerDate(
+        calculateOutflow(
           groupTransactions(filterTransactions(allReportableTransactions, filterOutAccounts))
         )
       )
     );
-  }, [allReportableTransactions, filters]);
+  }, [allReportableTransactions, filters, cumulativeSum]);
 
-  return <OutflowGraph series={outflowSeries} />;
+  return (
+    <div className="tk-flex tk-flex-column tk-flex-grow">
+      <div className="tk-flex tk-pd-05 tk-border-b">
+        <div className="tk-pd-x-1">
+          <LabeledCheckbox
+            id="tk-balance-over-time-stepgraph-option"
+            checked={cumulativeSum}
+            label="Cumulative sum"
+            onChange={() => setCumulativeSum(!cumulativeSum)}
+          />
+        </div>
+      </div>
+      <OutflowGraph series={outflowSeries} />
+    </div>
+  );
 };
 
 OutflowOverTimeComponent.propTypes = {
