4f8888b399ef21133bc0175dde6e3854ca09d419
Simon Johansson <simon@simonjohansson.com>
2022-06-21 18:59:11 +0200

    Revert "Allow category filters"
    This reverts commit ab885d52b08cfb3d4ec5b2e4a517b687b46aec3a.

--- src/extension/features/toolkit-reports/common/components/report-context/component.jsx
+++ src/extension/features/toolkit-reports/common/components/report-context/component.jsx
@@ -52,7 +52,7 @@ const REPORT_COMPONENTS = [
     component: OutflowOverTime,
     key: ReportKeys.OutflowOverTime,
     filterSettings: {
-      disableCategoryFilter: false,
+      disableCategoryFilter: true,
       includeTrackingAccounts: true,
     },
   },
