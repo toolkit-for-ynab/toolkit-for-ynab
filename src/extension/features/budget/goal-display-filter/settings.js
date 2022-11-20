685d28503dbeeac680202af4929e32ad8e76591b
michaelcmelton <michaelcmelton93@gmail.com>
2022-11-20 16:43:47 -0500

    feat: Move feature to existing target goal feature

--- src/extension/features/budget/goal-display-filter/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'SumGoalDisplay',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Add Goal Sum To Category Group',
-  description: 'Add a sum to each category group for goal amounts.',
-};
