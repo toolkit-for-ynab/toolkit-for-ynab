8b8f117ccb5fdd728452d8bb657f4d3013912ef2
Daniel Gale-Rosen <5487802+dangrous@users.noreply.github.com>
2023-03-28 21:15:47 -0400

    add new feature (#3047)

---
+++ src/extension/features/general/emphasize-negative-loans/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'EmphasizeNegativeLoans',
+  type: 'checkbox',
+  default: false,
+  section: 'general',
+  title: 'Emphasize Negative Loans',
+  description:
+    'Emphasize loans with negative balances similar to how other negative accounts are emphasized.',
+};
