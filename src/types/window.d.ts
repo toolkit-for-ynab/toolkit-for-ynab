49727fe0312e9d184683114766ae49bd09e4cebf
Josh Madewell <joshmadewell@users.noreply.github.com>
2023-08-01 08:33:04 -0500

    fix: remove YNABFEATURES check (#3188)

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,10 +1,9 @@
-import { YNABApp, YNABGlobal } from './ynab/window';
+import { YNABGlobal } from './ynab/window';
 import { YNABToolkitObject } from './toolkit';
 
 declare global {
   const ynab: YNABGlobal;
   const ynabToolKit: YNABToolkitObject;
-  const YNABFEATURES: any;
   const __toolkitUtils: unknown;
 
   interface Window {
