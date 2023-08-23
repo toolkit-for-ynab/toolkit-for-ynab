b100a590acad911d12627d3c4e2483f3e8304881
Yuval Shavit <yankee.sierra@gmail.com>
2023-08-23 11:25:53 -0400

    highlight mis-budgeted CC accounts in budgets nav (#3219)
    This augments the check-credit-balances feature to also add a warning
    icon to the corresponding account (in the Accounts nav) when it's out of
    sync with its budget.
    
    This slightly tweaks the existing logic, in that it now always sets the
    budget's background to the warning color, instead of only doing so when
    the available amount is non-negative.

--- src/extension/features/budget/check-credit-balances/index.js
+++ src/extension/features/budget/check-credit-balances/index.js
@@ -94,9 +94,39 @@ export class CheckCreditBalances extends Feature {
     if (!category.isCreditCardPaymentCategory) return;
 
     const difference = this.calculateDifference(category);
-    if (difference && category.available >= 0)
+    const categoryAccountId = category.subCategoryAccount?.entityId;
+    const accountBudgetLabel = categoryAccountId
+      ? document.querySelector(`a[data-account-id='${categoryAccountId}']`)
+      : undefined;
+
+    if (difference) {
+      // Upsert the indicator on the accounts nav
+      let mismatchIndicator = accountBudgetLabel?.querySelector('.cc-budget-mismatch');
+      if (mismatchIndicator) {
+        mismatchIndicator.style.display = 'inline';
+      } else if (accountBudgetLabel) {
+        const accountNameElem = accountBudgetLabel.querySelector('.nav-account-name');
+        $(accountNameElem).append(this.alertIcon());
+      }
+      // set the background in the budget nav
       categoryElement.setAttribute('data-tk-pif-assist', 'true');
-    else categoryElement.removeAttribute('data-tk-pif-assist');
+    } else {
+      // hide the indicator in the accounts nav
+      accountBudgetLabel?.querySelectorAll('.cc-budget-mismatch').forEach((elem) => {
+        elem.style.display = 'none';
+      });
+      // unset the background in the budget nav
+      categoryElement.removeAttribute('data-tk-pif-assist');
+    }
+  }
+
+  alertIcon() {
+    return $(`
+      <svg class="ynab-new-icon view-button-icon cc-budget-mismatch">
+        <title>Account balance does not match available funds in budget.</title>
+        <use href="#icon_sprite_general_warning"></use>
+      </svg>
+    `);
   }
 
   calculateDifference(category) {
