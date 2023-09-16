f7e6b1b18566fc0878ec6d04e539e5c253540a35
Trifecta Technology Services Ltd <49250355+gouldjames@users.noreply.github.com>
2023-09-16 15:17:51 +0100

    Moved link in memo to accounts (#3235)

--- src/extension/features/accounts/link-in-memo/settings.js
+++ src/extension/features/accounts/link-in-memo/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'LinkInMemo',
   type: 'checkbox',
   default: false,
-  section: 'budget',
+  section: 'accounts',
   title: 'Hyperlinks in the memo field',
   description:
     'Any memo on the budget screen that starts with "https://", will be displayed as a hyperlink. Use this to link to files in your favourite cloud storage (iCloud, OneDrive, Dropbox, etc.) Note - "http", rather than "https", links are ignored for security reasons.',
