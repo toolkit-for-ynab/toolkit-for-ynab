10ecabcad947d0d4225822c2c07477d6a5d8cc34
Oleh <OlegWock@gmail.com>
2024-01-01 16:02:19 +0100

    Fix labels not showing for Reconciled and Scheduled filters (#3310)
    * Fix labels not showing for Reconciled and Scheduled filters #3307
    
    * Fix wording in comment

--- src/types/ynab/services/YNABAccountsService.d.ts
+++ src/types/ynab/services/YNABAccountsService.d.ts
@@ -1,6 +1,16 @@
+// Not complete definition
 interface YNABAccountsService {
   selectedAccountId: string | null;
   collapsedSplits: {
     [transactionID: string]: boolean;
   };
+  filters: {
+    reconciled: boolean;
+    scheduled: boolean;
+
+    set: (field: string, value: any) => void;
+    applyFilters: VoidFunction;
+  };
+  // Field here might be nested (e.g. 'filters.scheduled')
+  addObserver: (field: string, cb: (target: YNABAccountsService, field: string) => void) => void;
 }
