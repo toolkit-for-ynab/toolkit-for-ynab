4f559ee44bcece62971be936ed590eec2cf32b43
Jake Hamilton <jakehamtexas@gmail.com>
2024-01-15 22:34:41 -0600

    chore(core): convert compressDist.ts to typescript

--- scripts/compressDist.ts
+++ scripts/compressDist.ts
@@ -1,51 +1,56 @@
 #!/usr/bin/env ts-node
 
-const archiver = require('archiver');
-const fs = require('fs');
-const path = require('path');
-const yargs = require('yargs').argv;
+import archiver from 'archiver';
+import fs from 'fs';
+import path from 'path';
+import yargs from 'yargs';
 
-const workspaceRoot = path.join(__dirname, '..');
-const extensionDirectory = path.join(workspaceRoot, 'dist', 'extension');
-const outputDirectory = path.join(workspaceRoot, 'dist');
+const allowedBrowsers = ['chrome', 'firefox', 'edge'] as const;
 
-if (!fs.existsSync(extensionDirectory)) {
-  console.error(
-    "The dist/web-extension directory doesn't exist yet. Run `yarn build` before running this script."
-  );
-  process.exit(1);
-}
+async function main() {
+  const { browser } = await yargs.choices('browser', allowedBrowsers).parse();
+  const workspaceRoot = path.join(__dirname, '..');
+  const extensionDirectory = path.join(workspaceRoot, 'dist', 'extension');
+  const outputDirectory = path.join(workspaceRoot, 'dist');
 
-if (!['chrome', 'firefox', 'edge'].includes(yargs.browser)) {
-  console.log(`${yargs.browser} is not a valid browser option`);
-  process.exit(1);
-}
-
-// It's nice to create zip files with the version number in the name
-const version = require(path.join(extensionDirectory, 'manifest.json')).version;
-const zipFile = path.join(outputDirectory, `toolkit-for-ynab-v${version}-${yargs.browser}.zip`);
-
-// Create our zip file
-const output = fs.createWriteStream(zipFile);
-const archive = archiver('zip', { zlib: { level: 9 } });
-
-output.on('close', () => {
-  console.log('Archive successfully created.');
-});
-
-archive.on('warning', (error) => {
-  if (error.code === 'ENOENT') {
-    console.warn('Warning while archiving: ', error);
-  } else {
-    throw error;
+  if (!fs.existsSync(extensionDirectory)) {
+    console.error(
+      "The dist/web-extension directory doesn't exist yet. Run `yarn build` before running this script."
+    );
+    process.exit(1);
   }
-});
 
-archive.on('error', (error) => {
-  throw error;
-});
+  // It's nice to create zip files with the version number in the name
+  const version = require(path.join(extensionDirectory, 'manifest.json')).version;
+  const zipFile = path.join(outputDirectory, `toolkit-for-ynab-v${version}-${browser}.zip`);
 
-// append everything from dist/, putting its contents at the root of archive
-archive.pipe(output);
-archive.directory(extensionDirectory, false);
-archive.finalize();
+  // Create our zip file
+  const output = fs.createWriteStream(zipFile);
+  const archive = archiver('zip', { zlib: { level: 9 } });
+
+  output.on('close', () => {
+    console.log('Archive successfully created.');
+  });
+
+  archive.on('warning', (error) => {
+    if (error.code === 'ENOENT') {
+      console.warn('Warning while archiving: ', error);
+    } else {
+      throw error;
+    }
+  });
+
+  archive.on('error', (error) => {
+    throw error;
+  });
+
+  // append everything from dist/, putting its contents at the root of archive
+  archive.pipe(output);
+  archive.directory(extensionDirectory, false);
+  archive.finalize();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
