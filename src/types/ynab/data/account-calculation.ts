efcfa9e0d260f3ba5fdfede1ddca557d6f885a0e
Jake Hamilton <jakehamtexas@gmail.com>
2024-01-21 15:10:29 -0600

    chore(core): convert account-calculation.d.ts to .ts

---
+++ src/types/ynab/data/account-calculation.ts
@@ -0,0 +1,3 @@
+export interface YNABAccountCalculation {
+  clearedBalance: number;
+}
--- src/types/ynab/data/account.d.ts
+++ src/types/ynab/data/account.d.ts
@@ -1,4 +1,5 @@
 import type { YNABAccountType } from '../window/ynab-enums';
+import { YNABAccountCalculation } from './account-calculation';
 import type { YNABTransaction } from './transaction';
 
 export interface YNABAccount {
