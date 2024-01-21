b93d45ebda1fd3e8da3b3cf603c57d42e79bdef6
Jake Hamilton <jakehamtexas@gmail.com>
2024-01-21 15:13:35 -0600

    chore(core): convert transaction-collection.d.ts to .ts

---
+++ src/types/ynab/data/transaction-collection.ts
@@ -0,0 +1,5 @@
+import type { YNABTransaction } from './transaction';
+
+export interface YNABTransactionCollection extends YNABCollection<YNABTransaction> {
+  findItemByEntityId(entityId: string | null): YNABTransaction;
+}
