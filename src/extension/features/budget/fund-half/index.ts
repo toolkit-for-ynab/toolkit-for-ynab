4bd22ed4dec1e1228701ae99fc93dbdc6ca3c7ef
Joe Rattz <joerattz@yahoo.com>
2024-02-06 00:31:11 -0500

    Fix for issue #3352. (#3363)
    * Assign-Half:  Pass 1 - working.
    
    * Assign-Half:  Pass 2 - First part of renaming to Fund Half.
    
    * Assign-Half:  Pass 3 - Completed renaming to Fund Half and cleaned up a little.
    
    * Assign-Half:  Pass 4 - One last clean-up pass.
    
    * Fund-Half:  Pass 5 - Clean-up code review items.
    
    * [BUG] Collapse Inspector Blue Box #3328 - fixed.
    
    * Fix for issue #3352.

--- src/extension/features/budget/fund-half/index.ts
+++ src/extension/features/budget/fund-half/index.ts
@@ -1,7 +1,6 @@
 import { Feature } from 'toolkit/extension/features/feature';
 import { formatCurrency } from 'toolkit/extension/utils/currency';
-import { getBudgetService, getEntityManager } from 'toolkit/extension/utils/ynab';
-import { getEmberView } from 'toolkit/extension/utils/ember';
+import { getBudgetService, isCurrentMonthSelected } from 'toolkit/extension/utils/ynab';
 
 // The concept here is that for odd monthly Target amounts there is a low half and a high half.
 // Example:  For $65.05 the low half is $32.52 (x2 = $65.04).  The high half is $32.53 (x2 - $65.06).
@@ -49,6 +48,7 @@ export class FundHalf extends Feature {
   }
 
   updateDOM() {
+    if (!isCurrentMonthSelected()) return;
     if (!$('.budget-inspector').length) return;
     if (!$('.inspector-quick-budget').length) return;
     if ($('.budget-inspector-button.fund-half').length) return;
@@ -153,10 +153,19 @@ export class FundHalf extends Feature {
   };
 
   _fundAmountToBudgetCategory(budgetRowId: string, amount: number) {
-    const budgetRow = document.querySelector(`[data-entity-id="${budgetRowId}"]`);
-    const emberView = getEmberView<BudgetTableRowComponent>(budgetRow?.id);
-    if (emberView) {
-      emberView.category.budgeted += amount;
+    const budgetService = getBudgetService();
+    if (
+      budgetService !== undefined &&
+      budgetService != null &&
+      budgetService.checkedRowsCount > 0
+    ) {
+      const checkedRows = budgetService.checkedRows;
+      if (Array.isArray(checkedRows)) {
+        const categoryRow = checkedRows.find((row) => row.categoryId === budgetRowId);
+        if (categoryRow) {
+          categoryRow.budgeted += amount;
+        }
+      }
     }
   }
 }
