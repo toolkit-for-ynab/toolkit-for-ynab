750cf195c05b81c08c2733c2a5092ee9d5c06b2c
Yusef Ouda <YusefOuda@users.noreply.github.com>
2024-02-19 16:16:19 -0600

    Fixes for #3379 & #3259
    - Fixes issue #3379 by only adding filter buttons onRouteChanged instead of observe
    - Fixes issue #3259 by including enable-ember-debug.js in Firefox manifest
    - Additional safety check for import notification feat.

--- src/extension/features/accounts/toggle-transaction-filters/index.tsx
+++ src/extension/features/accounts/toggle-transaction-filters/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Feature } from 'toolkit/extension/features/feature';
 import { componentBefore } from 'toolkit/extension/utils/react';
 import { getAccountsService } from 'toolkit/extension/utils/ynab';
+import { isCurrentRouteAccountsPage } from 'toolkit/extension/utils/ynab';
 
 const ToggleButton = ({
   stateField,
@@ -37,34 +38,38 @@ const ToggleButton = ({
 };
 
 export class ToggleTransactionFilters extends Feature {
-  observe(changedNodes: Set<string>) {
-    if (changedNodes.has('accounts-toolbar-right')) {
-      this.injectButtons($('.accounts-toolbar'));
-    }
+  containerClass = 'tk-toggle-transaction-filters';
+
+  shouldInvoke() {
+    return isCurrentRouteAccountsPage();
   }
 
   injectCSS() {
     return require('./styles.scss');
   }
 
-  destroy() {
-    $('#transaction-filters').remove();
+  onRouteChanged() {
+    if (!this.shouldInvoke()) return;
+
+    this.invoke();
   }
 
-  injectButtons = ($element: JQuery<HTMLElement>) => {
-    const toolbarRight = $('.accounts-toolbar-right', $element);
-    if ($('#tk-toggle-transaction-filters', toolbarRight).length) {
-      return;
-    }
+  destroy() {
+    document.querySelector('.' + this.containerClass)?.remove();
+  }
 
+  invoke() {
     const showLabel = this.settings.enabled === '2';
+    const toggleTransactionFiltersContainer = document.querySelector('.' + this.containerClass);
 
-    componentBefore(
-      <span id="tk-toggle-transaction-filters" className="tk-toggle-transaction-filters">
-        <ToggleButton stateField="scheduled" showLabel={showLabel} />
-        <ToggleButton stateField="reconciled" showLabel={showLabel} />
-      </span>,
-      $('.js-transaction-search', toolbarRight)[0]
-    );
-  };
+    if (!toggleTransactionFiltersContainer) {
+      componentBefore(
+        <span className={this.containerClass}>
+          <ToggleButton stateField="scheduled" showLabel={showLabel} />
+          <ToggleButton stateField="reconciled" showLabel={showLabel} />
+        </span>,
+        document.querySelector('.js-transaction-search')
+      );
+    }
+  }
 }
