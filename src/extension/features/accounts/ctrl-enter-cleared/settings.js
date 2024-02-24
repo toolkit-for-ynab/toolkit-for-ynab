2ddb1909d85e104f4ee600adf1b3d5db48c5c4a3
Alex Hatzenbuhler <ahatzenbuhler@figure.com>
2024-02-23 20:57:43 -0600

    Remove approve and clear keyboard shortcut in favor of native shortcuts (#3384)
    * Remove easy approve keyboard shortcut in favor of native shortcuts
    
    * Remove ctrl-enter-cleared

--- src/extension/features/accounts/ctrl-enter-cleared/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'CtrlEnterCleared',
-  type: 'checkbox',
-  default: false,
-  section: 'accounts',
-  title: 'Keyboard Shortcut: Clear Transaction [CTRL + Enter]',
-  description:
-    "Clear a new or edited transaction by pressing CTRL+Enter (CMD on Mac) while it's selected.",
-};
