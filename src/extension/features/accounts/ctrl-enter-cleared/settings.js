06c271a985a42752d81f782adf0fac15d516bc0d
Alex Hatzenbuhler <hatz@hey.com>
2024-03-09 00:40:11 -0600

    Revert "Remove approve and clear keyboard shortcut in favor of native shortcu…" (#3400)
    This reverts commit 2ddb1909d85e104f4ee600adf1b3d5db48c5c4a3.

---
+++ src/extension/features/accounts/ctrl-enter-cleared/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'CtrlEnterCleared',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Keyboard Shortcut: Clear Transaction [CTRL + Enter]',
+  description:
+    "Clear a new or edited transaction by pressing CTRL+Enter (CMD on Mac) while it's selected.",
+};
