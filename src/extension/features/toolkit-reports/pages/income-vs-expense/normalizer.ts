b5c4220ff97af6fe7f51562695d23e30095dfe87
Will Poorman <will.k.poorman@gmail.com>
2024-04-23 01:12:11 -0500

    fix(normalizeNetIncomes): Handle edge cases
    There are some instances where expenses and incomes monthly totals don't
    start or end on the same months. The case where they might start on
    different months can happen if you create an account, then add some
    transactions to it with dates in prior months to the month the starting
    balance was set it. The case where they might end on different months may
    not actually exist, since latest months should always have at least an
    empty array of totals for both. But it can't hurt to handle it just incase.

--- src/extension/features/toolkit-reports/pages/income-vs-expense/normalizer.ts
+++ src/extension/features/toolkit-reports/pages/income-vs-expense/normalizer.ts
@@ -7,13 +7,60 @@ export function normalizeNetIncomes(
   const expensesMonthlyTotals = expenses.monthlyTotals;
   const incomesMonthlyTotals = incomes.monthlyTotals;
 
-  return incomesMonthlyTotals.map((incomeMonthData, index) => {
-    const expenseMonthData = expensesMonthlyTotals[index];
+  let netIncomes: NormalizedNetIncome[] = [];
 
-    return {
-      date: incomeMonthData.date.clone(),
-      total: incomeMonthData.total + expenseMonthData.total,
-      transactions: [...incomeMonthData.transactions, ...expenseMonthData.transactions],
-    };
-  });
+  let expensesIdx = 0;
+  let incomeIdx = 0;
+
+  while (expensesIdx < expensesMonthlyTotals.length || incomeIdx < incomesMonthlyTotals.length) {
+    const expenses = expensesMonthlyTotals[expensesIdx];
+    const income = incomesMonthlyTotals[incomeIdx];
+
+    let netIncome: NormalizedNetIncome;
+
+    // There is no case for if expense and income are both null, because that should never happen.
+    // If it did, then we would not have anything to determine date from and thus we'd generate an
+    // invalid NormalizedNetIncome object
+    if (expenses == null) {
+      netIncome = {
+        date: income.date.clone(),
+        total: income.total,
+        transactions: [...income.transactions],
+      };
+      incomeIdx++;
+    } else if (income == null) {
+      netIncome = {
+        date: expenses.date.clone(),
+        total: expenses.total,
+        transactions: [...expenses.transactions],
+      };
+      expensesIdx++;
+    } else if (expenses.date.equalsByMonth(income.date)) {
+      netIncome = {
+        date: income.date.clone(),
+        total: income.total + expenses.total,
+        transactions: [...income.transactions, ...expenses.transactions],
+      };
+      expensesIdx++;
+      incomeIdx++;
+    } else if (expenses.date.isBefore(income.date)) {
+      netIncome = {
+        date: expenses.date.clone(),
+        total: expenses.total,
+        transactions: [...expenses.transactions],
+      };
+      expensesIdx++;
+    } else {
+      netIncome = {
+        date: income.date.clone(),
+        total: income.total,
+        transactions: [...income.transactions],
+      };
+      incomeIdx++;
+    }
+
+    netIncomes.push(netIncome);
+  }
+
+  return netIncomes;
 }
