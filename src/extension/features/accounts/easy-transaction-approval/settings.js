058a7f9b8ef89ffc79c02d10b0ba1025e6f81676
Alex Rusbridge <git@alexrusbridge.co.uk>
2024-06-04 14:16:28 +0100

    Removed easy transaction approval now already implemented in YNAB.

--- src/extension/features/accounts/easy-transaction-approval/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'EasyTransactionApproval',
-  type: 'checkbox',
-  default: false,
-  section: 'accounts',
-  title: 'Keyboard Shortcut: Approve Transaction [A] or [Enter]',
-  description:
-    "Approve scheduled or linked transactions by pressing 'A' or 'Enter' on your keyboard selecting any number of transactions. Alternately, approve single scheduled or linked transactions by right clicking on the blue 'i' or link icon.",
-};
