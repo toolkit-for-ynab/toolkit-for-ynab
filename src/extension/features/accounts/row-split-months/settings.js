a5f4744302a6c697158329fc79c6556d71e98075
Sandrina Pereira <sandrin4pereir4@gmail.com>
2024-08-13 15:27:01 +0100

    feat(RowSplitMonths): Create "Visually separate transactions between months" feature (#3507)
    * feat(RowSplitMonths): Create settings
    
    * feat(RowSplitMonths): Build code
    
    * docs: Add feature to feature-list

---
+++ src/extension/features/accounts/row-split-months/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'RowSplitMonths',
+  type: 'checkbox',
+  default: false,
+  section: 'accounts',
+  title: 'Visually separate transactions between months',
+  description:
+    'Add a thick line in the transactions table after the last transactions of a month to visually separate from the next month.',
+};
