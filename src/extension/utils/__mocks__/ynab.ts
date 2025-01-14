c94fd27237d0ea423029b33b676bb6fc61b9f344
Josh Madewell <josh.madewell@gmail.com>
2025-01-13 21:50:48 -0600

    fix tests

--- src/extension/utils/__mocks__/ynab.ts
+++ src/extension/utils/__mocks__/ynab.ts
@@ -1,3 +1,2 @@
 export const isYNABReady = jest.fn().mockReturnValue(false);
 export const getCurrentRouteName = jest.fn().mockReturnValue('');
-export const ynabRequire = (global as any).requireModule;
