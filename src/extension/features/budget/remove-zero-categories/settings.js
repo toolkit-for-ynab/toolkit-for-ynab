df4c7d3fff7d67dd65b87909aa1885c2e529b17d
Em Lowing <elowing@gmail.com>
2025-04-24 16:51:20 -0400

    Remove now-native hiding of zero categories

--- src/extension/features/budget/remove-zero-categories/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'RemoveZeroCategories',
-  type: 'checkbox',
-  default: false,
-  section: 'budget',
-  title: 'Hide Negative/Zero Categories When Covering Overspending',
-  description: `Removes categories which have a zero or negative balance from the "Cover Overspending" menu since they don't have any way of helping.`,
-};
