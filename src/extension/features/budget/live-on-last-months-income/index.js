e499d2c1232a855c9f170a1f5c4d7b97ac7431e8
Mark Hansen <mark@markhansen.info>
2025-05-25 14:19:10 -0500

    fix: restore Live on Last Month's Income feature in Inspector pane

--- src/extension/features/budget/live-on-last-months-income/index.js
+++ src/extension/features/budget/live-on-last-months-income/index.js
@@ -52,12 +52,16 @@ export class LiveOnLastMonthsIncome extends Feature {
 
     // Add the income from last month section and structure, if not already in place
     if ($('#tk-last-months-income').length === 0) {
-      $('.budget-breakdown-monthly-totals').after(
+      $('.budget-breakdown .card[class="card"]').after(
         $('<section>', {
           class: 'card',
           id: 'tk-last-months-income',
         })
-          .append($('<div>', { class: 'card-roll-up' }).text("Live on Last Month's Income"))
+          .append(
+            $('<button>', { class: 'card-roll-up', type: 'button' })
+              .append('<h2>')
+              .text("Live on Last Month's Income"),
+          )
           .append(
             $('<div>', {
               class: 'card-body',
