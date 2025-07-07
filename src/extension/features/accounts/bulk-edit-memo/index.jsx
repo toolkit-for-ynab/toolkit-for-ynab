c9078e1fd8ac4b261d86a0c14bf2c33e34183ada
Brian Smiley <briantsmiley42@gmail.com>
2025-07-07 14:30:34 -0700

    removes now-unneeded empty tag around EditMemo jsx and adjusts indentation

--- src/extension/features/accounts/bulk-edit-memo/index.jsx
+++ src/extension/features/accounts/bulk-edit-memo/index.jsx
@@ -65,88 +65,86 @@ const EditMemo = () => {
       : l10n('toolkit.editMemoSuffixMany', 'Add suffix to each memo');
 
   return (
-    <>
-      <li className="tk-bulk-edit-memo">
-        {displayMode === DEFAULT_DISPLAY_MODE && (
-          <button className="button-list" onClick={() => setDisplayMode(MENU_DISPLAY_MODE)}>
-            <svg className="ynab-new-icon" width="16" height="16">
-              <use href="#icon_sprite_document"></use>
-            </svg>
-            {makeEditLabel()}
+    <li className="tk-bulk-edit-memo">
+      {displayMode === DEFAULT_DISPLAY_MODE && (
+        <button className="button-list" onClick={() => setDisplayMode(MENU_DISPLAY_MODE)}>
+          <svg className="ynab-new-icon" width="16" height="16">
+            <use href="#icon_sprite_document"></use>
+          </svg>
+          {makeEditLabel()}
+        </button>
+      )}
+      {displayMode === MENU_DISPLAY_MODE && (
+        <>
+          <button
+            autoFocus
+            className="button-list"
+            onClick={() => {
+              setDisplayMode(EDIT_DISPLAY_MODE);
+              setEditMode(REPLACE_EDIT_MODE);
+            }}
+          >
+            {makeReplacelabel()}
           </button>
-        )}
-        {displayMode === MENU_DISPLAY_MODE && (
-          <>
-            <button
+          <button
+            className="button-list"
+            onClick={() => {
+              setDisplayMode(EDIT_DISPLAY_MODE);
+              setEditMode(ADD_PREFIX_EDIT_MODE);
+            }}
+          >
+            {makeAddPrefixLabel()}
+          </button>
+          <button
+            className="button-list"
+            onClick={() => {
+              setDisplayMode(EDIT_DISPLAY_MODE);
+              setEditMode(ADD_SUFFIX_EDIT_MODE);
+            }}
+          >
+            {makeAddSuffixLabel()}
+          </button>
+          <button
+            className="button-list button-cancel tk-memo-cancel"
+            onClick={() => setDisplayMode(DEFAULT_DISPLAY_MODE)}
+          >
+            {l10n('toolkit.editMemoCancel', 'Cancel')}
+          </button>
+        </>
+      )}
+      {displayMode === EDIT_DISPLAY_MODE && (
+        <>
+          {editMode === REPLACE_EDIT_MODE && (
+            <li className="button-list button-disabled">{makeReplacelabel()}</li>
+          )}
+          {editMode === ADD_PREFIX_EDIT_MODE && (
+            <li className="button-list button-disabled">{makeAddPrefixLabel()}</li>
+          )}
+          {editMode === ADD_SUFFIX_EDIT_MODE && (
+            <li className="button-list button-disabled">{makeAddSuffixLabel()}</li>
+          )}
+          <div className="button-list">
+            <input
               autoFocus
-              className="button-list"
-              onClick={() => {
-                setDisplayMode(EDIT_DISPLAY_MODE);
-                setEditMode(REPLACE_EDIT_MODE);
-              }}
-            >
-              {makeReplacelabel()}
-            </button>
-            <button
-              className="button-list"
-              onClick={() => {
-                setDisplayMode(EDIT_DISPLAY_MODE);
-                setEditMode(ADD_PREFIX_EDIT_MODE);
-              }}
-            >
-              {makeAddPrefixLabel()}
-            </button>
-            <button
-              className="button-list"
-              onClick={() => {
-                setDisplayMode(EDIT_DISPLAY_MODE);
-                setEditMode(ADD_SUFFIX_EDIT_MODE);
-              }}
-            >
-              {makeAddSuffixLabel()}
-            </button>
-            <button
-              className="button-list button-cancel tk-memo-cancel"
-              onClick={() => setDisplayMode(DEFAULT_DISPLAY_MODE)}
-            >
-              {l10n('toolkit.editMemoCancel', 'Cancel')}
-            </button>
-          </>
-        )}
-        {displayMode === EDIT_DISPLAY_MODE && (
-          <>
-            {editMode === REPLACE_EDIT_MODE && (
-              <li className="button-list button-disabled">{makeReplacelabel()}</li>
-            )}
-            {editMode === ADD_PREFIX_EDIT_MODE && (
-              <li className="button-list button-disabled">{makeAddPrefixLabel()}</li>
-            )}
-            {editMode === ADD_SUFFIX_EDIT_MODE && (
-              <li className="button-list button-disabled">{makeAddSuffixLabel()}</li>
-            )}
-            <div className="button-list">
-              <input
-                autoFocus
-                className="accounts-text-field"
-                value={memoInputValue}
-                onChange={(e) => setMemoInputValue(e.target.value)}
-              />
-              <div className="tk-grid-actions">
-                <button
-                  className="button button-cancel tk-memo-cancel"
-                  onClick={() => setDisplayMode(MENU_DISPLAY_MODE)}
-                >
-                  {l10n('toolkit.editMemoBack', 'Back')}
-                </button>
-                <button className="button button-primary tk-memo-save" onClick={handleConfirm}>
-                  {l10n('toolkit.editMemoSave', 'Save')}
-                </button>
-              </div>
+              className="accounts-text-field"
+              value={memoInputValue}
+              onChange={(e) => setMemoInputValue(e.target.value)}
+            />
+            <div className="tk-grid-actions">
+              <button
+                className="button button-cancel tk-memo-cancel"
+                onClick={() => setDisplayMode(MENU_DISPLAY_MODE)}
+              >
+                {l10n('toolkit.editMemoBack', 'Back')}
+              </button>
+              <button className="button button-primary tk-memo-save" onClick={handleConfirm}>
+                {l10n('toolkit.editMemoSave', 'Save')}
+              </button>
             </div>
-          </>
-        )}
-      </li>
-    </>
+          </div>
+        </>
+      )}
+    </li>
   );
 };
 
